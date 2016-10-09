57c1ed4e40867539e2f78c6a1b3be391708b5f96
Jingjie Jiang <chygojiang@gmail.com>
2016-10-10 00:28:42 +0800

    220. Contains Duplicate III JJJ Oct 9, 2016 1. used sliding window + treeset, runtime was 59-69ms and beat 12% - 20%. The best solution can be 0ms. The 2nd time I should try sorting the array and then keep the original index.

---
+++ ContainsDuplicateiII/src/ContainsDuplicateIII.java
@@ -0,0 +1,50 @@
+import java.util.TreeSet;
+
+
+public class ContainsDuplicateIII {
+	
+	public static boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        // * idea: sliding window + treeSet
+        // *** result: 59-69ms, beat 20 - 12%, there must be better solutions. Fastest solution in the database can be 0ms.
+        
+        // *** second time: try this solution: another solution can be: sort the array, then keep the original index.
+        
+        
+        TreeSet<Integer> numSet = new TreeSet<Integer>();
+        
+        for (int i = 0; i < nums.length; i ++) {
+            
+            if ( (numSet.floor(nums[i]) != null && nums[i] <= t + numSet.floor(nums[i]) ) 
+                || (numSet.ceiling(nums[i]) != null && t + nums[i] >= numSet.ceiling(nums[i]) ) ) {
+                    return true;
+                }
+            
+            // unlike duplicate II, same elements do not mean |nums[i] - nums[j]| < t, when t < 0
+            numSet.add(nums[i]);
+            
+            if (i >= k) {
+                numSet.remove(nums[i - k]);
+            }
+        }
+        
+        return false;
+    }
+
+	public static void main(String[] args) {
+		// test case
+		/**
+		 * []
+			0
+			0
+			
+			[2,3,4,1,3,15,7,8,2,324,3,4,2]
+			4
+			2
+			
+			[2,5,8,11,15,18,21,24,27,324,38,41,45]
+			4
+			2
+		 */
+	}
+
+}
