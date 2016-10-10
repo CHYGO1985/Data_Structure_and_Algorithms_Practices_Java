51c4268b1cd8a695798c2924808ee1214188fdfb
Jingjie Jiang <chygojiang@gmail.com>
2016-10-10 17:21:45 +0800

    223. Rectangle Area JJJ Oct 10, 2016 I couldn't find the general solution to define whether there is intersection between the two rectangles. Coz I didn't bother to draw the example on paper to observe the law which result in wasting so much time. This is the lesson I learned.

---
+++ RectangleArea/src/RectangleArea.java
@@ -0,0 +1,53 @@
+
+class RectangleArea {
+	
+	public static int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        // *** 1st round: I misunderstood the question "Find the total area covered by two rectilinear rectangles in a 2D plane." not the intersected area.
+        
+        // * reach to the solution: draw examples (difficult part: think about different typical cases) --> find laws --> coding (write down main steps as comments --> coding)
+        // 1. draw four dif cases that A is covered by B in four dif directions: bot-left, bot-right, top-leff, top-right
+        // 2. find the laws: the covered area: 
+        // 1) for bottom-left, get higher x and y
+        // 2) for top-right, get lower x and y
+        
+        int intersect = 0;
+        
+        // areaA + areaB - intersection = total area.
+        
+        // check whether there is intersection between the two
+        if ( ( (E >= A && E <= C) && F <= D ) 
+            || ( (A >= E && A <= G) && B <= H ) ) {
+            
+            // find higher x and y for bot-left (A,B and E,F)
+            int botLX = Math.max(A, E);
+            int botLY = Math.max(B, F);
+            
+            // find lower x and y for top right (C,D and G,H)
+            int topRX = Math.min(C, G);
+            int topRY = Math.min(D, H);
+            
+            // cal the area
+            // length = top.x - bot.x
+            // wide = top.y - bot.y;
+            intersect = calArea(botLX, botLY, topRX, topRY);
+        }
+        
+        int area1 = calArea(A, B, C, D);
+        int area2 = calArea(E, F, G, H);
+        
+        return area1 + area2 - intersect;
+    }
+    
+    public static int calArea(int botLX, int botLY, int topRX, int topRY) {
+        
+        return (topRX - botLX) * (topRY - botLY);
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// int area = computeArea(-2, -2, 2, 2, 1, -3, 3, -1); 
+		// int area = computeArea(-2, -2, 2, 2, -4, 3, -3, 4); 
+		int area = computeArea(-5, 4, 0, 5, -3, -3, 3, 3); 
+	}
+
+}
