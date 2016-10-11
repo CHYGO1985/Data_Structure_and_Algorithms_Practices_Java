a6f5ca843cb6c314b54b82a7f57908ccfff94062
Jingjie Jiang <chygojiang@gmail.com>
2016-10-11 14:20:11 +0800

    172. Factorial Trailing Zeroes JJJ Oct 11, 2016 The hint was from internet, then I found the fastest solution.

---
+++ FactorialTrailingZeroes/src/FactorialTrailingZeroes.java
@@ -0,0 +1,9 @@
+
+public class FactorialTrailingZeroes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- RectangleArea/src/RectangleArea.java
+++ RectangleArea/src/RectangleArea.java
@@ -4,12 +4,16 @@ class RectangleArea {
 	public static int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
         // *** 1st round: I misunderstood the question "Find the total area covered by two rectilinear rectangles in a 2D plane." not the intersected area.
         
+        // *** I couldn't find the general solution for finding whether there is intersect area, coz I didn't bother to
+        // draw draft to observe the laws, which wasted many time.
+        
         // * reach to the solution: draw examples (difficult part: think about different typical cases) --> find laws --> coding (write down main steps as comments --> coding)
         // 1. draw four dif cases that A is covered by B in four dif directions: bot-left, bot-right, top-leff, top-right
         // 2. find the laws: the covered area: 
         // 1) for bottom-left, get higher x and y
         // 2) for top-right, get lower x and y
         
+        /*
         int intersect = 0;
         
         // areaA + areaB - intersection = total area.
@@ -36,11 +40,21 @@ class RectangleArea {
         int area2 = calArea(E, F, G, H);
         
         return area1 + area2 - intersect;
-    }
-    
-    public static int calArea(int botLX, int botLY, int topRX, int topRY) {
+        */
+        int areaOfSqrA = (C-A) * (D-B);
+        int areaOfSqrB = (G-E) * (H-F);
         
-        return (topRX - botLX) * (topRY - botLY);
+        int left = Math.max(A, E);
+        int right = Math.min(G, C);
+        int bottom = Math.max(F, B);
+        int top = Math.min(D, H);
+        
+        //If overlap
+        int overlap = 0;
+        if(right > left && top > bottom)
+             overlap = (right - left) * (top - bottom);
+        
+        return areaOfSqrA + areaOfSqrB - overlap;
     }
 
 	public static void main(String[] args) {
