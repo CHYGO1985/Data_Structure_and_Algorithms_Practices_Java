522dd64e891603bc55b3b364cd2259b9ff359d81
Jingjie Jiang <chygojiang@gmail.com>
2016-10-18 00:43:05 +0800

    136. Single Number JJJ Oct 18, 2016 1. bit manipulation to find a single difference. The solution costs 1ms,  which is the fastest solution in the database.

---
+++ SingleNumber/src/SingleNumber.java
@@ -0,0 +1,21 @@
+
+public class SingleNumber {
+	
+	public static int singleNumber(int[] nums) {
+        // * idea: find differences --> bit manipulation (exclusive or) or math calculation
+        // *** result: 1ms, beat 40.05% the fastest in the database.
+        int num = 0;
+        
+        for (int i = 0; i < nums.length; i ++) {
+            num ^= nums[i];
+        }
+        
+        return num;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
