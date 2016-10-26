123c10ac3360e2fe60f1fa213816a75d1b4fa6dd
Jingjie Jiang <chygojiang@gmail.com>
2016-10-26 11:14:50 +0800

    238. Product of Array Except Self JJJ Oct 26, 2016. I could not solve the problem. Use two direction methods.

---
+++ ProductOfArrayExceptSelf/src/ProductOfArrayExceptSelf.java
@@ -0,0 +1,29 @@
+
+public class ProductOfArrayExceptSelf {
+	
+	public static int[] productExceptSelf(int[] nums) {
+        // *** I could not find the solution myself.
+        // * method: two direction method https://discuss.leetcode.com/topic/25121/java-solution-with-constant-space
+        // result: 2ms, beat 37.60%, fatest in the database
+        int n = nums.length;
+        int[] output = new int[n];
+        int temp = 1;
+        output[0] = 1;
+        for (int i = 1; i < n; i++)
+        {
+            output[i] = output[i-1] * nums[i-1];
+        }
+        for (int i = n - 1; i >= 0 ; i--)
+        {
+            output[i] = temp * output[i];
+            temp = temp * nums[i];
+        }
+        return output;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
