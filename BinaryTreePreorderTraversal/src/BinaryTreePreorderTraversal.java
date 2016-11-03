555821a66a9e6f31e2069f484476e48ac48f5336
Jingjie Jiang <chygojiang@gmail.com>
2016-11-03 18:15:14 +0800

    337. House Robber III JJJ Nov 3, 2016 1. *** unsolved, solution from: http://blog.csdn.net/happyaaaaaaaaaaa/article/details/50880121 2.  idea: dynamic programming 3. * result : unsolved, 1ms, beat 78.64%, fastest in the database,  used 2 hours to think, 2 hours to learn 4, * things learned: 1) the example given is to enlight my thought, not confine my thought, do not consider the example as the only case for the question. 2) dynamic programming: find the atomic step first 3) use [0,1] to control interval

--- BinaryTreePreorderTraversal/src/BinaryTreePreorderTraversal.java
+++ BinaryTreePreorderTraversal/src/BinaryTreePreorderTraversal.java
@@ -1,5 +1,80 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
 
 public class BinaryTreePreorderTraversal {
+	
+		public class TreeNode {
+			int val;
+			TreeNode left;
+			TreeNode right;
+			TreeNode(int x) { val = x; }
+		}
+		public static List<Integer> preorderTraversal(TreeNode root) {
+	        // * idea: use stack to represent the recursive procedure
+	        // * result : solved/half-solved/unsolved, used time and run time, % in the db, fastest in the db
+	        // * things learned：I was confused about preorder
+	        // *** result: 1ms, beat 31.62%, fastest is 0ms (4.48%)
+	        
+	        List<Integer> list = new LinkedList<Integer>();
+	        Stack<TreeNode> stack = new Stack<TreeNode>();
+	        boolean isFstVisit = false;
+	        
+	        // special case
+	        if (root != null) {
+	            list.add(root.val);
+	            stack.push(root);
+	            if (null == root.left) {
+	                // *** i did not consider that left is empty
+	                isFstVisit = true;
+	            }
+	        }
+	        
+	        while (false == stack.empty()) {
+	            
+	            TreeNode topInStack = stack.peek();
+	            
+	            /*
+	            if (false == isFstVisit) {
+	                
+	                while (topInStack.left != null) {
+	                    
+	                    topInStack = topInStack.left;
+	                    list.add(topInStack.val);
+	                    stack.push(topInStack);
+	                }
+	                topInStack = stack.pop();
+	                isFstVisit = true;
+	            }
+	            else{
+	                
+	                topInStack = stack.pop();
+	            }
+	            */
+	            
+	            if (false == isFstVisit) {
+	                
+	                while (topInStack.left != null) {
+	                    
+	                    topInStack = topInStack.left;
+	                    list.add(topInStack.val);
+	                    stack.push(topInStack);
+	                }
+	                isFstVisit = true;
+	            }
+	                
+	            topInStack = stack.pop();
+	            
+	            if (topInStack.right != null) {
+	                list.add(topInStack.right.val);
+	                stack.push(topInStack.right);
+	                isFstVisit = false;
+	            }
+	        }
+	        
+	        return list;
+	    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
