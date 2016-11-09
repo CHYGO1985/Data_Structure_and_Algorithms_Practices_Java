a9dd693fd3211c9edff7f3118d702d5a46655ef4
Jingjie Jiang <chygojiang@gmail.com>
2016-11-10 01:45:00 +0800

    18. 4Sum JJJ Nov 09, 2016 I used mourned 8 hours to learn the algorithm behind it (the method used to eliminate repeated answered). I wasted my time on finding one via index, but the thing is that index has nothing to do with the numbers, different index can have sane sums. 1. unsolved, I forgot 2sum method, then with the hint, I was close to the solution, must go for second round things learned: always prove idea before actually programming it, otherwise it will waste time.

---
+++ 4Sum/src/FourSum.java
@@ -0,0 +1,197 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+
+public class FourSum {
+
+    public static List<Integer> add(int i,int j,int k,int m){
+        List<Integer> l = new ArrayList<Integer>();
+        l.add(i);
+        l.add(j);
+        l.add(k);
+        l.add(m);
+        return l;
+    }
+    
+    public static int[] sortAndMergeArray(int[] nums, int[] array1, int[] array2) {
+	        
+        int[] sum = new int[4];
+        
+        sum[0] = nums[array1[0]];
+        sum[1] = nums[array1[1]];
+        sum[2] = nums[array2[0]];
+        sum[3] = nums[array2[1]];
+        
+        Arrays.sort(sum);
+        
+        return sum;
+    }
+    
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+        // method 1: brutal force
+        // * idea: combination / permutation --> how to solve it
+        
+        // * things learned : hash table for the 2sum problems. 
+        // I was thinking to split 4sum to 2sum + 2sum, 3*(2sum + 1) + 1 which is one of the solution.
+        // steps:
+        // 1. sort array;
+        // 2. find all the two sums with their indexes stored
+        // 3. two sum methods to find all the matched two sums
+        
+        // leetcode: 7ms solution
+        // https://discuss.leetcode.com/topic/29585/7ms-java-code-win-over-100
+        
+        // method 2: http://blog.csdn.net/taxueguilai1992/article/details/45584533
+        // result: 72ms, beat 68%
+        
+        
+        // method 3: same idea as 2, but with different strategy to compare Map
+        int[] num = nums;
+        Arrays.sort(num);
+        int len = num.length;
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        Map<Integer,List<int[]>> map = new HashMap<Integer,List<int[]>>();
+        
+        // two loops to build maps of two sums
+        for(int i = 0; i < len - 1; i ++){
+            
+            for(int j = i + 1; j < len; j ++){
+                
+                int[] temp = new int[2];
+                temp[0] = i;
+                temp[1] = j;
+                int sum = num[i]+num[j];
+                if(!map.containsKey(sum)){
+                    List<int[]> list = new ArrayList<int[]>();
+                    list.add(temp);
+                    map.put(sum, list);
+                }
+                else{
+                    List<int[]> list = map.get(sum);
+                    list.add(temp);
+                    map.put(sum, list);
+                }
+            }
+        }
+        
+        /*
+        for(int i=0;i<len-3;i++){
+            if(i!=0&&num[i]==num[i-1])
+                continue;
+            for(int j=i+1;j<len-2;j++){
+                if(j!=i+1&&num[j]==num[j-1])
+                    continue;
+                int targetMinus=target-num[i]-num[j];
+                //直接从map中寻找是否存在对应的键值
+                if(map.containsKey(targetMinus)){
+                    List<int[]> list=map.get(targetMinus);
+                    boolean flag=false;//记录此次循环是否为第一次添加到结果
+                    for(int p = 0;p < list.size();p ++){
+                        int[]  temp=list.get(p);
+                        //如果小标小于j忽略，这里是为了避免重复
+                        if(temp[0]<=j)
+                            continue;
+                        //将这一次的结果与该循环中上一次添加的结果对比，如果相同则继续下一次循环
+                        if(flag&&res.get(res.size()-1).get(2)==num[temp[0]])
+                            continue;
+                        List<Integer> l=add(i,j,temp[0],temp[1]);
+                        res.add(l);
+                        flag=true;
+                    }
+                }
+            }
+        }
+        */
+        
+        for (int key: map.keySet()) {
+            // if there is a match key for current key,then check whether there is repeated index in the correspinding
+            // arrays
+            if (true == map.containsKey(target - key)) {
+                
+                List<int[]> list1 = map.get(key);
+                List<int[]> list2 = map.get(target - key);
+                
+                for (int[] array1: list1) {
+                
+                    for (int[] array2: list2) {
+                        
+                        boolean isValid = true;
+                        int[] indexArr = sortAndMergeArray(num, array1, array2);
+                        
+                        // cal the sum of the four index
+                        int sum = 0;
+                        
+                        if (array1[0] == array2[0] || array1[0] == array2[1]
+                            || array1[1] == array2[0] || array1[1] == array2[1]) {
+                            isValid = false;
+                        }
+                        
+                        // compare two array has same index: 1) bit manipulation, not available, length may exist 32/64
+                        // 2) if use sum of index,it can not tell the dif, e.g. 0,1,4,5 = 1,2,3,4...
+                        if (true == isValid) {
+                            
+                        	boolean isRepeat = false;
+                        	
+                            for (List<Integer> list: res) {
+                                
+                                if ( (0 == list.get(0) - indexArr[0]) && (0 == list.get(1) - indexArr[1])
+                                    && (0 == list.get(2) - indexArr[2]) && (0 == list.get(3) - indexArr[3]) ) {
+                                    isRepeat = true;
+                                }
+                            }
+                            
+                            if (false == isRepeat) {
+                                
+                                List<Integer> temp = add (indexArr[0], indexArr[1]
+                                , indexArr[2], indexArr[3]);
+                                res.add(temp);
+                            }
+                                // test
+                               // List<Integer> temp = add (array1[0], array1[1], array2[0], array2[1]);
+                        }
+                    }
+                }
+            }
+        }  
+        return res;
+    }
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//int[] a = {-3,-2,-1,0,0,1,2,3};
+		 int[] a = {1,0,-1,0,-2,2};
+		List<List<Integer>> list = fourSum(a, 0);
+		
+		int b = 0;
+		int c = b;
+		
+		for (int i = 0; i < 10; i ++) {
+			int j = i;
+			if (j == 2) {
+				break;
+			}
+			int hh = j;
+		}
+		/*
+		Set<int[]> set = new HashSet<int[]>();
+		int[] b = {1,2,3,4};
+		int[] c = {1,2,3,4};
+		set.add(b);
+		System.out.println(set.add(c));
+		*/
+		/*
+		Map<int[], Integer> map = new HashMap<>();
+		int[] b = {1,2,3,4};
+		int[] c = {1,2,3,4};
+        map.put(b, 1);
+		map.put(c, 1);
+		System.out.print(map.getOrDefault(c, 0));
+		*/
+	}
+
+}
--- GroupAnagrams/src/GroupAnagrams.java
+++ GroupAnagrams/src/GroupAnagrams.java
@@ -1,33 +1,119 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 
 public class GroupAnagrams {
 	
-	public static int count(String temp) {
-		
-		int sum = 0;
-		
-		for (int j = 0; j < temp.length(); j ++) {
-            sum += temp.charAt(j) - 'a';
+	public static List<List<String>> groupAnagrams(String[] strs) {
+	       
+        // key idea: is to use a thing to represent different strings.
+        
+        // method 1: https://discuss.leetcode.com/topic/24494/share-my-short-java-solution
+        // *** result: 28ms, beat 78.78% 
+        // * thins learned: 1) I was confined by the idea that hash can only be used for numbers
+        // hash can be used for any match
+        // 2) sorted string --> used string as a key
+        if (strs == null || strs.length == 0) 
+            return new ArrayList<List<String>>();
+        
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        
+        for (int i = 0; i < strs.length; i ++) {
+            
+            String temp = strs[i];
+            char[] arr = temp.toCharArray();
+            Arrays.sort(arr);
+            String keyStr = String.valueOf(arr); // return the string form of arr [a,b,c] --> abc
+            
+            if (false == map.containsKey(keyStr))
+                map.put(keyStr, new LinkedList<String>());
+            map.get(keyStr).add(temp);
         }
-		
-		return sum;
-	}
+        
+        return new ArrayList<List<String>>(map.values());
+        
+        // method 2: I was thinking use sum to distinguish strings, in method 2, the author use teh fact of the product of
+        // diff primes to represent each string
+        // https://discuss.leetcode.com/topic/45639/java-beat-100-use-prime-number/2
+        // similar to my method, but use the product of prime number.
+        
+        /*
+        prime = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103};//最多10609个z
+    
+            List<List<String>> res = new ArrayList<>();
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for (String s : strs) {
+                int key = 1;
+                for (char c : s.toCharArray()) {
+                    key *= prime[c - 'a'];
+                }
+                List<String> t;
+                if (map.containsKey(key)) {
+                    t = res.get(map.get(key));
+                } else {
+                    t = new ArrayList<>();
+                    res.add(t);
+                    map.put(key, res.size() - 1);
+                }
+                t.add(s);
+            }
+            return res; 
+         
+         */
+        
+        // * My method: group strings that have same --> same sums of chars - 'a' -- > I was wrong, coz, different
+        // combination of chars can have the same sum. + sum of (chars in String a - chars in String b) = 0
+        
+        // method 1:
+        // unsolved, 2 hours: 
+        // special case
+        /*
+        // <index, appearance of chars>
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        // store result
+        List<List<String>> list = new ArrayList<List<String>>();
+        int sum = 0;
+        int listIndex = 0;
+        
+        
+        for (int i = 0; i < strs.length; i ++) {
+        
+            List<String> curlist = new LinkedList<String>();
+            String temp = strs[i];
+            
+            for (int j = 0; j < temp.length(); j ++) {
+                int key = temp.charAt(j) - 'a';
+            }
+            
+            // if temp is the new string
+            int tempIndex = map.getOrDefault(sum, 0);
+            if (0 == tempIndex) {
+                // if map key start from 0, then map.getOrDefault(sum, 0) will return 0, which means there is no such sum
+                for ()
+                map.put(sum, listIndex + 1);
+                curlist.add(temp);
+                list.add(listIndex, curlist);
+                listIndex ++;
+            }
+            // if list has anagram before like temp
+            else {
+                list.get(tempIndex - 1).add(temp);
+            }
+            
+            sum = 0;
+        }
+        
+        return list;
+        */
+    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String a = "duh";
-		String b = "ill";
-		
-		System.out.println("a = " + count(a) + " b = " + count(b));
-		
-		String s = "abc";
-		char[] ca = s.toCharArray();
-        Arrays.sort(ca);
-        String keyStr = String.valueOf(ca);
-        
-        System.out.println(keyStr);
 
 	}
 
