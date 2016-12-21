f066f0556491c9604dae106d7a122a686f8a4d8d
Jingjie Jiang <chygojiang@gmail.com>
2016-12-21 20:47:44 +0800

    186. Reverse Words in a String II JJJ Dec 21, 2016 1. idea: used a O(n^2) method, which takes too much time. 2. result : unsolved, used time 1 hour 3. things learned: 1) relative condition in the loop; 2) should do the reservation more carefully to find out that the whole char[] can be reversed first, then reverse each word

---
+++ ReverseWordsInAStringII/src/ReverseWordsInAStringII.java
@@ -0,0 +1,101 @@
+
+public class ReverseWordsInAStringII {
+	
+	public static void moveWord(char[] s, int startPos, int wordPos) {
+        
+        // keep the char for swapping
+        int arrLen = s.length;
+        
+        // shift all the left part (start from startPos) of "wordPos" to right within one unit
+        for (int i = wordPos; i < arrLen; i ++) {
+            
+            char temp = s[i];
+            
+            int j = i - 1;
+            for (; j >= startPos; j --) {
+                
+                s[j + 1] = s[j];
+            }
+            
+            j ++;
+            s[j] = temp;
+            startPos ++;
+        }
+        
+        // shift all left part of new word to right within one unit to cover the space at the end
+        int m = s.length - 2;
+        for (; m >= startPos; m --) {
+            
+            s[m  + 1] = s[m];
+        }
+        
+        // put a new space after the new word
+        m ++;
+        s[m] = ' ';
+    }
+    
+    public static void reverseWords(char[] s) {
+        // Idea: two pointers technique
+        // 1) num of space + 1 = num of word
+        // 2) loop as the_sky_is_blue
+        // --> blue_ (the_sky_is)
+        // --> blue_is_ (the_sky)
+        // --> blue_is_sky_the
+        // 3) for every single loop
+        // a. from rear, find a space where pos is A,
+        // b. start from A + 1, shift the word 
+        // c. new pos = startPos + (char pos + len of word) % total len 
+        // d. at the space at the end to the place next to shifted word
+        // e. back to step a
+        
+        // handle special case
+        if (null == s || 0 == s.length) {
+            return ;
+        }
+        
+        int searchSpace = s.length;
+        // find a space from the rear of s
+        int startPoint = 0;
+        int wordCount = 0;
+        
+        // *** need a word count to stop the reversing
+        for (int i = searchSpace - 1; i >= 0; i --) {
+        	
+            if (' ' == s[i]) {
+            	wordCount ++;
+            }
+        }
+        
+        for (int i = searchSpace - 1; wordCount >0 && i >= 0; i --) {
+            
+            if (' ' == s[i]) {
+                
+                // swap the word from i + 1
+                moveWord(s, startPoint, i + 1);
+                startPoint += searchSpace - i;
+                
+                // *** when there is a match, reset i
+                i = searchSpace - 1;
+                wordCount --;
+            }
+        }
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+        char[] s= {'t', 'h', 'e', ' ',
+        		  's', 'k', 'y', ' ',
+        		  'i', 's', ' ',
+        		  'b', 'l', 'u', 'e'};
+        */
+		
+		char[] s = {'s', 'k', 'y'};
+        reverseWords(s);
+        
+        for (char c: s) {
+        	
+        	System.out.print(c);
+        }
+	}
+}
