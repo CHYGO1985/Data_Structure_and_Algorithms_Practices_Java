b95f1b3fd8e5632babcba61b5ead78ae36eb166f
Jingjie Jiang <chygojiang@gmail.com>
2016-12-25 20:25:14 +0800

    236. Lowest Common Ancestor of a Binary Tree JJJ Dec 25, 2016 1. idea: hash table + post-order traverse 2. result : solved, used time 1.5 hours and run time is 32 ms, beat 9.4% in the db, fastest in the db is 9 ms 3. things learned: 1) learn to use recursive in trees. observe the features of results(draw some trees) 2) handle empty problem

---
+++ LCAOfABinaryTree/src/LCAOfABinaryTree.java
@@ -0,0 +1,157 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LCAOfABinaryTree {
+	
+	public static class TreeNode {
+        int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x, TreeNode left, TreeNode right) { 
+			val = x;
+			this.left = left;
+			this.right = right;
+		}
+    }
+
+	public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // Idea: post-order traverse + Hash Map (easy to find match in hash map)
+        // 1) common ancestor --> need ancestors infor --> need to keep the infor when traverse
+        // 2) pre-order and in-order are not suitable to keep ancestor infor
+        
+        // handle root == null
+        if (null == root) {
+            return root;
+        }
+        
+        // Integer store layer information
+        Map<Integer, TreeNode> mapP = new HashMap<Integer, TreeNode>(); 
+        Map<Integer, TreeNode> mapQ = new HashMap<Integer, TreeNode>();
+        
+        TreeNode tempRoot = root;
+        boolean hasMatch = false;
+        hasMatch = postOrderSearch(tempRoot, 1, mapP, p);
+        
+        if (false == hasMatch) {
+            return root;
+        }
+        
+        tempRoot = root;
+        hasMatch = postOrderSearch(tempRoot, 1, mapQ, q);
+        
+        if (false == hasMatch) {
+            return root;
+        }
+        
+        // if can find both p and q in the tree, compare the lost, otherwise return root
+        TreeNode lca = root;
+        
+        for (Map.Entry<Integer, TreeNode> entry: mapP.entrySet()) {
+            
+            int key = entry.getKey();
+            if (false == mapQ.containsKey(key)) {
+                break;
+            }
+            else {
+                
+                TreeNode tempP = entry.getValue();
+                TreeNode tempQ = mapQ.get(key);
+                if (tempP.val == tempQ.val && tempP.left.val == tempQ.left.val
+                    && tempP.right.val == tempQ.right.val) {
+                    lca = tempP;    
+                }
+            }
+        }
+        
+        return lca;
+    }
+    
+    // post-order traverse
+    public static boolean postOrderSearch(TreeNode node, int layer, Map<Integer, TreeNode> map,
+        TreeNode compNode) {
+        
+        if (null == node) {
+            return false;
+        }
+        else if (true == isTreeNodeEqual(node, compNode)) {
+        	map.put(layer, node);
+            return true;
+        }
+        else {
+            map.put(layer, node);
+        }        
+        // 1.add current node to the stack, then check left child and right child
+        // layer + 1 means current layer count +1, ++ for right child
+        boolean hasMatch = postOrderSearch(node.left, (layer ++) + 1, map, compNode);
+        
+        if (true == hasMatch) {
+            return true;
+        }
+        else {
+        	map.remove(layer, node.left);
+            hasMatch = postOrderSearch(node.right, layer, map, compNode);
+        }
+        
+        // 3. if not match for left and right child, pop the top node from the stack
+        if (false == hasMatch) {
+            map.remove(layer);
+            layer --;
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    
+    public static boolean isTreeNodeEqual(TreeNode m, TreeNode n) {
+    	
+    	if (null == m && null == n) {
+    		return true;
+    	}
+    	else if (null == m || null == n){
+    		return false;
+    	}
+    	else if (m.val == n.val ) {
+    		return isTreeNodeEqual(m.left, n.left) &&
+    				isTreeNodeEqual(m.right, n.right);
+    	}
+    	else {
+    		return false;
+    	}
+    }
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+        TreeNode node6 = new TreeNode(6, null, null);
+        TreeNode node7 = new TreeNode(7, null, null);
+        TreeNode node4 = new TreeNode(4, null, null);
+        TreeNode node8 = new TreeNode(8, null, null);
+        TreeNode node0 = new TreeNode(0, null, null);
+        
+        TreeNode node2 = new TreeNode(2, node7, node4);
+        TreeNode node5 = new TreeNode(5, node6, node2);
+        TreeNode node1 = new TreeNode(1, node0, node8);
+        TreeNode root = new TreeNode(3, node5, node1);
+        */
+		TreeNode noden100 = new TreeNode(-100, null, null);
+		TreeNode noden101 = new TreeNode(-100, null, null);
+		TreeNode noden71 = new TreeNode(-71, null, null);
+		TreeNode node8 = new TreeNode(8, null, null);
+		
+		TreeNode noden12 = new TreeNode(-12, null, node8);
+		TreeNode noden54 = new TreeNode(-54, noden71, noden12);
+		TreeNode node48 = new TreeNode(48, noden54, null);
+		TreeNode noden48 = new TreeNode(-48, noden101, node48);
+		TreeNode noden34 = new TreeNode(-34, null, noden100);
+		TreeNode root = new TreeNode(37, noden34, noden48);
+	
+		
+        TreeNode p = new TreeNode(-100, null, null);
+        TreeNode q = new TreeNode(-71, null, null);
+        
+        TreeNode lcaNode = lowestCommonAncestor(root, p, q);
+        
+	}
+
+}
