bcd04bc995ddfc1586d106aa15a88d7779602fdf
Jingjie Jiang <chygojiang@gmail.com>
2017-01-05 16:14:10 +0800

    103. Binary Tree Zigzag Level Order Traversal JJJ Jan 5, 2017 1. idea: implement method 1 2. result : solved, used time 20 minutes and run time is 2ms, beat 42% in the db, fastest in the db is 1ms 3. things learned:

--- ZigzagLevelOrder/src/ZigzagLevelOrder.java
+++ ZigzagLevelOrder/src/ZigzagLevelOrder.java
@@ -12,15 +12,64 @@ public class ZigzagLevelOrder {
 	
 	public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         
-        // method 1: linked list (for treenode and integer) + layer order traverse + layer%2 flag (flag for whether 
+		 // method 1: linked list (for treenode and integer) + layer order traverse + layer%2 flag (flag for whether 
         // insert to offset 0) 
         // or use layer%2 flag to save one space
+        
+        List<List<Integer>> list = new LinkedList<List<Integer>>(); 
+        if (null == root) {
+            return list;
+        }
+        
+        List<TreeNode> nodeList = new LinkedList<TreeNode>();
+        nodeList.add(root);
+        int layer = 0;
+        
+        while (nodeList.size() != 0) {
+            
+            int nodeSum = nodeList.size();
+            List<Integer> cur = new LinkedList<Integer>();
+            while (nodeSum > 0) {
+                
+                // get every node from nodelist from the beginning
+                TreeNode temp = nodeList.get(0);
+                nodeList.remove(0);
+                // add left and right of node to node list
+                if (temp.left != null) {
+                    nodeList.add(temp.left);
+                }
+                if (temp.right != null) {
+                    nodeList.add(temp.right);
+                }
+                
+                // add curent node val to val list
+                // if (layer%2 != 0), add to offset 0
+                if (0 == layer % 2) {
+                    cur.add(temp.val);
+                }
+                else {
+                    cur.add(0, temp.val);
+                }
+                
+                // nodeSum --
+                nodeSum--;
+            }
+            
+            // add list to res list
+            list.add(cur);
+            //layer ++
+            layer ++;
+        }
+        
+        // return
+        return list;
         
         
         // method 2: linklied list + preorder traverse + layer%2 flag
         // pre/in/post order can still get the layer info, i have figure it out myself in previous application
         // https://discuss.leetcode.com/topic/3413/my-accepted-java-solution   
         // result : 2ms, one of 49.39%, beat 42.46%
+        /*
         List<List<Integer>> list = new LinkedList<List<Integer>>(); 
         if (null == root) {
             return list;
@@ -29,6 +78,7 @@ public class ZigzagLevelOrder {
         zigzagTraverse(root, list, 0);
         
         return list;
+        */
     }
     
     // method 2:
