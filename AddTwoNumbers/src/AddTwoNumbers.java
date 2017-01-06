3342a79c7f240e304b8b0851838574a4ead5a3e0
Jingjie Jiang <chygojiang@gmail.com>
2017-01-06 16:23:18 +0800

    2. Add Two Numbers JJJ Jan 6, 2017 1. idea: using extra O(2) space 2. result : solved, used time 3 hours and run time is 48ms, beat 95.32% in the db, fastest in the db is 44ms. 3. things learned: the solution was very quick to figure out, but I wasted so much time (2 hours) on how to add a node to linkedlist (as I always use l1 = l1.next(even if when l1.next == null)), so at the end l1 always = null, and makes it fail to add it to the original list.

---
+++ AddTwoNumbers/src/AddTwoNumbers.java
@@ -0,0 +1,117 @@
+import javax.swing.text.AbstractDocument.LeafElement;
+
+
+public class AddTwoNumbers {
+	
+	public static class ListNode {
+		int val;
+		ListNode next;
+	    ListNode(int x) { val = x; }
+	}
+	// idea: use the original l1 to store result
+    // result: 48ms. beat 95.28%
+    // things learned: the solution was very quick to figure out, but I wasted so much time (2 hours) on how to add
+    // a node to linkedlist (as I always use l1 = l1.next(even if when l1.next == null)), so it failed to add it to
+    // the list.
+    
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        
+        if (null == l1) {
+            return l2;
+        }
+        
+        int carry = 0;
+        ListNode res = l1;
+        ListNode pointer = l1;
+        
+        while (pointer != null && l2 != null) {
+            
+            int sum = carry + l1.val + l2.val;
+            
+            carry = sum / 10;
+            sum = sum % 10;
+            
+            l1.val = sum;
+            if (l1.next != null) {
+                l1 = l1.next;
+            }
+            pointer = pointer.next;
+            l2 = l2.next;
+        }
+        
+        // for case: index < l1.size() && carry > 0 && index >= l2.size()
+        while (pointer != null && carry > 0) {
+            
+            int curDigit = carry + l1.val;
+            carry = curDigit / 10;
+            curDigit = curDigit % 10;
+            
+            pointer.val = curDigit;
+            if (l1.next != null) {
+                l1 = l1.next;
+            }
+            pointer = pointer.next;
+        }
+        
+        // optimise of previous
+        // optimised : 57ms
+        /*
+        while (pointer != null) {
+
+            int l2Val = (l2 == null ? 0 : l2.val);
+            int sum = carry + l1.val + l2Val;
+            
+            carry = sum / 10;
+            sum = sum % 10;
+            
+            l1.val = sum;
+            if (l1.next != null) {
+                l1 = l1.next;
+            }
+            pointer = pointer.next;
+            l2 = ((l2 == null) ? l2 : l2.next);
+        }
+        */
+        
+        // for case: index >= l1.size() && index < l2.size()
+        while (l2 != null) {
+            
+            int curDigit = carry + l2.val;
+            carry = curDigit / 10;
+            curDigit = curDigit % 10;
+            
+            l1.next = new ListNode(curDigit);
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        
+        // for case: index >= l1.size() && index >= l2.size() && carry > 0
+        if (carry > 0) {
+            
+            l1.next = new ListNode(carry);
+            l1 = l1.next;
+        }
+        
+        return res;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		ListNode l1 = new ListNode(8);
+		l1.next = new ListNode(6);
+		l1.next.next = new ListNode(5);
+		ListNode l2 = new ListNode(2);
+		l2.next = new ListNode(3);
+		l2.next.next = new ListNode(4);
+		*/
+		
+		ListNode l1 = new ListNode(8);
+		l1.next = new ListNode(9);
+		l1.next.next = new ListNode(9);
+		ListNode l2 = new ListNode(2);
+		
+		l1 = addTwoNumbers(l1, l2);
+	}
+
+}
