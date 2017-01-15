256b71f3c5761f248b661880739d7cbb1a159fbb
Jingjie Jiang <chygojiang@gmail.com>
2017-01-15 11:48:54 +0800

    215. Kth Largest Element in an Array JJJ Jan 15, 2017 1. idea: Quickselect — > median of medians algorithm 2. result : unsolved, used time more than 12 hours ( learn quickselect, median of medians and the analysis of their complexity, revise quick sorting implementation) 3. things learned: 1) quick sorting implementation; 2) quick select alg 3) median of medians all 4) the analysis of time complexity of quick select and median of medians

---
+++ KthLargestEleInArray/src/KthLargestEleInArray.java
@@ -0,0 +1,85 @@
+
+public class KthLargestEleInArray {
+	
+	public static int findKthLargest(int[] nums, int k) {
+        // method 1: heap sort O(nlogn), and method that reduce the question to sort problem, will meet
+        // the O(nlogn) bound
+        
+        // method 2: selection algorithm -- based on quick sort O(n)
+        // ref: https://discuss.leetcode.com/topic/14597/solution-explained
+        
+		// the partition is from min to max, so the (new k) num of k's small num than kth largest = length - (old k)kth largest
+        k = nums.length - k;
+        int low = 0;
+        int high = nums.length - 1;
+        
+        // *** why we need this condition? low < high?
+        // for the example: {3,2,1,5,6,4}, the first time, find inde 2, and the second time find index 4, the last time low = high
+        // which means j must be = k = 3, so there is no need to go into the loop, just return arr[k].
+        while (low < high) {
+        	// j represent the length of the left part
+        	// kth largest: 1,2,3,4,5,6,7 if kth largest = 3, then kth smallest is 4, so in this case, if j = 4 (pivot = a[5]), then
+        	// a[5] is the kth largest ele
+            final int j = partition(nums, low, high);
+            // if left part is shorter than k, then go to the right part
+            if(j < k) {
+                low = j + 1;
+            }
+            // if left part is longer than k, then go to the left part
+            else if (j > k) {
+                high = j - 1;
+            }
+            // if j == k, j is the ele that we are looking for
+            else {
+                break;
+            }
+        }
+        
+        return nums[k];
+    }
+    
+	// This function is to partition the array according to pivot a[lo], the return value indicate the length
+	// of the left part of the pivot
+    private static int partition(int[] a, int lo, int high) {
+
+        int i = lo;
+        int j = high + 1;
+        while(true) {
+        	// from left to right, locate the ele that is not less than the ele in index lo
+            while(i < high && less(a[++i], a[lo]));
+            // from right to left, locate the ele that is not bigger than the ele in index lo
+            while(j > lo && less(a[lo], a[--j]));
+            if(i >= j) {
+                break;
+            }
+            
+            // same function as quick sort: put the bigger ele to right part and smaller ele to left part.
+            exch(a, i, j);
+        }
+        
+        // this swap is to put the pivot to the middle of the sperated array
+        // e.g. {4,2,1,5,3,6,7} --> {4,2,1,3,5,6,7} --> swap {3,2,1,4,5,6,7}
+        exch(a, lo, j);
+        return j;
+    }
+
+    private static void exch(int[] a, int i, int j) {
+        final int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+
+    private static boolean less(int v, int w) {
+        return v < w;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int[] arr = {3,2,1,5,6,4};
+		// int[] arr = {1,2,3,4,5,6};
+		// int[] arr = {3,1,2,6,7,8,9};
+		
+		int b = findKthLargest(arr, 3);
+	}
+
+}
--- Permutations/src/Permutations.java
+++ Permutations/src/Permutations.java
@@ -1,10 +1,59 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 
 public class Permutations {
 	
-	public static List<List<Integer>> permute(int[] nums) {
+	// idea: method 1: traditional DFS method with status arrays
+    // result: 9ms, beat 20
+	/*
+    public List<List<Integer>> permute(int[] nums) {
+       
+       List<List<Integer>> list = new LinkedList<List<Integer>>();
+       if (null == nums || 0 == nums.length) {
+           return list;
+       }
+       
+       boolean[] isValid = new boolean[nums.length];
+       for (int i = 0; i < isValid.length; i ++) {
+           isValid[i] = false;
+       }
+       
+       permuteHelper(list, new LinkedList<Integer>(), nums, isValid);
+       return list;
+    }
+    
+    public void permuteHelper(List<List<Integer>> list, List<Integer> curList, int[] nums, boolean[] isValid) {
+        
+        if (curList.size() >= nums.length) {
+            list.add(new LinkedList<Integer>(curList));
+        }
+        else {
+            for (int i = 0; i < nums.length; i ++) {
+                
+                if (true == isValid[i]) {
+                    continue ;
+                }
+                
+                curList.add(nums[i]);
+                isValid[i] = true;
+                permuteHelper(list, curList, nums, isValid);
+                curList.remove(curList.size() - 1);
+                isValid[i] = false;
+            }
+        }
+    }
+    */
+    
+    // method 1.1: backtracking --> temp result list (save status array compared to method 1, but need more time to
+    // check status as it is a linkedlist)
+    // ref: https://discuss.leetcode.com/topic/46162/a-general-approach-to-backtracking-questions-in-java-subsets
+    // -permutations-combination-sum-palindrome-partioning
+    // result: 11 ms, beat 8.49%
+    /*
+    public List<List<Integer>> permute(int[] nums) {
        
        List<List<Integer>> list = new LinkedList<List<Integer>>();
        
@@ -16,15 +65,15 @@ public class Permutations {
        return list;
     }
     
-    public static void permuteHelper(List<List<Integer>> list, List<Integer> curList, int[] nums) {
+    public void permuteHelper(List<List<Integer>> list, List<Integer> curList, int[] nums) {
         
-        if (nums.length >= curList.size()) {
+        if (curList.size() >= nums.length) {
             list.add(new LinkedList<Integer>(curList));
         }
         else {
             for (int i = 0; i < nums.length; i ++) {
                 
-                if (true == list.contains(nums[i])) {
+                if (true == curList.contains(nums[i])) {
                     continue ;
                 }
                 
@@ -34,11 +83,66 @@ public class Permutations {
             }
         }
     }
+    */
+    
+    // method 2: (consider the permutation as swapping) recursive swap, O(1) extra space
+    // idea ref: http://blog.csdn.net/xx77009833/article/details/17843415 
+	public static List<List<Integer>> permute(int[] nums) {
+	       
+       List<List<Integer>> list = new LinkedList<List<Integer>>();
+       if (null == nums || 0 == nums.length) {
+           return list;
+       }
+       
+       // permuteHelper(list, new LinkedList<Integer>(), nums, isValid);
+       
+       permuteSwapper(list, nums, 0);
+       return list;
+    }
+    
+    public static void permuteSwapper(List<List<Integer>> list, int[] nums, int front) {
+        
+        // the recursive process:
+    	// start from 0, swap 0 -- 1, 2,..., nums.length
+    	for (int rear = front + 1; rear < nums.length; rear ++) {
+    		
+    		int[] temp = nums.clone();
+    		swap(temp, front, rear);
+    		// add the new nums to list
+    		list.add(convertArrayToList(temp));
+    		
+    		// within the swap, swap 1 --- 2, 3, ..., nums.length
+    		permuteSwapper(list, nums, 0);
+    	}
+    	
+    	// how the process will be ended
+    	// when the front == nums.leng - 1 (which is the last element in the array)
+    }
+    
+    public static void swap(int[] nums, int front, int rear) {
+    	
+    	int temp = nums[front];
+    	nums[front] = nums[rear];
+    	nums[rear] = temp;
+    }
 
-	public static void main(String[] args) {
+    public static List<Integer> convertArrayToList(int[] nums) {
+    	
+    	List<Integer> temp = new LinkedList<>();
+    	
+    	for(int i: nums) {
+    		temp.add(i);
+    	}
+    	return temp;
+    }
+
+    public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] nums = {1,2,3};
+		
 		List<List<Integer>> list = permute(nums);
+		
+		
 	}
 
 }
