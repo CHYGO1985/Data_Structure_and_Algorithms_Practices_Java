5b837401ee81382ee956f9b8ba8dc8ef940a2979
Jingjie Jiang <chygojiang@gmail.com>
2017-02-19 20:04:12 +0800

    Coin Change JJJ Feb 19, 2017 1. DP learning

---
+++ CoinChange/src/CoinChange.java
@@ -0,0 +1,71 @@
+import java.util.HashMap;
+
+// code ref: https://www.youtube.com/watch?v=sn0DWI-JdNA#t=212.147608
+public class CoinChange {
+	
+	public static long makeChange(int[] coins, int money) {
+		return makeChange(coins, money, 0, new HashMap<String, Long>());
+	}
+	
+	public static long makeChange(int[] coins, int money, int index, HashMap<String, Long> memo) {
+		
+		if (money == 0) {
+			return 1;
+		}
+		
+		if (index >= coins.length) {
+			return 0;
+		}
+		
+		String key = money + "-" + index;
+		if (memo.containsKey(key)) {
+			return memo.get(key);
+		}
+		int amountWithCoin = 0;
+		long ways = 0;
+		while (amountWithCoin <= money) {
+			int remaining = money - amountWithCoin;
+			ways += makeChange(coins, remaining, index + 1, memo);
+			amountWithCoin += coins[index];
+		}
+		
+		memo.put(key, ways);
+		return ways;
+	}
+	
+	/*
+	Question: https://www.hackerrank.com/challenges/ctci-coin-change
+	Ref: https://www.hackerrank.com/challenges/ctci-coin-change/forum
+	Method 2: DP + memp (non- recursive)
+	The theory behind this method is that, take the total method for amount A equals = ways of A (without coins[index]) + ways of (A - coins[index])
+	(need to draw on map to find the regulation)
+	For example: 
+	The regulation is that for amount 2, use coins 1 or coins 2, that is 2 ways
+	For amount 4, 
+	Way 1: all use 1 is same as previous,
+	Way 2:  then use coins 2, puls remain 2 use 1
+	Way 3: the remaining 2 which use 1 use another 2 
+
+	public static long makeChange(int[] coins, int money) {
+	    long[] DP = new long[money + 1]; // O(N) space.
+	    DP[0] = (long)1; 	// n == 0 case.
+	    for(int i = 0 ; i < coins.length; i++) {
+	        int coin = coins[i];
+	        for(int j = coin; j < DP.length; j++) {
+	        // The only tricky step.
+	            DP[j] += DP[j - coin];
+	        }
+	    }       
+	    return DP[money];
+	}
+	*/
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] coints = {4,2,1};
+		// int[] coints = {4,3};
+		int money = 10;
+		long ways = makeChange(coints, money);
+	}
+
+}
--- StringToInteger/src/StringToInteger.java
+++ StringToInteger/src/StringToInteger.java
@@ -1,5 +1,62 @@
 
 public class StringToInteger {
+	
+	public int myAtoi(String str) {
+        // possible input:
+        // Valid input: 
+        // 1) float number: 2.33
+        // 2) integer number: 233
+        // 3) negative number: -1
+        // Invalid:
+        // 1) invalid char: 0 - 9 (30 - 39) and - (2D)
+        // 2) out of int range: 2^31 - 1 and - 2^32
+        // 3) invalid zero: 00002 || 00002.22
+        // 4) invalid '.' and '-' : 4.3.2, 4-3-2 
+        // 5) *** I didn't consider empty spaces
+        
+        // 1. first, consider valid input 233 - from right to left get ele from str and *10
+        // 2. second, consider float number: 2.33
+        
+        // https://discuss.leetcode.com/topic/12473/java-solution-with-4-steps-explanations
+        // I was thinking invalid input like float number, but according to the answer the inputs are limited as integer
+        // result: 39ms, beat 88.90%
+        // *** things learned: 1) use simple way to handle invalid input: digits[index] >= '0' && digits[index] <= '9'
+        // 2) handle overflow problem (when meet numbers (convert, algebra)), always think about overflow
+        
+        // edge cases
+        if (null == str || 0 == str.length())
+            return 0;
+        
+        int res = 0;
+        int sign = 1;
+        int index = 0;
+        // trim the string, eliminate spaces
+        str = str.trim();
+        char[] digits = str.toCharArray();
+        
+        // check signs whether it is + or -
+        // it might be the case that the string does not contain any signs
+        if (index < digits.length && (digits[index] == '-' || digits[index] == '+')) {
+            if (digits[index] == '-')
+                sign = -1;
+            index ++;
+        }
+        
+        // check overflow and check invalid char in the array
+        // *** invalid input:  digits[index] >= '0' && digits[index] <= '9' can use this to decide return 0 or not
+        while (index < digits.length && digits[index] >= '0' && digits[index] <= '9' ) {
+            
+            int temp = digits[index] - '0';
+            if (Integer.MAX_VALUE / 10 < res || ( Integer.MAX_VALUE / 10 == res && temp > 7) ) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE; 
+            }
+            
+            res = res * 10 + temp;
+            index ++;
+        }
+        
+        return sign * res;
+    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
