510c6b7d3620e6532f8af0af402abad3b360080e
Jingjie Jiang <chygojiang@gmail.com>
2017-04-21 23:27:16 +0100

    230. Kth Smallest Element in a BST JJJ Apr 21, 2017 // Round 1: **** used 1 hour, did not get the O(log(n)) solution // *** things learned: can only count the num of nodes as half

--- InsertDeleteGetRandom/src/InsertDeleteGetRandom.java
+++ InsertDeleteGetRandom/src/InsertDeleteGetRandom.java
@@ -1,4 +1,70 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 public class InsertDeleteGetRandom {
 
+	// round 1: 40 minutes, beat 90.25% 
+    // *** learn to keep original index in a list when remove an ele (swap with the last one)
+    // *** 1) learn how to get iterator in set
+    // *** do not use index - 1 here, as index won't exceed list.size()
+    
+    // idea: resvoir sampling for random
+    // O(1) complexity for random --> linkedlist can use i to identify a val
+    
+    private Map<Integer, Integer> map;
+    private List<Integer> list;
+
+    /** Initialize your data structure here. */
+    public InsertDeleteGetRandom() {
+        
+        map = new HashMap<Integer, Integer>();
+        list = new ArrayList<Integer>();
+        
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+        
+        if (map.containsKey(val) == true) {
+            return false;
+        }
+        else {
+            list.add(val);
+            map.put(val, list.size() - 1);
+            return true;
+        }
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        
+        if (map.containsKey(val) == true) {
+            int index = map.get(val);
+            // *** avoid index change: swap the replaced one with the last one, renew the map
+            // ref: https://discuss.leetcode.com/topic/53216/java-solution-using-a-hashmap-and-an-arraylist-
+            // along-with-a-follow-up-131-ms
+            if (index < list.size() - 1) {
+                int temp = list.get(list.size() - 1);
+                list.set(index, temp);
+                map.put(temp, index);
+            }
+            list.remove(list.size() - 1);
+            map.remove(val);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+    /** Get a random element from the set. */
+    public int getRandom() {
+        
+        int index = (int)(Math.random() * list.size());
+        // *** do not use index - 1 here, as index won't exceed list.size()
+        return list.get(index);
+    }
 }
---
+++ KthSmallestEletInBST/src/KthSmallestEletInBST.java
@@ -0,0 +1,38 @@
+
+public class KthSmallestEletInBST {
+	
+	 public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+        TreeNode(int x) { val = x; }
+     }
+	
+	 public int kthSmallest(TreeNode root, int k) {
+        // Round 1: **** used 1 hour, did not get the O(log(n)) solution
+        
+        // Idea: i thought about building a list (in-order), then search the kth in the list, O(n)
+        // 1. I was thinking to use bianry search, but stuck on get the whole nodes count of the tree --> can count only half
+        // The method only count half nodes of the tree
+        // https://discuss.leetcode.com/topic/17810/3-ways-implemented-in-java-python-binary-search-in-order-iterative-recursive
+        
+        if (null == root) 
+            return -1;
+        int nodes = count(root.left);
+        if (k <= nodes) {
+            return kthSmallest(root.left, k);
+        }
+        else if (k > nodes + 1) {
+            // *** I missed -1, 1 represent the root
+            return kthSmallest(root.right, k - nodes - 1);
+        }
+        
+        return root.val;
+    }
+    
+    public int count(TreeNode root) {
+        if (null == root)
+            return 0; 
+        return 1 + count(root.left) + count(root.right);
+    }
+}
