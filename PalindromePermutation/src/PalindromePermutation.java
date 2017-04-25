31cb85374165df409c3ca09331a0c7ec75307d80
Jingjie Jiang <chygojiang@gmail.com>
2017-04-25 22:17:38 +0100

    369. Plus One Linked List JJJ Apr 25, 2017 // round 1: 15 minutes         // idea: method 1: use stack, rs: 1ms
            // round 2
            // method 2: two pointers tech, recursive method

--- PalindromePermutation/src/PalindromePermutation.java
+++ PalindromePermutation/src/PalindromePermutation.java
@@ -1,4 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class PalindromePermutation {
-
+	public boolean canPermutePalindrome(String s) {
+        // round 1: *** iterate through a map
+        
+        // method 2: use set (faster)
+        // https://discuss.leetcode.com/topic/22057/java-solution-w-set-one-pass-without-counters
+        
+        // round 2: try set 
+        
+        // idea: hashmap, check even occurance of char + 1 odd
+        if (null == s) 
+            return false;
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        
+        for (int i = 0; i < s.length(); i ++) {
+            char temp = s.charAt(i);
+            int count = map.getOrDefault(temp, 0);
+            map.put(temp, count + 1);
+        }
+        
+        // for (Map.Entry<Character, Integer> entry : map.entrySet())
+        int odd = 0;
+        for (Character key : map.keySet()) {
+            if (map.get(key) % 2 != 0)
+                odd ++;
+            if (odd >= 2)
+                return false;
+        }
+        return true;
+    }
 }
