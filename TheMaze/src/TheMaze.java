ace18ea5a66bb19620026339eda8ab5e18779315
Jingjie Jiang <chygojiang@gmail.com>
2017-04-25 20:53:14 +0100

    505. The Maze II JJJ Apr 25, 2017 // 1. Time Limit Exceeded         // *** use weight >= status[des[0]][des[1]] in the program to solve the TLE problem     	// *** learned: when need to pick min , during BSF or DSF, check whether the temp data is larger then currrent         // min, if so, then quit the current loop to save time

--- TheMaze/src/TheMaze.java
+++ TheMaze/src/TheMaze.java
@@ -1,15 +1,23 @@
 
 public class TheMaze {
 
-	private static int[][] dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
     
+	private static int[][] dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+	    
     public static boolean hasPath(int[][] maze, int[] start, int[] destination) {
-        // round 1: spend 1.5 hours to understand the question (rolling not step by step)
+        // round 1: spend 1.5 hours to understand the question (rolling not step by step), spend more than 10 hours to get
+        // the solution (with refs)
+        // *** 1. I did not know how to control the ball to roll one diretion when it is not a stop point, then
+        // roll at different direction at a stop point
+        // --> at each step, just go straight forward to the end, until meet a stop point, then go into dfs loop
+        // ref: https://discuss.leetcode.com/topic/78133/simple-dfs-solution-beat-97
         
         // idea: dfs (keep the previous direction infor, use % to roll over)
         // *** 1. when think about how to write code, start from simple case, then to complicated ones
         // 1) one dimen array: 1 0 0 0 0 start --> when start reach 1 (1 is not end), how to get back to start
         // *** 2. stackOverFlow for termination condition
+        // did not change the start value at each round
+        
         if (null == maze || 0 == maze.length || 0 == maze[0].length || null == start 
                 || 0 == start.length || null == destination || 0 == destination.length)
             return false;
@@ -24,12 +32,11 @@ public class TheMaze {
         if (status[start[0]][start[1]] == true)
             return false;
         status[start[0]][start[1]] = true;
-        
         if ( (start[0] == des[0]) && (start[1] == des[1]) )
             return true;
         
         // core step
-         for (int i = 0; i < dirs.length; i ++) {
+        for (int i = 0; i < dirs.length; i ++) {
                 
             int[] tempStart = new int[start.length];
             tempStart[0] = start[0];
@@ -39,7 +46,8 @@ public class TheMaze {
                 tempStart[1] += dirs[i][1];
             }
             while ( tempStart[0] >= 0 && tempStart[0] < maze.length 
-                    && tempStart[1] >= 0 && tempStart[1] < maze[0].length);
+                    && tempStart[1] >= 0 && tempStart[1] < maze[0].length 
+                    && maze[tempStart[0]][tempStart[1]] == 0);
             
             // when quit the while loop, the val has already exceeded the normal val
             tempStart[0] -= dirs[i][0];
@@ -49,16 +57,42 @@ public class TheMaze {
         }
         
         return false;
+        /*
+        // core step
+        boolean res = false;
+        for (int i = 0; i < dirs.length; i ++) {
+                
+            int[] tempStart = new int[start.length];
+            tempStart[0] = start[0];
+            tempStart[1] = start[1];
+            do {
+                tempStart[0] += dirs[i][0];
+                tempStart[1] += dirs[i][1];
+            }
+            while ( tempStart[0] >= 0 && tempStart[0] < maze.length 
+                    && tempStart[1] >= 0 && tempStart[1] < maze[0].length 
+                    && maze[tempStart[0]][tempStart[1]] == 0);
+            
+            // when quit the while loop, the val has already exceeded the normal val
+            tempStart[0] -= dirs[i][0];
+            tempStart[1] -= dirs[i][1];
+            res = res | dfs(maze, status, tempStart, des);
+         }
+         return res;
+         */
     }
    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		/*
 		int[][] map = {{0,0,1,0,0},{0,0,0,0,0},{0,0,0,1,0},{1,1,0,1,1},{0,0,0,0,0}};
 		int[] s = {0,4};
 		int[] e = {1,2};
+		*/
+		int[][] map = {{0,0,0,0,0}};
+		int[] s = {0,4};
+		int[] e = {0,0};
 	    boolean res = hasPath(map, s, e);
-
 	}
 
 }
---
+++ TheMazeII/src/TheMazeII.java
@@ -0,0 +1,90 @@
+import org.omg.CORBA.VersionSpecHelper;
+
+
+public class TheMazeII {
+
+    private static int[][] dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    
+    public static int shortestDistance(int[][] maze, int[] start, int[] destination) {
+        // idea: use two dimentional array to keep the data
+        // 1) add the weight data into each round
+        
+        if (null == maze || 0 == maze.length || 0 == maze[0].length || null == start 
+                || 0 == start.length || null == destination || 0 == destination.length)
+            return -1;
+       
+        int[][] status = new int[maze.length][maze[0].length];
+        status[destination[0]][destination[1]] = Integer.MAX_VALUE;
+        
+        return dfs(maze, status, start, destination, 1) == true ? status[destination[0]][destination[1]]: -1;
+    }
+    
+    private static boolean dfs(int[][] maze, int[][] status, int[] start, int[] des, int length) {
+        
+    	/*
+    	// verison 1
+    	if (status[start[0]][start[1]] > 0) {
+    		if (length >= status[start[0]][start[1]])
+    			return false;
+    		else {
+    			status[start[0]][start[1]] = Math.min(status[start[0]][start[1]], length);
+			}
+        }
+    	*/
+    	
+    	// *** must be careful about length >= status[start[0]][start[1]], means if the current route is shorter, must go through the whole old route again
+    	// verison 2 --> refactoring of version 1
+    	if (status[start[0]][start[1]] > 0 && length >= status[start[0]][start[1]])
+    			return false;
+        status[start[0]][start[1]] = length;
+        
+    	if ( (start[0] == des[0]) && (start[1] == des[1]) ) {
+    		status[des[0]][des[1]] = Math.min(status[des[0]][des[1]], length);
+            return true;
+        }
+    	
+        // core step
+        boolean res = false;
+        for (int i = 0; i < dirs.length; i ++) {
+                
+            int[] tempStart = new int[start.length];
+            int weight = 0;
+            tempStart[0] = start[0];
+            tempStart[1] = start[1];
+            do {
+                tempStart[0] += dirs[i][0];
+                tempStart[1] += dirs[i][1];
+                weight ++;
+            }
+            while ( tempStart[0] >= 0 && tempStart[0] < maze.length 
+                    && tempStart[1] >= 0 && tempStart[1] < maze[0].length 
+                    && maze[tempStart[0]][tempStart[1]] == 0);
+            
+            // when quit the while loop, the val has already exceeded the normal val
+            tempStart[0] -= dirs[i][0];
+            tempStart[1] -= dirs[i][1];
+            weight --;
+            weight += length;
+            res = res | dfs(maze, status, tempStart, des, weight);
+        }
+        
+        return res;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int[][] map = {{0,0,1,0,0},{0,0,0,0,0},{0,0,0,1,0},{1,1,0,1,1},{0,0,0,0,0}};
+		int[] s = {0,4};
+		int[] e = {4,4};
+		
+		/*
+		int[][] map = {{0,0,1,0,0},{0,0,0,0,0},{0,0,0,1,0}};
+		int[] s = {0,4};
+		int[] e = {1,0};
+		*/
+	    int shortest = shortestDistance(map, s, e);
+	    System.out.print(shortest);
+	}
+
+}
