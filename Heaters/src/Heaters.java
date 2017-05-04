65bec8d2b2481c878163f8f3f074de37b6693233
Jingjie Jiang <chygojiang@gmail.com>
2017-05-04 22:25:10 +0100

    359. Logger Rate Limiter JJJ May 4, 2017 // round 1: 15 minutes, solved

--- Heaters/src/Heaters.java
+++ Heaters/src/Heaters.java
@@ -3,11 +3,51 @@ import java.util.Arrays;
 
 public class Heaters {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] arr = {2};
-		
-        int a = Arrays.binarySearch(arr, 3);
-	}
+	public int findRadius(int[] houses, int[] heaters) {
+        // round 1: unsolved, did not think about fit house to heaters one by one when use binary search
+        // idea: DP like solution
+        // 1) brutal force: use heaters to check all the houses --> O(m(length of houses)*n(length of heaters))
+        // 2) improve --> binary search find the place where a <= heater <= c --> O(nlogm)
+        
+        // method 1: binary search. find the each house in the heater for distance
+        // ref: https://discuss.leetcode.com/topic/71460/short-and-clean-java-binary-search-solution/2
+        // run time: 30ms
+        // *** the behaviour of Arrays.bianrySearch() {2,4}, search 3, return -3
+        /*
+        Arrays.sort(heaters);
+        
+        int max = 0;
+        
+        for (int house : houses) {
+        
+            int pos = Arrays.binarySearch(heaters, house);
+            if (pos < 0)
+                pos = - (pos + 1);
+            
+            int left = pos - 1 >= 0 ? house - heaters[pos - 1] : Integer.MAX_VALUE;
+            int right = pos < heaters.length ? heaters[pos] - house : Integer.MAX_VALUE;
+            
+            max = Math.max(max, Math.min(left, right));
+        }
+        
+        return max;
+        */
+        // method 2: two pointers tech. Search each heater and the next one, so that can decided the max from house to house
+        // refs: https://discuss.leetcode.com/topic/71450/simple-java-solution-with-2-pointers/2
+        // run time: 20ms
+        Arrays.sort(heaters);
+        Arrays.sort(houses);
+        
+        int max = 0, i = 0;
+        for (int house : houses) {
+            // *** condition heaters[i] + heaters[i]+1 <= house*2 is very hard to find
+            while (i < heaters.length - 1 && heaters[i] + heaters[i + 1] <= house * 2) {
+                i ++;
+            }
+            max = Math.max(max, Math.abs(heaters[i] - house));
+        }
+        
+        return max;
+    }
 
 }
---
+++ LoggerRateLimiter/src/LoggerRateLimiter.java
@@ -0,0 +1,50 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class LoggerRateLimiter {
+	
+	// round 1: *** did not test the case that the message may come at time 0 (I thought about it when setting default value
+    // but I did not check)
+    // result: 165ms, beat 79.97%
+
+    private Map<String, Integer> map; 
+    
+    /** Initialize your data structure here. */
+    public LoggerRateLimiter() {
+        map = new HashMap<String, Integer>();
+    }
+    
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        
+        if (null == message)
+            return false;
+        
+        int preTime = map.getOrDefault(message, -1);
+        if (preTime == -1) {
+            map.put(message, timestamp);
+            return true;
+        }
+        else if (timestamp - preTime < 10) {
+            return false;
+        }
+        // timestamp - preTime >= 10
+        else {
+            map.put(message, timestamp);
+            return true;
+        }
+        
+        /*
+        // *** a more delicate implementation: 
+        // https://discuss.leetcode.com/topic/48359/short-c-java-python-bit-different/2
+        if (timestamp < ok.getOrDefault(message, 0))
+            return false;
+        ok.put(message, timestamp + 10);
+        return true;
+        */
+    }
+
+}
