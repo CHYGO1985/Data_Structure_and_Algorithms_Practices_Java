667e3e8f10f1fa4ad7f61c692e89be250b340568
Jingjie Jiang <chygojiang@gmail.com>
2017-05-05 12:54:26 +0100

    379. Design Phone Directory JJJ May 5, 2017 round 1: The question itself is confusing, it does not required get phone number in random style, it can just start from 0. 413ms: beat 93.52%

--- DesignHitCounter/src/DesignHitCounter.java
+++ DesignHitCounter/src/DesignHitCounter.java
@@ -1,4 +1,76 @@
+import java.util.LinkedList;
+import java.util.List;
+
 
 public class DesignHitCounter {
-
+	// round 1: 50 minutes, 94ms beat 93.73%
+    // method 1: list --> to keep the time line information (if hits come at the same time, they must cal at 
+    // the last ele of the queue)
+    
+    // 1. *** Queue is an Interface in Java. Methods: 
+    // 	Throws exception	Returns special value
+    /*
+            Insert	add(e)	offer(e)
+            Remove	remove()	poll()
+            Examine	element()	peek()
+    */
+    // 2. *** initialise linkedlist of arrays new LinkedList<int[]>();
+    // do not do it like: new LinkedList<new int[2]>()
+    
+    // method 2:
+    // this method is not good, if gethits is used to check historical data
+    // ref: https://discuss.leetcode.com/topic/48758/super-easy-design-o-1-hit-o-s-gethits-no-fancy-data-structure-is-needed/2
+    
+    // current total hits count in the linkedhashmap
+    int sum;
+    // 1st Integer: timestamp, 2nd Integer: hits at the timestamp
+    List<int[]> list;
+    
+    /** Initialize your data structure here. */
+    public DesignHitCounter() {
+        sum = 0;
+        list = new LinkedList<int[]>();
+    }
+    
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    public void hit(int timestamp) {
+        
+        // *** I did not consider empty case
+        if (list.isEmpty() == false) {
+            int[] preHits = list.get(list.size() - 1);
+            if (preHits[0] == timestamp) {
+                preHits[1] += 1;
+                // *** this sum ++ is easy to ignore
+                sum ++;
+                return ;
+            }
+        }
+    
+        int[] newHits = new int[2];
+        newHits[0] = timestamp;
+        newHits[1] = 1;
+        list.add(newHits);
+    
+        sum ++;
+    }
+    
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    public int getHits(int timestamp) {
+        
+        int tempSum = sum; 
+        for (int i = 0; i < list.size(); i ++) {
+            int[] hits = list.get(i);
+            if (timestamp - hits[0] < 300) {
+                return tempSum;
+            }
+            else {
+                tempSum -= hits[1];
+            }
+        }
+        
+        // it may be the case that tempSum = 0
+        return tempSum;
+    }
 }
---
+++ DesignPhoneDirectory/src/DesignPhoneDirectory.java
@@ -0,0 +1,10 @@
+
+public class DesignPhoneDirectory {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = new int[3];
+		int b = a.length;
+	}
+
+}
