4f18f220e58557123f63767d55efbdac79a3a98b
Jingjie Jiang <jingjiejiang85@gmail.com>
2017-05-14 00:07:57 +0100

    249. Group Shifted Strings JJJ May 14, 2017 Refactoring

--- GroupShiftedStrings/src/GroupShiftedStrings.java
+++ GroupShiftedStrings/src/GroupShiftedStrings.java
@@ -7,8 +7,12 @@ import java.util.Map;
 public class GroupShiftedStrings {
 
 	public List<List<String>> groupStrings(String[] strings) {
+        // round 2: use less line implementation
+        
         // round 1: sovled, 40m. 7ms, beat 46%
-        // improve: without sorting, 5ms, beat 67.16%
+        // improve 1: without sorting, 5ms, beat 67.16%
+        // improve 2: refactoring add ele to list: 4ms, beat 77% 
+        // String used more time than StringBuilder
         // idea: method 1
         // 1. (sort will make it slow )must be the same length (when it is 1, then all the same group) --> sort the array 
         // (otherwise need to check length with strings in the map every time, worst case O(n^2) (none of repeat))
@@ -18,6 +22,25 @@ public class GroupShiftedStrings {
         // 3) list to store result
         
         // *** java: (char)(cur - pattern) char + int = int, need to use(char)(char + int) = char
+        // *** map.values())
+        
+        // more concise implementation use Map<String, List<String>>
+        // ref:  https://discuss.leetcode.com/topic/35088/12-lines-java-solution-with-explanation
+        /*
+        Map<String,List<String>> map = new HashMap<>();
+        for(String str : strs) {
+            String key = "";
+            char first = str.charAt(0);
+            for(char c:str.toCharArray())
+                key+=(c-first<0?c-first+26:c-first)+",";
+            if(!map.containsKey(key))
+                map.put(key,new ArrayList<String>());
+            map.get(key).add(str);
+        }
+        for(String key:map.keySet())
+            Collections.sort(map.get(key));
+        return new ArrayList<List<String>>(map.values());
+        */
         
         List<List<String>> list = new LinkedList<List<String>>();
         if (null == strings || 0 == strings.length) 
@@ -49,6 +72,7 @@ public class GroupShiftedStrings {
             String pattern = builder.toString();
             
             // if have new pattern add to map and add a new list item
+            /*
             int index = map.getOrDefault(pattern, -1);
             List<String> tempList;
             if (index == -1) {
@@ -63,6 +87,18 @@ public class GroupShiftedStrings {
                 tempList.add(strings[i]);
                 // *** the tempList already exist, so use list.add() will create duplicate
             }
+            */
+            
+            // refacoring to
+            int index = map.getOrDefault(pattern, -1);
+            List<String> tempList;
+            if (index == -1) {
+                tempList = new LinkedList<String>();
+                list.add(tempList);
+                map.put(pattern, list.size() - 1);
+            }
+            tempList = list.get(map.get(pattern));
+            tempList.add(strings[i]);
         }
         
         return list;
