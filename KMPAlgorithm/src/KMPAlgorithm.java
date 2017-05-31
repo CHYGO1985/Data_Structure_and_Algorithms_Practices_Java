af9dccb8f80747a8d4ae47ae8ad527947fbfc86e
Jingjie Jiang <jingjiejiang85@gmail.com>
2017-05-31 17:34:42 +0100

    KMP Algorithm JJJ May 31, 2017

---
+++ KMPAlgorithm/src/KMPAlgorithm.java
@@ -0,0 +1,36 @@
+
+/**
+ * The implementation of KMP Algorithm.
+ * 
+ * @author jingjiejiang
+ * @history JJJ May 31, 2017
+ */
+public class KMPAlgorithm {
+	
+	public static int[] KMPAlgorithm(String s) {
+		
+		int len = s.length();
+		int[] next = new int[len];
+		int prefix = -1;
+		int suffix = 0;
+		next[suffix] = prefix;
+		
+		while (suffix < len - 1) {
+			if (prefix == -1 || s.charAt(prefix) == s.charAt(suffix)) {
+				prefix ++;
+				suffix ++;
+				next[suffix] = prefix;
+			}
+			else {
+				prefix = next[prefix];
+			}
+		}
+		
+		return next;
+	}
+
+	public static void main(String[] args) {
+		int[] next = KMPAlgorithm("catacbbcatac");
+	}
+
+}
