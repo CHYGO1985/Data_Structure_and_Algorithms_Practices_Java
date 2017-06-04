32bcdb4f172b2c05ce7c04bcd34970cd1dfa88fe
Jingjie Jiang <jingjiejiang85@gmail.com>
2017-06-04 23:42:43 +0100

    245. Shortest Word Distance III /**  * round 1: 10m, 2ms, beat 74%  *  * @author jingjiejiang  * @history Jun 4, 2017  */

---
+++ ShortestWordDistanceIII/src/ShortestWordDistanceIII.java
@@ -0,0 +1,25 @@
+/**
+ * round 1: 10m, 2ms, beat 74%
+ * 
+ * @author jingjiejiang
+ * @history Jun 4, 2017
+ */ 
+public class ShortestWordDistanceIII {
+	public int shortestWordDistance(String[] words, String word1, String word2) {
+        
+        int distance = Integer.MAX_VALUE;
+        if (null == words || words.length == 0) return -1;
+        boolean isSame = (word1.equals(word2) == true) ? true : false;
+        
+        int index = -1, minDistance = Integer.MAX_VALUE;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(word1) || words[i].equals(word2)) {
+                if (index != -1 && ( words[index].equals(words[i]) == false || isSame == true)) {
+                    minDistance = Math.min(minDistance, i - index);
+                }
+                index = i;
+            }
+        }
+       return minDistance;
+    }
+}
