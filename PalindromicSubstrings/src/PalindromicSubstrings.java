c9e883cc9dba72ea8b6efd4b93b982a1ce87dc8b
Jingjie Jiang <jingjiejiang85@gmail.com>
2017-07-27 23:41:00 +0100

    647. Palindromic Substrings Add DP solution. Jul 27, 2017

--- PalindromicSubstrings/src/PalindromicSubstrings.java
+++ PalindromicSubstrings/src/PalindromicSubstrings.java
@@ -1,3 +1,4 @@
+
 /**
  * 
  * 647. Palindromic Substrings
@@ -11,12 +12,37 @@
  * 
  * method 3: DP (implement later)
  * ref: https://discuss.leetcode.com/topic/96821/java-dp-solution-based-on-longest-palindromic-substring
+ * DP: from brutal force solution (O(n^3)) to recursive -> to DP (save intermediate status)
+ * Brutal force: i: n -1 -> 0; j : i -> n; for each i and j, check whether it is a palindrome, so it is O(n^3)
+ * from brutal force, it is noticed that many i--j will be recalculated during brutal force, so use DP to save
+ * the status
  *
  * @author jingjiejiang
  * @history 
  * 1. Jul 27, 2017
  */
 public class PalindromicSubstrings {
+	
+	// DP solution
+	public int countSubstrings(String s) {
+        
+        // DP Algorithm
+        int sum = 0;
+        
+        if (null == s || 0 == s.length()) return sum;
+        int len = s.length();
+        
+        boolean[][] dp = new boolean[len][len];
+        
+        for (int i = len - 1; i >= 0; i --) {
+            for (int j = i; j < len; j ++) {
+                dp[i][j] = (s.charAt(i) == s.charAt(j) && (j - i < 3 || dp[i + 1][j - 1] == true));
+                if (dp[i][j] == true) sum ++;
+            }
+        }
+        
+        return sum;
+	}
 	
 	// manacher's algorithm
 	/*
@@ -77,5 +103,28 @@ public class PalindromicSubstrings {
         return indicator;
     }
     */
+	
+	public static int longestPalindromeSubseq(String s) {
+        return helper(s, 0, s.length() - 1, new Integer[s.length()][s.length()]);
+    }
+    
+    private static int helper(String s, int i, int j, Integer[][] memo) {
+        if (memo[i][j] != null) {
+            return memo[i][j];
+        }
+        if (i > j)      return 0;
+        if (i == j)     return 1;
+        
+        if (s.charAt(i) == s.charAt(j)) {
+            memo[i][j] = helper(s, i + 1, j - 1, memo) + 2;
+        } else {
+            memo[i][j] = Math.max(helper(s, i + 1, j, memo), helper(s, i, j - 1, memo));
+        }
+        return memo[i][j];
+    }
+    
+    public static void main(String[] args) {
+    	int a = longestPalindromeSubseq("abca");
+    }
     
 }
