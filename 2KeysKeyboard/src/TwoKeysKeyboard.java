5c306738d02a71bf4f446914b109dd579012e5eb
Jingjie Jiang <jingjiejiang85@gmail.com>
2017-08-04 01:09:37 +0100

    650. 2 Keys Keyboard Add more detailed comments.

--- 2KeysKeyboard/src/TwoKeysKeyboard.java
+++ 2KeysKeyboard/src/TwoKeysKeyboard.java
@@ -3,6 +3,13 @@
  * 650. 2 Keys Keyboard
  *
  * round 1: solved, 40 minutes, O(n^2) solution, beat 64.15%
+ * DP: 1) if n is even: dp[j / 2] + 2; (copy the closet dp[x])
+ * the theory is: take 8 as example,as 4 is from 2, if only copy 2 to reach 8, 
+ * then the another 4 will cost more than 2 for sure, but if copy 4 if will cost
+ * another 2, so just use dp[j / 2]
+ * 
+ * 2) odd: from big to small check j % m == 0, then dp[j] = dp[m] + 2 + (m - 2)
+ * --> dp[j] = dp[m] + m
  * 
  * @author jingjiejiang
  * @history 
@@ -26,6 +33,9 @@ public class TwoKeysKeyboard {
                 int limit = (int)Math.sqrt(j);
                 while (m <= limit) {
                     if (j % m == 0) {
+                    	// the first 2 means 2 steps copy all and paste
+                    	// the second 2 means 2 m, the dp[j/m] produces the first m, 
+                    	// then 2 produce the second m
                         dp[j] = Math.min(dp[j], dp[j / m] + 2 + m - 2);
                     }
                     m ++;
