9b809950bb1a44b8643fe8a144d298cacfe0902e
Jingjie Jiang <jingjiejiang85@gmail.com>
2017-08-08 23:54:37 +0100

    62. Unique Paths Add one dimnensional dp array solution.

--- UniquePaths/src/UniquePaths.java
+++ UniquePaths/src/UniquePaths.java
@@ -4,16 +4,18 @@
  * 
  * round 1: solved, 25 minutes, typical DP problem
  * 
+ * *** I used 2 dimensional dp array, but it can use 1 dimensional dp array
+ * 
  * @author jingjiejiang
  * @history 
  * 1. Aug 8, 2017
  */
 public class UniquePaths {
 
+	/*
 	public int uniquePaths(int m, int n) {
         // dp[row][col] = dp[row - 1][col] + dp[row][col - 1]
         // (row >= 0 && row < m) && (col >= 0 && col < n)
-        
         int[][] dp = new int[m][n];
         
         if (m < 0 || n < 0) return 0;
@@ -32,5 +34,27 @@ public class UniquePaths {
         }
         
         return dp[m - 1][n - 1];
+    } 
+    */
+	
+	public int uniquePaths(int m, int n) {
+        
+        // one dimensional dp
+        if (m < 0 || n < 0) return 0;
+        if (m == 0 || n == 0) return 1;
+        
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i ++)
+            dp[i] = 1;
+        
+        int row = 1;
+        while (row < m) {
+            for (int col = 0; col < n; col ++) {
+                if (col > 0) dp[col] += dp[col -1];
+            }
+            row ++;
+        }
+        
+        return dp[n -1];
     } 
 }
