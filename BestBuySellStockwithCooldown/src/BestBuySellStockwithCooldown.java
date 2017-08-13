df12e360708636aec13c9aa753aca22ea3367c76
Jingjie Jiang <jingjiejiang85@gmail.com>
2017-08-13 21:42:48 +0100

    309. Best Time to Buy and Sell Stock with Cooldown /**  *  * 309. Best Time to Buy and Sell Stock with Cooldown  *  * round 1: unsolved, 4 hours  * *** I didn't consider buy and sell in one  day--> so do not need to keep the prev min price records  * did not need to consider prices[i] - prices[i - 2] cases: 1 3 5, cause can sell and buy in one day  *  * The reason why I went to wrong direction is that I went straight to find dp formula, which in this question  * it is not available  *  * ref: https://discuss.leetcode.com/topic/31349/7-line-java-only-consider-sell-and-cooldown/2  *  *  * @author jingjiejiang  * @history  * 1. Aus 13, 2017  */

--- BestBuySellStockwithCooldown/src/BestBuySellStockwithCooldown.java
+++ BestBuySellStockwithCooldown/src/BestBuySellStockwithCooldown.java
@@ -1,4 +1,19 @@
-
+/**
+ * 
+ * round 1: unsolved, 4 hours
+ * *** I didn't consider buy and sell in one  day--> so do not need to keep the prev min price records
+ * did not need to consider prices[i] - prices[i - 2] cases: 1 3 5, cause can sell and buy in one day
+ * 
+ * The reason why I went to wrong direction is that I went straight to find dp formula, which in this question
+ * it is not available
+ * 
+ * ref: https://discuss.leetcode.com/topic/31349/7-line-java-only-consider-sell-and-cooldown/2
+ * 
+ * 
+ * @author jingjiejiang
+ * @history 
+ * 1. Aus 13, 2017
+ */
 public class BestBuySellStockwithCooldown {
 	
 	public static int maxProfit(int[] prices) {
@@ -6,17 +21,9 @@ public class BestBuySellStockwithCooldown {
         // partial best option --> reach the final best option
         // take first three as example:
         
-        // 1 2 1 2
-        // 0 3 4 5 3 2
-        // 0 3 4 5 5 5
-        // 0 0 0 0 0 0
-        
-        // 1 2 3 2 5
-        // 0 1 2 2 4
-        // 1 1 1 1 1
-        
-        // 1 2 3 0 2
-        // 0 1 2 2 3
+	    //       1 2 3 0 2
+        // sum1: 0 1 2 1 3
+        // sum2: 0 0 1 2 2
         
         // from matrix
         // dp[i] = price[i] - min stock; (if prices[i] > min.stock)
@@ -28,40 +35,24 @@ public class BestBuySellStockwithCooldown {
         
         int len = prices.length;
         
-        int[] priceIdx = new int[len];
-        int[] max = new int[len + 1];
-        
-        int minPriceIdx = 0; 
-        priceIdx[0] = 0;
-        max[0] = 0;
-        max[1] = 0;
+        int sum1 = 0;
+        int sum2 = 0;
         
         for (int i = 1; i < len; i ++) {
-            int maxIndex = i + 1;
-            if (prices[i] < prices[minPriceIdx]) minPriceIdx = i;
-            
-            if (prices[i] - prices[i - 1] > 0) {
-                max[maxIndex] = prices[i] - prices[i - 1];
-                if (maxIndex >= 3) max[maxIndex] += max[maxIndex - 3];
-            }
-            else {
-                max[maxIndex] = max[i];
-            }
-            
-            if (minPriceIdx != i - 1) {
-                int temp = prices[i] - prices[minPriceIdx];
-                if (minPriceIdx >= 2) temp += max[minPriceIdx - 2 + 1];
-                max[maxIndex]= Math.max(max[maxIndex], temp);
-            }
-                
-        }
+            int temp = sum1;
+            //       1 2 3 0 2
+            // sum1: 0 1 2 1 3 : sum1: means sell and buy
+            // sum2: 0 0 1 2 2 : sum2: do nothing
+            // when it is at 0, 1 means: if sell things, the max is previous nothing
+            // sum2: means current round, do nothing, the max will be the last round sell things
+            sum1 = Math.max(sum1 + prices[i] - prices[i - 1], sum2);
+            sum2 = Math.max(temp, sum2);
+        }     
         
-        return max[len];
+        return Math.max(sum1, sum2);
     }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int a = maxProfit(new int[]{2,1,2,1,0,1,2});
+		int a = maxProfit(new int[]{1,2,3,0,2});
 	}
-
 }
