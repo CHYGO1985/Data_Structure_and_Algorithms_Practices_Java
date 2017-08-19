b65a1b351b15a6be0aef493e071fb28d6b748cda
Jingjie Jiang <jingjiejiang85@gmail.com>
2017-08-19 23:59:04 +0100

    375. Guess Number Higher or Lower II incomplete

---
+++ GuessNumHigherOrLowerII/src/GuessNumHigherOrLowerII.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * round 1: 
+ * 
+ * idea: from divide two to divide three
+ * 
+ * 1) cann't find any regulation when draw from 1 to 14
+ * 2) so go back to try to find the best strategy first
+ * 
+ * @author jingjiejiang
+ *
+ */
+public class GuessNumHigherOrLowerII {
+
+}
