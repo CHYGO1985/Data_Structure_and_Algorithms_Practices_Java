cca4862e0a48d2ff6c26d4ffdfd873e4f62e784e
Jingjie Jiang <jingjiejiang85@gmail.com>
2017-08-20 23:59:47 +0100

    thougths.

--- GuessNumHigherOrLowerII/src/GuessNumHigherOrLowerII.java
+++ GuessNumHigherOrLowerII/src/GuessNumHigherOrLowerII.java
@@ -7,6 +7,19 @@
  * 1) cann't find any regulation when draw from 1 to 14
  * 2) so go back to try to find the best strategy first
  * 
+ * 1) the best strategy is binary search and I did not explore it well for best strategy
+ * and then I went for divide for 3 parts, however, I still can't find any regulation.
+ * 
+ * at the begining, from 1 to 14, I only find out that compare the n-1 + n- 3 with n- 3 + min[n - 4], however it 
+ * cannot be scaled to larger number like 50.
+ * 
+ * 2) I go back to binary search
+ * for 1 2 3 4 5 6 7 8 9 10 11 12 13 14
+ * I find that the matching point is 13 --> 13 can identify 12 and 14, so next point to divide the part is 11
+ * 8 9 10 *11* 12 13 14, 
+ * 3) and 11 it still does not cover all eles so go to next point, which is 7, so 1 2 3 4 5 6 *7* 8 9 10 11 12 13 14
+ * so 7 is the
+ * 
  * @author jingjiejiang
  *
  */
