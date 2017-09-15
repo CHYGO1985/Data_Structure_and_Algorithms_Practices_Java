a6ca04294a0dcba8141c35f93c2dcc0b5847c421
Jingjie Jiang <jingjiejiang85@gmail.com>
2017-09-15 18:29:18 +0800

    467. Unique Substrings in Wraparound String  * @author jingjiejiang  * @history  * 1. Sep 15, 2017

--- UniqueSubstrInWrapString/src/UniqueSubstrInWrapString.java
+++ UniqueSubstrInWrapString/src/UniqueSubstrInWrapString.java
@@ -1,23 +1,78 @@
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * 
- * round 1: 
+ * round 1: 5 hours, solved, 2 dimensional dp, 300ms
+ * idea: 1. brutal force. first find out there is no math solution so it is a recursive dp.
+ * I first tried brutal force, construct all the substrings as : "a-b-3"
+ * start char - end char - length. add it to the set, then check the length of
+ * the set.
  * 
- * idea: dp + hashmap
- * method 1: used only hashset, tle 12/81
+ * 2. dp + hashset. The problem of brutal force obviously is TLE. so i think about use 
+ * dp + hashset to avoid checking repeated substrings. The idea is:
+ * dp[startChar][endChar(int value)] = length of the sub string.
+ * e.g. abc abcabc
+ * first abc, then dp[0][2] = 2 - 0 = 2
+ * second abcabc, coz 6 - 1 = 5 > dp[0][2] (3), so update dp[0][2] = 5
+ * otherwise, if <= dp[startChar][endChar], then it means we have already
+ * examine the sub string. e.g. abcabc -> dp[0][2] = 5,
+ * then if abc, 2 < dp[0][2], means abc has already counted
  * 
- * method 2: dp + hashset, to prevent count repeated string in p, 79/81
+ * 3. only dp. from previous method, i further think about whether it is possible to 
+ * avoid using hashset. coz the question only ask the number of substring not the 
+ * exact substring. so i think according to the feature
+ * a b c d
+ * from end to start cb cba get two new substrings,
+ * d: cd bcd abcd 
+ * so the regulation is from end to start, the new substrings num to (i - 1)
+ * and i can use dp[startchar][endchar] to check whether the new substring has been
+ * counted.
+ * so there comes the method 3, get rid of hashset, use only dp.
+ * if new substring has a value in dp, then skip, otherwise, go inside to count the
+ * new substrings.
  * 
- * method 3: only dp array, 300ms
+ * 4. *** 2 dimensional substring to 1 dimensional substring. get rid of star char, only count
+ * end char.
+ * The regulation is:
+ * for every char in a continuous increasing string, "abcd", the max num of substrings of 
+ * involved end char, is the pos that end char at in the increasing string.
+ * e.g. c is at 3, so substring end with c is abc, bc c.
+ * d is at 4, so substrings end with d is abcd, bcd, cd, d
  * 
- * method 4
+ * then we consider repeated situation: abcdabcd, for repeated d, we just update the num of
+ * substrings end with d with is 8 for abcdabcd. dp['d' - 'a'] = max(8, dp['d' - 'a'])
+ * Here is the reason why we can use dp only with end char.
+ * ref: https://discuss.leetcode.com/topic/70658/concise-java-solution-using-dp/2
  * 
  * @author jingjiejiang
- *
+ * @history
+ * 1. Sep 15, 2017
  */
 public class UniqueSubstrInWrapString {
+	
+	// ref: https://discuss.leetcode.com/topic/70658/concise-java-solution-using-dp/2
+	public static int findSubstringInWraproundString(String p) {
+        
+        if (null == p || 0 == p.length()) return 0;
+        int[] dp = new int[26];
+        int sum = 0;
+        int maxLen = 1;
+        
+        for (int i = 0; i < p.length(); i ++) {
+            
+            int index = p.charAt(i) - 'a';
+            dp[index] = Math.max(dp[index], maxLen);
+            
+            if (i + 1 < p.length() && ((index + 1) % 26 == (p.charAt(i + 1) - 'a')))
+                maxLen ++;
+            else 
+                maxLen = 1;
+        }
+        
+        for (int i = 0; i < 26; i ++)
+            sum += dp[i];
+        
+        return sum;
+    }
 
 	// method 2: dp + hashset
 	/*
@@ -90,6 +145,7 @@ public class UniqueSubstrInWrapString {
     }
     */
 	
+	/*
     public static int findSubstringInWraproundString(String p) {
         
         if (null == p || 0 == p.length()) return 0;
@@ -103,9 +159,6 @@ public class UniqueSubstrInWrapString {
         // iterate through p to find the increasing strings
         int start = 0;
         int end = 0;
-        
-        System.out.println(p.charAt(26));
-        System.out.println(p.charAt(27));
         
         for (int i = 0; i < p.length(); i ++) {
         
@@ -124,8 +177,9 @@ public class UniqueSubstrInWrapString {
                 // use haspmap to prevent counting repeating string
                 int tempStart = p.charAt(start) - 'a';
                 int tempEnd = p.charAt(end) - 'a';
-                if ( (end - start) > dp[tempStart][tempEnd]) 
+                if ( (end - start) > dp[tempStart][tempEnd]) {
                     sum += countString(start, end, p, dp);
+                }
                 start = i + 1;
                 end = start;
             }
@@ -156,8 +210,9 @@ public class UniqueSubstrInWrapString {
         }
         return count;
     }
+    */
     
     public static void main(String[] args) {
-    	findSubstringInWraproundString("rstuvwxyzabcdefghijklmnopqrcdefghijklmnopqr");
+    	findSubstringInWraproundString("abcd");
     }
 }
