2b691ef26828280a75a912ac05c54d16e5c1242e
Jingjie Jiang <jingjiejiang85@gmail.com>
2017-09-26 23:30:33 +0800

    611. Valid Triangle Number  * @author jingjiejiang  * @history  * 1. Sep 26, 2017

--- ValidTriangleNumber/src/ValidTriangleNumber.java
+++ ValidTriangleNumber/src/ValidTriangleNumber.java
@@ -1,13 +1,42 @@
+import java.util.Arrays;
+
 
 /**
  * 
  * 611. Valid Triangle Number
  * 
- * dfs?
+ * round 1: unsolved, do not know how to optimise from O(n^3) brutal force to O(n^2)
+ * *** array --> two pointers tech(same sides or frond + end)
+ * ref: https://discuss.leetcode.com/topic/92099/java-o-n-2-time-o-1-space/2
+ * 
+ * test case: 
+ * *** [1,1,1]
+ * [0,0,0]
  * 
  * @author jingjiejiang
- * 
+ * @history
+ * 1. Sep 26, 2017
  */
 public class ValidTriangleNumber {
 
+public int triangleNumber(int[] nums) {
+        
+        if (null == nums || nums.length < 3) return 0;
+        
+        Arrays.sort(nums);
+        
+        int sum = 0, n = nums.length;
+        for (int i = n - 1; i >= 2; i --) {
+            int l = 0, r = i-1;
+            while (l < r) {
+                if (nums[l] + nums[r] > nums[i]) {
+                    sum += r - l;
+                    r --;
+                }
+                else l ++;
+            }
+        }
+            
+        return sum;
+    }
 }
