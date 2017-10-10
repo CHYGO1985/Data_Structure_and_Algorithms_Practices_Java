5e18b85aa95d171911bb48f59df40b67c25ba88e
Jingjie Jiang <jingjiejiang85@gmail.com>
2017-10-10 17:30:24 +0800

    94. Binary Tree Inorder Traversal  * @author jingjiejiang  * @history  * 1. round 2 Oct 10, 2017

--- BinaryTreeInorderTraversal/src/BinaryTreeInorderTraversal.java
+++ BinaryTreeInorderTraversal/src/BinaryTreeInorderTraversal.java
@@ -2,7 +2,23 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
-
+/**
+ * 
+ * 94. Binary Tree Inorder Traversal
+ * 
+ * round 2: 25m, more concise solution
+ * idea: mimic recursive process
+ * 1) fist find the leftmost node
+ * 2) add the value of such node to list
+ * 3) check right of the node, if it is not null, go to 1)
+ * otherwise,go to 4)
+ * 4) pop a node from stack, add its value to list, go to 3
+ * 5) until find one's right child is not null, go to 1)
+ * 
+ * @author jingjiejiang
+ * @history
+ * 1. round 2 Oct 10, 2017
+ */
 public class BinaryTreeInorderTraversal {
 	
 	public static class TreeNode {
@@ -12,6 +28,40 @@ public class BinaryTreeInorderTraversal {
 		TreeNode(int x) { val = x; }
     }
 	
+	// round 2 implementation
+	public List<Integer> inorderTraversal(TreeNode root) {
+        
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        List<Integer> list = new LinkedList<Integer>();
+        
+        if (root == null) return list;
+        
+        while (root != null) {
+            
+            // get left until left is null
+            while (root.left != null) {
+                stack.push(root);
+                root = root.left;
+            }
+
+            // add current node value to list
+            list.add(root.val);
+
+            // check whether current node has right, if so add to stack
+            // else pop a node from stack (1.add value to list
+            // 2. check whether has right child 
+            while (root.right == null && stack.empty() == false) {
+                root = stack.pop();
+                list.add(root.val);
+            }
+            
+            root = root.right;
+        }
+        
+        return list;
+    }
+	
+	/*
 	public static List<Integer> inorderTraversal(TreeNode root) {
         // * idea: recursive --> stack, iterative --> explicitly use stack to implement the recursive inorder
         // * result : solved, 60 minutes, 1ms, beat 36.83% in the db, fastest in the db is 1ms, 3.3%
@@ -51,10 +101,6 @@ public class BinaryTreeInorderTraversal {
         
         return list;
     }
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	*/
 
 }
