0d71b7a861866f87c3ad86be579aa1cb026b6520
Jingjie Jiang <jingjiejiang85@gmail.com>
2017-10-28 21:36:39 +0800

    367. Valid Perfect Square  * @author jingjiejiang  * @history  * 1. Oct 28, 2017

---
+++ ValidPerfectSquare/src/ValidPerfectSquare.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ * 367. Valid Perfect Square
+ * 
+ * round 1: solved, 40m, test case: num = 1
+ * 
+ * @author jingjiejiang
+ * @history
+ * 1. Oct 28, 2017
+ */
+public class ValidPerfectSquare {
+	public boolean isPerfectSquare(int num) {
+        
+        int left = 1;
+        int right = num / 2;
+        // give num the value num for case when num = 1
+        int res = num;
+            
+        while (true) {
+            // *** should add this condition
+            if (left > right) break;
+            
+            int mid = left + (right - left) / 2;
+            if (num / mid > mid) {
+                left = mid + 1;
+            }
+            else if (num / mid < mid){
+                right = mid - 1;
+            }
+            else {
+                res = mid;
+                break;
+            } 
+        }
+        return res * res == num ? true : false;
+	}
+}
