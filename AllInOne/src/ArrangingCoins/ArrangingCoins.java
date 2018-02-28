e1ca950a2d2316e1ee90642975da6a441c1598de
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-02-28 23:29:17 +0800

    Refactoring.

--- AllInOne/src/ArrangingCoins/ArrangingCoins.java
+++ AllInOne/src/ArrangingCoins/ArrangingCoins.java
@@ -5,15 +5,19 @@ package ArrangingCoins;
  * 441. Arranging Coins
  * 
  * round 1: half-solved
- * The difference between : 0.5 * mid * mid + 0.5 * mid and mid * mid / 2 + mid / 2
+ * The difference between : 0.5 * mid * mid + 0.5 * mid, mid * mid / 2 + mid / 2, mid * mid * 0.5 + mid * 0.5
+ * 1) 0.5 * mid * mid: = (0.5*mid)*mid put 0.5 in front, the result will be converted to double
+ * 2) mid * mid * 0.5: = (mid*mid)*0.5, coz mid is int, so mid*mid is int if mid*mid may exceed
+ * the max range of int.
  * 
  * @author jingjiejiang
  * @history
  * 1. Jan 7, 2018
+ * 2. Finished on Feb 27,2018 
  */
 public class ArrangingCoins {
 	
-	public int arrangeCoins(int n) {
+	public static int arrangeCoins(int n) {
         
         int left = 0;
         int right = n;
@@ -25,7 +29,7 @@ public class ArrangingCoins {
             
             // *** 0.5 * mid * mid + 0.5 * mid is diffrent from mid * mid / 2 + mid / 2
             // when mid = 3, former = 4.5 + 1.5 = 6, latter = 5.
-            if (mid * mid * 0.5 + mid * 0.5 > n) {
+            if (0.5 * mid * mid + mid * 0.5 > n) {
                 right = mid - 1;
             }
             else {
@@ -35,21 +39,12 @@ public class ArrangingCoins {
         
         return left - 1;
         
-        /*
-        int start = 0; 
-        int end = n; 
-        int mid = 0; 
-        while (start <= end){ 
-            mid = (start + end) / 2; 
-            if ((0.5 * mid * mid + 0.5 * mid ) <= n){ 
-                start = mid + 1; 
-            } else { 
-                end = mid - 1; 
-            } 
-        } 
-        
-        return start - 1; 
-        */
     }
+	
+	public static void main (String[] args) {
+		
+		int a = arrangeCoins (1804289383);
+		System.out.println(a);
+	}
 
 }
