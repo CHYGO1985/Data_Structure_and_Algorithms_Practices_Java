9db4514a8cc31ec250a8d001ec48927d87bdc03f
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-08-29 16:56:03 +0800

    initial solution

---
+++ AllInOne/src/PartitionLabels/PartitionLabels.java
@@ -0,0 +1,63 @@
+package PartitionLabels;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.omg.PortableInterceptor.IORInterceptor;
+
+// First thought:
+// 1) start from the first leter as 0, find its last pos as curEnd
+// 2) search from 0 to curEnd, see if any char appear further than curEnd, if
+// so curEnd = new char farthest pos
+// 3) repeat the process of step 2, start from step previous pos i to curEnd
+// ====> disadvantage: multi search through the string
+
+// Optimise:
+// use an array with length 25 to indicate the start of each letter and another
+// to record the end of each letter 
+// use one for loop to get the start and end of each letter
+// 1) from pos 0, when meet a new letter, record the pos as its start and end pos
+// 2) only update the end pos array
+public class PartitionLabels {
+	
+	public static List<Integer> partitionLabels(String S) {
+		List<Integer> res = new LinkedList<Integer>();
+		int[] start = new int[25];
+		Arrays.fill(start, -1);
+		int[] end = new int[25];
+		
+		for (int index = 0; index < S.length(); index ++) {
+			int curLetter = S.charAt(index) - 'a';
+			if (start[curLetter] == -1) {
+				start[curLetter] = index;
+			}
+			end[curLetter] = index;
+		}
+		
+		int curEnd = 0;
+        // here require some time of thinking
+		int preEnd = -1;
+		for (int index = 0; index < S.length(); index ++) {
+			int curLetter = S.charAt(index) - 'a';
+			if (end[curLetter] > curEnd) {
+				curEnd = end[curLetter];
+			};
+			if (index == curEnd) {
+				res.add(index - preEnd);
+				preEnd = index;
+			}
+		}
+		
+		return res;
+    }
+
+	public static void main(String[] args) {
+		String S = "ababcbacadefegdehijhklij";
+		List<Integer> list = partitionLabels(S);
+		for (int index = 0; index < list.size(); index ++) {
+			System.out.println(list.get(index));
+		}
+	}
+
+}
