2e206e236131dc49a6cb38a888da3f8f155294a0
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-09-04 21:38:27 +0800

    accepted solution

--- AllInOne/src/BestTimeBuyAndSellStock/BestTimeBuyAndSellStockIII.java
+++ AllInOne/src/BestTimeBuyAndSellStock/BestTimeBuyAndSellStockIII.java
@@ -1,19 +1,26 @@
 package BestTimeBuyAndSellStock;
 
+/**
+ * 
+ * @author jingjiejiang created on Sep 4, 2018
+ *
+ */
 public class BestTimeBuyAndSellStockIII {
 
 	public static int maxProfit(int[] prices) {
 		int[][][] profit = new int[2][3][2];
 		profit[0][0][0] = 0;
 		profit[0][0][1] = Integer.MIN_VALUE;
+		profit[0][1][1] = Integer.MIN_VALUE;
+		profit[0][2][1] = Integer.MIN_VALUE;
 		
 		for (int i = 0, k = 0; i < prices.length; i ++) {
-			profit[(i + 1) % 2][k][0] = Math.max(profit[i % 2][k][0], profit[i % 2][k][1] + prices[i]);
-			if (k <= 1) {
-				profit[(i + 1) % 2][k + 1][1] = Math.max(profit[i % 2][k][1], profit[i % 2][k][0] - prices[i]);
-			}
+			profit[(i + 1) % 2][1][0] = Math.max(profit[i % 2][1][0], profit[i % 2][1][1] + prices[i]);
+			profit[(i + 1) % 2][1][1] = Math.max(profit[i % 2][1][1], profit[i % 2][0][0] - prices[i]);
+			profit[(i + 1) % 2][2][0] = Math.max(profit[i % 2][2][0], profit[i % 2][2][1] + prices[i]);
+			profit[(i + 1) % 2][2][1] = Math.max(profit[i % 2][2][1], profit[i % 2][1][0] - prices[i]);
 		}
-		return Math.max(profit[prices.length % 2][2][0], profit[prices.length % 2][2][1]);
+		return Math.max(0, Math.max(profit[prices.length % 2][2][0], profit[prices.length % 2][1][0]));
     }
 
 	public static void main(String[] args) {
