bdc9d31590dcdbc56eaae397ae03c49196dc5833
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-09-09 14:29:37 +0800

    acc solution, beat 100%

--- AllInOne/src/Greedy/ReorganizeString.java
+++ AllInOne/src/Greedy/ReorganizeString.java
@@ -35,18 +35,39 @@ public class ReorganizeString {
 		// vvvlo: vlvov
 		char[] newStr = new char[ori.length];
 		int pos = 0;
-		for (int i = maxPos; charNums > 0; i = (++ i) % count.length) {
-			if (count[i] == 0) continue;
-			newStr[pos ++] = (char)('a' + i);
-			count[i] --;
-			if (count[i] == 0) charNums --;
+		int i = maxPos;
+		
+		// odd position
+		for (; charNums > 0 && pos < newStr.length; i = (i + 1) % count.length) {
+			while (count[i] != 0 && pos < newStr.length) {
+				newStr[pos] = (char)('a' + i);
+				count[i] --;
+				if (count[i] == 0) charNums --;
+				pos += 2;
+			}
+			// in case, not finish the current pos, then i + 1, so for even pos, start with
+			// different pos
+			if (pos >= newStr.length) break;
+		}
+		
+		// even pos
+		pos = 1;
+		for (; charNums > 0 && pos < newStr.length; i = (i + 1) % count.length) {
+			while (count[i] != 0 && pos < newStr.length) {
+				newStr[pos] = (char)('a' + i);
+				count[i] --;
+				if (count[i] == 0) charNums --;
+				pos += 2;
+			}
 		}
 		
 		return String.valueOf(newStr);
     }
 
 	public static void main(String[] args) {
-		String S = "aaabbbc";
+//		String S = "aaabbbc";
+//		String S = "aaalovxy";
+		String S = "vvvlo";
 		System.out.println(reorganizeString(S));
 	}
 }
