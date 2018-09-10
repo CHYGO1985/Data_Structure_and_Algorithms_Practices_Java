82bbda846a7fb1359601537cd008ca10df55ef4b
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-09-10 19:56:29 +0800

    refactoring

--- AllInOne/src/Greedy/ReorganizeString.java
+++ AllInOne/src/Greedy/ReorganizeString.java
@@ -1,7 +1,5 @@
 package Greedy;
 
-import java.util.Arrays;
-
 /**
  * 
  * @author jingjiejiang created on Sep 9, 2018
@@ -27,47 +25,41 @@ public class ReorganizeString {
 				maxPos = charVal;
 			}
 		}
-		// 7: 4/3 6: 4/2
+		
 		if (max - (ori.length - max) > 1) {
 			return "";
 		}
 		
-		// vvvlo: vlvov
 		char[] newStr = new char[ori.length];
-		int pos = 0;
-		int i = maxPos;
-		
+				
 		// odd position
-		for (; charNums > 0 && pos < newStr.length; i = (i + 1) % count.length) {
-			while (count[i] != 0 && pos < newStr.length) {
-				newStr[pos] = (char)('a' + i);
-				count[i] --;
-				if (count[i] == 0) charNums --;
-				pos += 2;
-			}
-			// in case, not finish the current pos, then i + 1, so for even pos, start with
-			// different pos
-			if (pos >= newStr.length) break;
-		}
+		stringMaker(newStr, count, charNums, maxPos, 0);
 		
 		// even pos
-		pos = 1;
-		for (; charNums > 0 && pos < newStr.length; i = (i + 1) % count.length) {
-			while (count[i] != 0 && pos < newStr.length) {
-				newStr[pos] = (char)('a' + i);
-				count[i] --;
-				if (count[i] == 0) charNums --;
-				pos += 2;
-			}
-		}
+		stringMaker(newStr, count, charNums, maxPos, 1);
 		
 		return String.valueOf(newStr);
     }
+	
+	public static void stringMaker(char[] newStr, int[] count, int charNums, int maxPos, int startPos) {
+		int pos = startPos;
+		int i = maxPos;
+		
+		for (; charNums > 0 && pos < newStr.length; i = (i + 1) % count.length) {
+			while (count[i] != 0 && pos < newStr.length) {
+				newStr[pos] = (char)('a' + i);
+				count[i] --;
+				if (count[i] == 0) charNums --;
+				pos += 2;
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 //		String S = "aaabbbc";
 //		String S = "aaalovxy";
-		String S = "vvvlo";
+//		String S = "vvvlo";
+		String S = "rvhrlpiesrrryrbrrrrrxrkirranrrrrbdrrzgasoxrrr";
 		System.out.println(reorganizeString(S));
 	}
 }
