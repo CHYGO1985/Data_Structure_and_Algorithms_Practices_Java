328826b033b3faa6ccefcda720f6981ecdbaed39
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-09-14 21:53:29 +0800

    316. Remove Duplicate Letters add another implementation

--- AllInOne/src/Greedy/RemoveDuplicateLetters.java
+++ AllInOne/src/Greedy/RemoveDuplicateLetters.java
@@ -1,5 +1,7 @@
 package Greedy;
 
+import java.nio.charset.spi.CharsetProvider;
+
 /**
  *
  * same idea, but did not know how to write code quickly
@@ -9,7 +11,7 @@ package Greedy;
  *
  */
 public class RemoveDuplicateLetters {
-	
+	/*
 	public static String removeDuplicateLetters(String s) {
 		
 		int[] cnt = new int[26];
@@ -22,8 +24,30 @@ public class RemoveDuplicateLetters {
         }
         //why "" + s.charAt(pos): convert to string, get rid of the smallest
         return s.length() == 0 ? "" : s.charAt(pos) + removeDuplicateLetters(s.substring(pos + 1).replaceAll("" + s.charAt(pos), ""));
-            
     }
+	*/
+	
+	// my implementation
+	public static String removeDuplicateLetters(String s) {
+		if (s == null || s.length() == 0) return "";
+		int[] count = new int[26];
+		char[] sArray = s.toCharArray();
+		int minPos = 0;
+		StringBuilder builder = new StringBuilder();
+		
+		for (int i = 0; i < sArray.length; i ++) {
+			count[sArray[i] - 'a'] += 1;
+			if (sArray[i] - 'a' < sArray[minPos] - 'a') minPos = i;
+		}
+		
+		for (int i = 0; i < minPos; i ++) {
+			// get singles until minPos
+			if (count[sArray[i] - 'a'] == 1) builder.append(sArray[i]);
+		}
+		builder.append(sArray[minPos]);
+		// replace alll min letter after minPos is important
+		return builder.toString() + removeDuplicateLetters(s.substring(minPos + 1).replaceAll("" + sArray[minPos], ""));
+	}
 
 	public static void main(String[] args) {
 		String string = "xcbacdcbc";
