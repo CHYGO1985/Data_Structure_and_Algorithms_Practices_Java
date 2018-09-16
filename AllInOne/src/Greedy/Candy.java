6dd5e6203b4976ea8140d24160ae0ae8f609bd5b
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-09-16 23:54:33 +0800

    135. Candy

--- AllInOne/src/Greedy/Candy.java
+++ AllInOne/src/Greedy/Candy.java
@@ -1,24 +1,39 @@
 package Greedy;
 
+import java.util.function.IntPredicate;
+
+import javax.swing.InputMap;
+
 public class Candy {
 	
 	// [1,3,2,2,1] if start from 1, did not count i > i -1
 	public static int candy(int[] ratings) {
 		
 		if (ratings == null || ratings.length == 0) return 0;
-        int sum = 1;
-        if (ratings.length == 1) return sum; 
-        if (ratings[0] > ratings[1]) sum += 1;
+		int[] carries = new int[ratings.length];
+		int[] candies = new int[ratings.length];
+        int sum = 0;
+        int index = 0;
         
-        for (int index = 0; index < ratings.length; index ++) {
-        	sum += (ratings[index] > ratings[index + 1]) ? 2 : 1;
+        for (; index < ratings.length - 1; index ++) {
+        	if (ratings[index] > ratings[index + 1] && carries[index] < 1) {
+        		carries[index] = -1;
+        		candies[index] = 1 + carries[index];
+        	}
+        	else if (ratings[index] < ratings[index + 1]) {
+        		carries[index + 1] = carries[index] + 1;
+        	}
+        	
         }
         
+        sum += 1 + carries[index];
         return sum;
     }
 
 	public static void main(String[] args) {
-		int[] ratings = new int[]{1,1,1};
+//		int[] ratings = new int[]{1,1,1};
+//		int[] ratings = new int[]{1,3,2,2,1};
+		int[] ratings = new int[]{1,2,3,4};
 		System.out.println(candy(ratings));
 	}
 }
