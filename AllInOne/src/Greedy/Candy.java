a17874d79dca809c2bf16a2a7d64edd593ca8e13
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-09-19 22:40:52 +0800

    135. Candy

--- AllInOne/src/Greedy/Candy.java
+++ AllInOne/src/Greedy/Candy.java
@@ -1,32 +1,44 @@
 package Greedy;
 
-import java.util.function.IntPredicate;
+import java.util.Arrays;
 
-import javax.swing.InputMap;
-
+/**
+ * 
+ * solved
+ * @author jingjiejiang created on Sep 19, 2018
+ *
+ */
 public class Candy {
 	
 	// [1,3,2,2,1] if start from 1, did not count i > i -1
+	// 1234, 4321 
 	public static int candy(int[] ratings) {
 		
 		if (ratings == null || ratings.length == 0) return 0;
 		int[] carries = new int[ratings.length];
-		int[] candies = new int[ratings.length];
+		Arrays.fill(carries, 1);
         int sum = 0;
         int index = 0;
         
         for (; index < ratings.length - 1; index ++) {
-        	if (ratings[index] > ratings[index + 1] && carries[index] < 1) {
-        		carries[index] = -1;
-        		candies[index] = 1 + carries[index];
-        	}
-        	else if (ratings[index] < ratings[index + 1]) {
+        	if (ratings[index] < ratings[index + 1]) {
         		carries[index + 1] = carries[index] + 1;
         	}
-        	
+        	else if (ratings[index] > ratings[index + 1]) {
+        		int start = index;
+        		do {
+        			index ++;
+        		}
+        		while (index < ratings.length - 1 && ratings[index] > ratings[index + 1]);
+        		for (int end = index - 1; end >= start; end --) {
+        			if (carries[end] <= carries[end + 1]) carries[end] = carries[end + 1] + 1;
+        		}
+        		index --;
+        	}
         }
         
-        sum += 1 + carries[index];
+        for (int carry: carries) sum += carry;
+        
         return sum;
     }
 
@@ -34,6 +46,9 @@ public class Candy {
 //		int[] ratings = new int[]{1,1,1};
 //		int[] ratings = new int[]{1,3,2,2,1};
 		int[] ratings = new int[]{1,2,3,4};
+//		int[] ratings = new int[]{4,3,2,1}; 
+//		int[] ratings = new int[]{1,0,2};
+//		int[] ratings = new int[]{1,3,4,5,2};
 		System.out.println(candy(ratings));
 	}
 }
