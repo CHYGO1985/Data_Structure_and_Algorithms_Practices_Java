6c6cc9daad50d13aa83bd8a4986cd33b663e16f2
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-09-24 16:44:56 +0800

    330. Patching Array init

---
+++ AllInOne/src/PatchingArray.java
@@ -0,0 +1,18 @@
+
+/**
+ * 
+ * @author jingjiejiang created on Sep 24, 20170
+ *
+ */
+public class PatchingArray {
+	
+	public static int minPatches(int[] nums, int n) {
+        return 0;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
