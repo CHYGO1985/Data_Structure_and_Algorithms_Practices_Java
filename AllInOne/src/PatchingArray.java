9c179167d832b34f40dbb946c21f70803d5fd0af
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-09-24 17:25:17 +0800

    330. Patching Array acc solution

--- AllInOne/src/PatchingArray.java
+++ AllInOne/src/PatchingArray.java
@@ -1,18 +1,33 @@
 
 /**
  * 
+ * https://leetcode.com/problems/patching-array/discuss/78490/Share-my-greedy-solution-by-Java-with-simple-explanation-(time:-1-ms)
+ * 
  * @author jingjiejiang created on Sep 24, 20170
  *
  */
 public class PatchingArray {
 	
 	public static int minPatches(int[] nums, int n) {
-        return 0;
+		long max = 0;
+		int cnt = 0;
+		for (int i = 0; max < n;) {
+			if (i >= nums.length || max < nums[i] - 1) {
+				// here is the greedy part, use max to cover the range (max + 1 will be the missing value)
+				// e.g. 1 n = 3, then missing is 2, max = 1 + 2(1+ 1)
+				max += max + 1;
+				cnt++;
+			} else {
+				max += nums[i];
+				i++;
+			}
+		}
+		return cnt;
     }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] nums = new int[]{1};
+		System.out.println(minPatches(nums, 3));
 	}
 
 }
