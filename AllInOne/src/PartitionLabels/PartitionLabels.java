dd3bc10ba4ab8397a2f10f2bfe1a7f25dc4ef1fd
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-09-24 16:42:52 +0800

    refactoring

--- AllInOne/src/PartitionLabels/PartitionLabels.java
+++ AllInOne/src/PartitionLabels/PartitionLabels.java
@@ -1,10 +1,7 @@
 package PartitionLabels;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
-
-import org.omg.PortableInterceptor.IORInterceptor;
 
 // First thought:
 // 1) start from the first leter as 0, find its last pos as curEnd
