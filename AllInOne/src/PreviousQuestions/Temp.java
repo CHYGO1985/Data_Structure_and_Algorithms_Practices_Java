62e06afb39d2bb7b3dfb9acaa8eae886731b36ef
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-10-02 14:06:28 +0800

    435. Non-overlapping Intervals

--- AllInOne/src/PreviousQuestions/Temp.java
+++ AllInOne/src/PreviousQuestions/Temp.java
@@ -1,47 +1,60 @@
 package PreviousQuestions;
 
+import java.awt.event.MouseWheelEvent;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+import Greedy.NonOverlappingIntervals.Interval;
+
 /**
  * 
- * @author jingjiejiang Oct 1st, 2018
+ * @author jingjiejiang Oct 2nd, 2018
  *
  */
 public class Temp {
-
-	public static int leastInterval(char[] tasks, int n) {
-		// get the frequecies of each char in the array and the most appearance char X
-		// from the most appearance, construct the res array (do not need to)
-		// if back to X and inerval < n, add length till gap = n
+	
+	// 1. sort
+	// 2. iterate through to get the most overlapping one
+	// 3. only compare with the current range, if next.start >= cur.end, update the range
+	// 4. if next.end <= cur.end. update cur.end as next.end (get rid of the bigger one)
+	public class Interval {
+		int start;
+		int end;
+		Interval() { start = 0; end = 0; }
+		Interval(int s, int e) { start = s; end = e; }
+	}
+	
+	public int eraseOverlapIntervals(Interval[] intervals) {
 		
-		if (null == tasks || 0 == tasks.length) return 0;
-        
-        int[] chars = new int[26];
-        int maxCount = 0;
-        int sum = tasks.length;
-        
-        for (char temp : tasks) {
-            chars[temp - 'A'] += 1;
-            maxCount = Math.max(maxCount, chars[temp - 'A']);
-        }
-        
-        // not repeat
-        if (maxCount == 1) return sum;
-        int rest = 0;
-        // check whether there is num of other > (maxCount - 1) e.g. AAABBB,
-        for (int i : chars) {
-            if (i - (maxCount - 1) > 0)
-                rest += i - (maxCount - 1);
-        }
-        
-        int fixSum = n * (maxCount - 1) + maxCount;
-        //rest - 1: coz the for loop will count maxCount char, so -1 of the 1 got from max count char
-        fixSum += (sum - maxCount > n * (maxCount - 1)) ? 
-        		sum - maxCount - n * (maxCount - 1) : rest - 1;
-        		
-        
-        return fixSum;
+if (intervals == null || intervals.length == 0) return 0;
+		
+		int cnt = 0, curEnd = Integer.MIN_VALUE;
+		
+		Arrays.sort(intervals, new Comparator<Interval>() {
+			@Override
+			public int compare(Interval a, Interval b) {
+				return a.start == b.start ? a.start - b.start : a.end - b.end; 
+			}
+		});
+		
+		for (Interval interval : intervals) {
+			if (interval.start >= curEnd) {
+				curEnd = interval.end;
+			} else { // interval.start < curEnd
+				if (interval.end <= curEnd) {
+					curEnd = interval.end;
+				}
+				cnt ++;
+			}
+		}
+		
+		return cnt;
     }
 	
 	public static void main(String[] args) {
+		
 	}
-
 }
