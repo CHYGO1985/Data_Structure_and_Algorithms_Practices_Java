9c10ce1e28f651755667e9e5180cacb5c61b3155
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-10-06 23:39:19 +0800

    402. Remove K Digits

--- AllInOne/src/PreviousQuestions/Temp.java
+++ AllInOne/src/PreviousQuestions/Temp.java
@@ -2,28 +2,12 @@ package PreviousQuestions;
 
 /**
  * 
- * @author jingjiejiang Oct 5, 2018
+ * @author jingjiejiang Oct 6, 2018
  *
  */
 public class Temp {
 	
-	// iterate through, for each step nextRange = max(i + nums[i], curRange);
-	// at pos = 0, 
-	// if (i == curRange), then curRange = nextRange, cnt ++;
-	// until i <= nums.length - 1
-	public int jump(int[] nums) {
-		
-		if (nums == null) return 0;
-		
-		int curRange = 0, nextRange = 0, count = 0;
-		for (int idx = 0; idx < nums.length - 1; idx ++) {
-			nextRange = Math.max(nextRange, idx + nums[idx]);
-			if (idx == curRange) {
-				count ++;
-				curRange = nextRange;
-			}
-		}
-		
-        return count;
+	public static String removeKdigits(String num, int k) {
+	           
     }
 }
