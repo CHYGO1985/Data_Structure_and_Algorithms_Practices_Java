e782075504ee49a7a736b7ef3670b020a0dc0082
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-10-07 23:22:41 +0800

    330. Patching Array

--- AllInOne/src/Greedy/PatchingArray.java
+++ AllInOne/src/Greedy/PatchingArray.java
@@ -1,3 +1,4 @@
+package Greedy;
 
 /**
  * 
@@ -9,25 +10,30 @@
 public class PatchingArray {
 	
 	public static int minPatches(int[] nums, int n) {
+		
 		long max = 0;
-		int cnt = 0;
-		for (int i = 0; max < n;) {
-			if (i >= nums.length || max < nums[i] - 1) {
+		int cnt = 0, idx = 0;
+		while (max < n) {
+			if (idx >= nums.length || max < nums[idx] - 1) {
 				// here is the greedy part, use max to cover the range (max + 1 will be the missing value)
 				// e.g. 1 n = 3, then missing is 2, max = 1 + 2(1+ 1)
-				max += max + 1;
-				cnt++;
-			} else {
-				max += nums[i]; // add the value that the array has to the max
-				i++;
+				
+				// if the gap is large, num[] = 1, 10, n 20, then it will iterate multi times to get 10
+				// 1 --> 3 (1 + (1 + 1)) --> 7 (3 + (3 + 1)) --> 4
+				max += max + 1; // patch for next num of the max
+				cnt ++;
+			}
+			else {
+				max += nums[idx ++];
 			}
 		}
+		
 		return cnt;
     }
 
 	public static void main(String[] args) {
-		int[] nums = new int[]{1};
-		System.out.println(minPatches(nums, 3));
+		int[] nums = new int[]{1, 10};
+		System.out.println(minPatches(nums, 20));
 	}
 
 }
