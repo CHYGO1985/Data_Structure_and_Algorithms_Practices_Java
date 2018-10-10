08430743f3b915e0446c5fedfe3518445aefa78e
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-10-10 23:53:48 +0800

    init impl

--- AllInOne/src/PreviousQuestions/Temp.java
+++ AllInOne/src/PreviousQuestions/Temp.java
@@ -1,45 +1,45 @@
 package PreviousQuestions;
 
-import java.util.Arrays;
-
 /**
  * 
- * @author jingjiejiang Oct 9, 2018
+ * @author jingjiejiang Oct 10, 2018
  *
  */
 public class Temp {
 	
 	// find the min
 	// from min to left, and from min right, get the the final array
-	public int candy(int[] ratings) {
-	   
-		if (ratings == null || ratings.length == 0) return 0;
-		int[] carries = new int[ratings.length];
-		Arrays.fill(carries, 1);
-        int sum = 0;
-        int index = 0;
-        
-        for (; index < ratings.length - 1; index ++) {
-        	if (ratings[index] < ratings[index + 1]) {
-        		carries[index + 1] = carries[index] + 1;
-        	}
-        	else if (ratings[index] > ratings[index + 1]) {
-        		int start = index;
-        		do {
-        			index ++;
-        		}
-        		while (index < ratings.length - 1 && ratings[index] > ratings[index + 1]);
-        		for (int end = index - 1; end >= start; end --) {
-        			if (carries[end] <= carries[end + 1]) carries[end] = carries[end + 1] + 1;
-        		}
-        		index --;
-        	}
-        }
-        
-        for (int carry: carries) sum += carry;
-        
-        return sum;
+	public static int[] maxNumber(int[] nums1, int[] nums2, int k) {
+		// for two array, get i max num from nums1 and k - i max num from nums2
+		// merge them
+		// compare the res with previous, get the max at the end
+		int[] max = new int[k];
+		int limit = Math.min(nums1.length, k);
+		for (int i = 0; i <= limit; i ++) {
+			max = getMax(max, merge(maxNum(nums1, i), maxNum(nums2, k- i)));
+		}
+		
+		return max;
     }
+	
+	public int[] maxNum(int[] nums, int digits) {
+		
+//		if (digits == 0) return 
+		int[] res = new int[nums.length];
+		int top = 0;
+		res[top] = Integer.MIN_VALUE;
+		
+		for (int idx = 0; idx < nums.length; idx ++) {
+			int curCnt = digits;
+			while (nums[idx] < res[top] && curCnt > 0) {
+				top --;
+				curCnt --;
+			}
+			res[top ++] = nums[idx]; 
+		}
+		
+		return res;
+	}
 	
 	public static void main(String[] args) {
 		
