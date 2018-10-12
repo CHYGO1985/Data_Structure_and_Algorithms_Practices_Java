355bf198374b3f33eb99d25d2aee92a5208c8be8
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-10-13 00:49:59 +0800

    651. 4 Keys Keyboard

--- AllInOne/src/PreviousQuestions/Temp.java
+++ AllInOne/src/PreviousQuestions/Temp.java
@@ -7,8 +7,21 @@ package PreviousQuestions;
  */
 public class Temp {
 	
+	// use an array to record previous max for each num
+	// iterate through, for each num of N, check 3 to num, get the max max = max(max. dp[num])
 	public int maxA(int N) {
+		
+		int[] dp = new int[N + 1];
+		dp[0] = 0;
+		
+		for (int idx = 0; idx < dp.length; idx ++) {
+			dp[idx] = idx;
+			for (int checkPoint = 3; checkPoint < idx; checkPoint ++) {
+				dp[idx] = Math.max(dp[idx], dp[idx - checkPoint] * (checkPoint - 1));
+			}
+		}
         
+		return dp[N];
     }
 	
 	public static void main(String[] args) {
