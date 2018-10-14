2803ecefbe30253a69b1dd1c79e8a8b03187f643
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-10-14 21:54:00 +0800

    330. Patching Array

--- AllInOne/src/PreviousQuestions/Temp.java
+++ AllInOne/src/PreviousQuestions/Temp.java
@@ -1,37 +1,26 @@
 package PreviousQuestions;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
-
 /**
  * 
- * @author jingjiejiang Oct 13, 2018
+ * @author jingjiejiang Oct 14, 2018
  *
  */
 public class Temp {
 	
-	public int[][] reconstructQueue(int[][] people) {
-        // sort the people as h in descend order and k in ascend order
-		// insert the person to a list according to k
-		// convert list to array
-		if (people == null || people.length == 0) return new int[0][0];
-		
-		List<int[]> list = new LinkedList<>();
-		
-		Arrays.sort(people, new Comparator<int[]>() {
-			@Override
-			public int compare(int[] a, int[] b) {
-				return a[0] != b[0] ? b[0] - a[0] : a[1] - b[1]; 
+	public int minPatches(int[] nums, int n) {
+		long max = 0;
+		int cnt = 0, idx = 0;
+		while (max < n) {
+			if (idx >= nums.length || max < nums[idx] - 1) {
+				max += max + 1;
+				cnt ++;
 			}
-		});
-		
-		for (int[] person : people) {
-			list.add(person[1], person);
+			else {
+				max += nums[idx ++];
+			}
 		}
 		
-		return list.toArray(new int[people.length][people[0].length]);
+		return cnt;
     }
 	
 	public static void main(String[] args) {
