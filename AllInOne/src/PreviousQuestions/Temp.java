ec1b92af33625b03f0e06e913cc9f9b7851d8609
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-10-21 23:37:50 +0800

    402. Remove K Digits

--- AllInOne/src/PreviousQuestions/Temp.java
+++ AllInOne/src/PreviousQuestions/Temp.java
@@ -2,7 +2,7 @@ package PreviousQuestions;
 
 /**
  * 
- * @author jingjiejiang Oct 20, 2018
+ * @author jingjiejiang Oct 21, 2018
  *
  */
 public class Temp {
@@ -14,38 +14,8 @@ public class Temp {
 		Interval(int s, int e) { start = s; end = e; }
 	}
 	
-	public int leastInterval(char[] tasks, int n) {
-		if (null == tasks || 0 == tasks.length) return 0;
+	public static String removeKdigits(String num, int k) {
         
-        int[] chars = new int[26];
-        int maxCount = 0;
-        int sum = tasks.length;
-        
-        for (char temp : tasks) {
-            chars[temp - 'A'] += 1;
-            maxCount = Math.max(maxCount, chars[temp - 'A']);
-        }
-        
-        // not repeat
-        if (maxCount == 1) return sum;
-        
-        // get basic interval count of the frame
-        int fixSum = n * (maxCount - 1) + maxCount;
-        int surpass = 0;
-        
-        // if cur count > (maxCnt - 1), then frame += cur count - (maxCnt - 1)
-        for (int cntOfChar : chars) {
-        	int curSurPass = cntOfChar - (maxCount - 1);
-        	surpass += curSurPass > 0 ? curSurPass : 0; 
-        }
-        
-        // check num of chars - n * (maxCount - 1) > 0
-        // if > 0, then return task.length
-        // esle, return fixSum + surpass - 1
-        fixSum += (sum - maxCount > n * (maxCount - 1)) ? 
-        		sum - maxCount - n * (maxCount - 1) : surpass - 1;
-        	
-        return fixSum;
     }
 	
 	public static void main(String[] args) {
