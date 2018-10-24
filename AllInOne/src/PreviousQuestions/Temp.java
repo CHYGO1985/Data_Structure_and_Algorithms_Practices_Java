0dcba983e61ec3ab5503bbb752bd623b69ecb516
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-10-24 23:44:51 +0800

    135. Candy

--- AllInOne/src/PreviousQuestions/Temp.java
+++ AllInOne/src/PreviousQuestions/Temp.java
@@ -1,5 +1,7 @@
 package PreviousQuestions;
 
+import java.util.Arrays;
+
 /**
  * 
  * @author jingjiejiang Oct 24, 2018
@@ -14,19 +16,48 @@ public class Temp {
 		Interval(int s, int e) { start = s; end = e; }
 	}
 	
-	public String removeDuplicateLetters(String s) {
-		int[] cnt = new int[26];
-        int pos = 0; // the position for the smallest s[i]
-        for (int i = 0; i < s.length(); i++) cnt[s.charAt(i) - 'a']++;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) < s.charAt(pos)) pos = i;
-            // if there is single one in the string, add it to final result first
-            if (--cnt[s.charAt(i) - 'a'] == 0) break;
+	public static int candy(int[] ratings) {
+        // init res array
+		// int sum = ratings.len
+		// for (1 to len) --> 0 to len - 1 is much better
+		// 1) if [i] > [i - 1]
+		// res[i] ++;
+		// sum + res[i]
+		// 2) else 
+		// find the acsend one or the end of ratings while ([i] <= [i - 1] && i < len) i ++
+		// from (new idx - 1) to cur i, i --
+		// if ([iidx] > [idx] - 1) res[idx] ++, sum + res[i]
+		// return sum;
+		if (ratings == null || ratings.length == 0) return 0;
+		int[] carries = new int[ratings.length];
+		Arrays.fill(carries, 1);
+        int sum = 0;
+        int index = 0;
+        
+        for (; index < ratings.length - 1; index ++) {
+        	if (ratings[index] < ratings[index + 1]) {
+        		carries[index + 1] = carries[index] + 1;
+        	}
+        	else if (ratings[index] > ratings[index + 1]) {
+        		int start = index;
+        		do {
+        			index ++;
+        		}
+        		while (index < ratings.length - 1 && ratings[index] > ratings[index + 1]);
+        		for (int end = index - 1; end >= start; end --) {
+        			if (carries[end] <= carries[end + 1]) carries[end] = carries[end + 1] + 1;
+        		}
+        		index --;
+        	}
         }
-        //why "" + s.charAt(pos): convert to string, get rid of the smallest
-        return s.length() == 0 ? "" : s.charAt(pos) + removeDuplicateLetters(s.substring(pos + 1).replaceAll("" + s.charAt(pos), "")); 
+        
+        for (int carry: carries) sum += carry;
+        
+        return sum;
     }
 	
 	public static void main(String[] args) {
+		int[] arr = new int[]{1,2,3,3};
+		candy(arr);
 	}
 }
