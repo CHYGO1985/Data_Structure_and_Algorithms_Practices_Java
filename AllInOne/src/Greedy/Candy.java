b0644945c047dcf31d09da520dc0f2999f88af08
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-10-25 00:58:36 +0800

    135. Candy more easily understand solution

--- AllInOne/src/Greedy/Candy.java
+++ AllInOne/src/Greedy/Candy.java
@@ -10,6 +10,35 @@ import java.util.Arrays;
  */
 public class Candy {
 	
+//	public int candy(int[] ratings) {
+//        if (ratings == null || ratings.length == 0) return 0;
+//		int[] carries = new int[ratings.length];
+//		Arrays.fill(carries, 1);
+//        int sum = 0;
+//        int index = 0;
+//        
+//        for (; index < ratings.length - 1; index ++) {
+//        	if (ratings[index] < ratings[index + 1]) {
+//        		carries[index + 1] = carries[index] + 1;
+//        	}
+//        	else if (ratings[index] > ratings[index + 1]) {
+//        		int start = index;
+//        		do {
+//        			index ++;
+//        		}
+//        		while (index < ratings.length - 1 && ratings[index] > ratings[index + 1]);
+//        		for (int end = index - 1; end >= start; end --) {
+//        			if (carries[end] <= carries[end + 1]) carries[end] = carries[end + 1] + 1;
+//        		}
+//        		index --;
+//        	}
+//        }
+//        
+//        for (int carry: carries) sum += carry;
+//        
+//        return sum;
+//    }
+	
 	// [1,3,2,2,1] if start from 1, did not count i > i -1
 	// 1234, 4321 
 	public static int candy(int[] ratings) {
@@ -21,17 +50,19 @@ public class Candy {
         int index = 0;
         
         for (; index < ratings.length - 1; index ++) {
+        	// ascend case
         	if (ratings[index] < ratings[index + 1]) {
         		carries[index + 1] = carries[index] + 1;
         	}
+        	// decend case: two things, find the ending : from end to start, build carries (consider the last bit that already has value)
         	else if (ratings[index] > ratings[index + 1]) {
         		int start = index;
         		do {
         			index ++;
         		}
-        		while (index < ratings.length - 1 && ratings[index] > ratings[index + 1]);
-        		for (int end = index - 1; end >= start; end --) {
-        			if (carries[end] <= carries[end + 1]) carries[end] = carries[end + 1] + 1;
+        		while (index < ratings.length - 1 && ratings[index] >= ratings[index + 1]);
+        		for (int end = index; end > start; end --) {
+        			if (ratings[end - 1] > ratings[end] && carries[end - 1] <= carries[end]) carries[end - 1] = carries[end] + 1;
         		}
         		index --;
         	}
@@ -43,9 +74,9 @@ public class Candy {
     }
 
 	public static void main(String[] args) {
-//		int[] ratings = new int[]{1,1,1};
+		int[] ratings = new int[]{1,3,5,2,3};
 //		int[] ratings = new int[]{1,3,2,2,1};
-		int[] ratings = new int[]{1,2,3,4};
+//		int[] ratings = new int[]{1,2,3,4};
 //		int[] ratings = new int[]{4,3,2,1}; 
 //		int[] ratings = new int[]{1,0,2};
 //		int[] ratings = new int[]{1,3,4,5,2};
