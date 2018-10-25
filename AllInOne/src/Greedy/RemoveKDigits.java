fa13e777d0643a9afe82125b7b41e759c0f175ed
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-10-25 23:28:40 +0800

    402. Remove K Digits

--- AllInOne/src/Greedy/RemoveKDigits.java
+++ AllInOne/src/Greedy/RemoveKDigits.java
@@ -64,35 +64,53 @@ public class RemoveKDigits {
 	*/
 
 	// ref: https://leetcode.com/problems/remove-k-digits/discuss/88660/A-greedy-method-using-stack-O(n)-time-and-O(n)-space
+//	public static String removeKdigits(String num, int k) {
+//        int digits = num.length() - k;
+//        char[] stk = new char[num.length()];
+//        int top = 0;
+//        // k keeps track of how many characters we can remove
+//        // if the previous character in stk is larger than the current one
+//        // then removing it will get a smaller number
+//        // but we can only do so when k is larger than 0
+//        for (int i = 0; i < num.length(); ++i) {
+//            char c = num.charAt(i);
+//            while (top > 0 && stk[top-1] > c && k > 0) {
+//                top -= 1;
+//                k -= 1;
+//            }
+//            stk[top++] = c;
+//        }
+//        // find the index of first non-zero digit
+//        int idx = 0;
+//        while (idx < digits && stk[idx] == '0') idx++;
+//        return idx == digits? "0": new String(stk, idx, digits - idx);
+//    }
+	
 	public static String removeKdigits(String num, int k) {
-        int digits = num.length() - k;
-        char[] stk = new char[num.length()];
-        int top = 0;
-        // k keeps track of how many characters we can remove
-        // if the previous character in stk is larger than the current one
-        // then removing it will get a smaller number
-        // but we can only do so when k is larger than 0
-        for (int i = 0; i < num.length(); ++i) {
-            char c = num.charAt(i);
-            while (top > 0 && stk[top-1] > c && k > 0) {
-                top -= 1;
-                k -= 1;
-            }
-            stk[top++] = c;
-        }
-        // find the index of first non-zero digit
-        int idx = 0;
-        while (idx < digits && stk[idx] == '0') idx++;
-        return idx == digits? "0": new String(stk, idx, digits - idx);
-    }
+	
+		char[] res = new char[num.length() - k];
+		int top = 0;
+		
+		for (int idx = 0; idx < num.length(); idx ++) {
+			char cur = num.charAt(idx);
+			while (top > 0 && num.length() - idx + top > res.length && cur < res[top - 1]) {
+				top --;
+			}
+			if (top < res.length) res[top ++] = cur;
+		}
+		
+		int idx = 0;
+		while (idx < res.length && res[idx] == '0') idx ++;
+		return idx == res.length ? "0" : new String(res, idx, res.length - idx);
+	}
 	
 	public static void main(String[] args) {
 //		String num = "100000";
-//		String num = "10200";
+		String num = "10200";
 //		String num = "456123";
-		String num = "1432219";
+//		String num = "1432219";
 //		String num = "1173";
-		System.out.println(removeKdigits(num, 2));
+		System.out.println(removeKdigits(num, 1));
 	}
 
 }
--- AllInOne/src/PreviousQuestions/Temp.java
+++ AllInOne/src/PreviousQuestions/Temp.java
@@ -16,48 +16,34 @@ public class Temp {
 		Interval(int s, int e) { start = s; end = e; }
 	}
 	
-	public static int candy(int[] ratings) {
+	public int[] maxNumber(int[] nums1, int[] nums2, int k) {
         // init res array
-		// int sum = ratings.len
-		// for (1 to len) --> 0 to len - 1 is much better
-		// 1) if [i] > [i - 1]
-		// res[i] ++;
-		// sum + res[i]
-		// 2) else 
-		// find the acsend one or the end of ratings while ([i] <= [i - 1] && i < len) i ++
-		// from (new idx - 1) to cur i, i --
-		// if ([iidx] > [idx] - 1) res[idx] ++, sum + res[i]
-		// return sum;
-		if (ratings == null || ratings.length == 0) return 0;
-		int[] carries = new int[ratings.length];
-		Arrays.fill(carries, 1);
-        int sum = 0;
-        int index = 0;
-        
-        for (; index < ratings.length - 1; index ++) {
-        	if (ratings[index] < ratings[index + 1]) {
-        		carries[index + 1] = carries[index] + 1;
-        	}
-        	else if (ratings[index] > ratings[index + 1]) {
-        		int start = index;
-        		do {
-        			index ++;
-        		}
-        		while (index < ratings.length - 1 && ratings[index] > ratings[index + 1]);
-        		for (int end = index - 1; end >= start; end --) {
-        			if (carries[end] <= carries[end + 1]) carries[end] = carries[end + 1] + 1;
-        		}
-        		index --;
-        	}
-        }
-        
-        for (int carry: carries) sum += carry;
-        
-        return sum;
+		return null;
+		
     }
+	
+ 	public int[] findMax(int[] nums, int digits) {
+		
+		// init res[digit], pointer of stack: top
+		// for (0 --> num.len)
+		// while ( top > 0 && num.len - idx + top < digits && num[idx] > res[top - 1])
+		// {top --}
+		// res[top ++] = nums[idx]
+		
+		int[] res = new int[digits];
+		int top = 0;
+		
+		for (int idx = 0; idx < nums.length; idx ++) {
+			while (top > 0 && nums.length - idx + top < digits && nums[idx] > res[top - 1]) {
+				top --;
+			}
+			res[top ++] = nums[idx];
+		}
+		
+		return res;
+	}
 	
 	public static void main(String[] args) {
 		int[] arr = new int[]{1,2,3,3};
-		candy(arr);
 	}
 }
