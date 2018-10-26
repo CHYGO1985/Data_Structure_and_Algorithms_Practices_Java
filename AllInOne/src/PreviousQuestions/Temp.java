745c8e2c4a970f89fa44101a5fac7e6c72132ab9
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-10-26 23:25:29 +0800

    330. Patching Array

--- AllInOne/src/PreviousQuestions/Temp.java
+++ AllInOne/src/PreviousQuestions/Temp.java
@@ -1,7 +1,5 @@
 package PreviousQuestions;
 
-import java.util.Arrays;
-
 /**
  * 
  * @author jingjiejiang Oct 24, 2018
@@ -16,63 +14,28 @@ public class Temp {
 		Interval(int s, int e) { start = s; end = e; }
 	}
 	
-	public int[] maxNumber(int[] nums1, int[] nums2, int k) {
-        // init res array
-		int len1 = nums1.length;
-		int len2 = nums2.length;
-		int[] res = new int[k];
-		for (int i = Math.max(0, k - len2); i <= k && i <= len1; i ++) {
-			int[] curNum = merge(findMax(nums1, i), findMax(nums2, k - i), k);
-			if (compare(res, curNum, 0, 0) == false) {
-				res = curNum;
+	public int minPatches(int[] nums, int n) {
+        // sum = 0, cnt = 0;
+		// for (i --> len && sum < n)
+		// int limit = idx < len ? min([i], n) : n;
+		// while (sum < limit)
+		// { sum += sum + 1; cnt ++}
+		// sum += [i]
+		if (nums == null) return 0;
+		
+		long sum = 0;
+        int cnt = 0;
+		for (int idx = 0; idx < nums.length || sum < n; idx ++) {
+			long limit = (idx < nums.length) ? Math.min(nums[idx] - 1, n) : n;
+			while (sum < limit) {
+				sum += sum + 1;
+				cnt ++;
 			}
+			sum += nums[idx];
 		}
-		
-		return res;
-		
+        
+		return cnt;
     }
-	
-	public int[] merge (int[] num1, int[] num2, int targetLen) {
-		
-		int[] res = new int[targetLen];
-		int idx1 = 0, idx2 = 0, resIdx = 0;
-		while (resIdx < targetLen) {
-			res[resIdx ++] = compare(num1, num2, idx1, idx2) == true ?
-					num1[idx1 ++] : num2[idx2 ++]; 
-		}
-		
-		return res;
-	}
-	
-	public boolean compare(int[] nums1, int[] nums2, int idx1, int idx2) {
-		while (idx1 < nums1.length && idx2 < nums2.length && nums1[idx1] == nums2[idx2]) {
-			idx1 ++;
-			idx2 ++;
-		}
-		
-		return (idx2 == nums2.length) || (idx1 < nums1.length && nums1[idx1] > nums2[idx2]);
-	}
-	
- 	public int[] findMax(int[] nums, int digits) {
-		
-		// init res[digit], pointer of stack: top
-		// for (0 --> num.len)
-		// while ( top > 0 && num.len - idx + top < digits && num[idx] > res[top - 1])
-		// {top --}
-		// res[top ++] = nums[idx]
-		
-		int[] res = new int[digits];
-		int top = 0;
-		
-		for (int idx = 0; idx < nums.length; idx ++) {
-			while (top > 0 && nums.length - idx + top < digits && nums[idx] > res[top - 1]) {
-				top --;
-			}
-			res[top ++] = nums[idx];
-		}
-		
-		return res;
-	}
 	
 	public static void main(String[] args) {
 		int[] arr = new int[]{1,2,3,3};
