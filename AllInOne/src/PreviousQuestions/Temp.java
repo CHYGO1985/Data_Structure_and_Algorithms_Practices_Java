65c333135ecb850e06d0ca9b50e86dc3cd5bd613
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-10-26 21:05:25 +0800

    321. Create Maximum Number

--- AllInOne/src/PreviousQuestions/Temp.java
+++ AllInOne/src/PreviousQuestions/Temp.java
@@ -22,6 +22,27 @@ public class Temp {
 		
     }
 	
+	public int[] merge (int[] num1, int[] num2, int targetLen) {
+		
+		int[] res = new int[targetLen];
+		int idx1 = 0, idx2 = 0, resIdx = 0;
+		while (resIdx < targetLen) {
+			res[resIdx ++] = compare(num1, num2, idx1, idx2) == true ?
+					num1[idx1 ++] : num2[idx2 ++]; 
+		}
+		
+		return res;
+	}
+	
+	public boolean compare(int[] nums1, int[] nums2, int idx1, int idx2) {
+		while (idx1 < nums1.length && idx2 < nums2.length && nums1[idx1] == nums2[idx2]) {
+			idx1 ++;
+			idx2 ++;
+		}
+		
+		return (idx2 == nums2.length) || (idx1 < nums1.length && nums1[idx1] > nums2[idx2]);
+	}
+	
  	public int[] findMax(int[] nums, int digits) {
 		
 		// init res[digit], pointer of stack: top
