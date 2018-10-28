d823ed7248342e75b076e0114d99ee7162e4d93a
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-10-28 22:45:18 +0800

    376. Wiggle Subsequence

--- AllInOne/src/Greedy/WiggleSubsequence.java
+++ AllInOne/src/Greedy/WiggleSubsequence.java
@@ -32,6 +32,7 @@ package Greedy;
  */
 public class WiggleSubsequence {
 
+	/*
 	public int wiggleMaxLength(int[] nums) {
         
 		// iterate through nums
@@ -59,5 +60,38 @@ public class WiggleSubsequence {
 		}
 		
 		return res;
+    }
+	*/
+	
+	public int wiggleMaxLength(int[] nums) {
+		// if nums.len < 2, return nums.len
+		// find the fst [i] - [i - 1] that does not == 0
+        // cnt = pre == 0 ? 1: 2;
+		// int curNum = [i];
+		// for (i --> len) {
+		// cur = [i] - [i - 1]
+		// if (pre * cur < 0) {cnt ++, curNum = [i]}
+		// else 
+		// {if (pre > 0) curNum = max(cur, [i]) // 1 7 8
+		// else (pre < 0) curNum = min(cur, [i])} // 3 2 1
+		if (nums.length < 2) return nums.length;
+		int idx = 1, cnt = 1;
+		
+		while (idx < nums.length && nums[idx] - nums[idx - 1] == 0) idx ++;
+		if (idx == nums.length) return 1;
+		
+		int pre = nums[idx] - nums[idx - 1];
+		cnt ++;
+
+		for (idx ++; idx < nums.length; idx ++) {
+			int cur = nums[idx] - nums[idx - 1];
+			if (pre * cur < 0) {
+				pre = cur;
+				cnt ++;
+			}
+			// 1 7 8 or 3 2 1 or 3 2 2 curNum = [i]
+		}
+		
+		return cnt;
     }
 }
--- AllInOne/src/PreviousQuestions/Temp.java
+++ AllInOne/src/PreviousQuestions/Temp.java
@@ -16,6 +16,50 @@ public class Temp {
 		Interval(int s, int e) { start = s; end = e; }
 	}
 	
+//	public int wiggleMaxLength(int[] nums) {
+//		// if nums.len < 2, return nums.len
+//		// find the fst [i] - [i - 1] that does not == 0
+//        // cnt = pre == 0 ? 1: 2;
+//		// int curNum = [i];
+//		// for (i --> len) {
+//		// cur = [i] - [i - 1]
+//		// if (pre * cur < 0) {cnt ++, curNum = [i]}
+//		// else 
+//		// {if (pre > 0) curNum = max(cur, [i]) // 1 7 8
+//		// else (pre < 0) curNum = min(cur, [i])} // 3 2 1
+//		if (nums.length < 2) return nums.length;
+//		int idx = 1, cnt = 1;
+//		
+//		while (idx < nums.length && nums[idx] - nums[idx - 1] == 0) idx ++;
+//		if (idx == nums.length) return 1;
+//		
+//		int pre = nums[idx] - nums[idx - 1];
+//		int curNum = nums[idx];
+//		cnt ++;
+//		idx ++;
+//		
+//		for (; idx < nums.length; idx ++) {
+//			int cur = nums[idx] - nums[idx - 1];
+//			if (pre * cur < 0) {
+//				pre = cur;
+//				curNum = nums[idx];
+//				cnt ++;
+//			}
+//			else {
+//				if (pre > 0) {
+//					// 1 7 8
+//					curNum = Math.max(curNum, nums[idx]); 
+//				}
+//				else {
+//					// 3 2 1 or 3 2 2
+//					curNum = Math.min(curNum, nums[idx]);
+//				}
+//			}
+//		}
+//		
+//		return cnt;
+//    }
+
 	public int wiggleMaxLength(int[] nums) {
 		// if nums.len < 2, return nums.len
 		// find the fst [i] - [i - 1] that does not == 0
@@ -34,27 +78,15 @@ public class Temp {
 		if (idx == nums.length) return 1;
 		
 		int pre = nums[idx] - nums[idx - 1];
-		int curNum = nums[idx];
 		cnt ++;
-		idx ++;
-		
-		for (; idx < nums.length; idx ++) {
+
+		for (idx ++; idx < nums.length; idx ++) {
 			int cur = nums[idx] - nums[idx - 1];
 			if (pre * cur < 0) {
 				pre = cur;
-				curNum = nums[idx];
 				cnt ++;
 			}
-			else {
-				if (pre > 0) {
-					// 1 7 8
-					curNum = Math.max(curNum, nums[idx]); 
-				}
-				else {
-					// 3 2 1 or 3 2 2
-					curNum = Math.min(curNum, nums[idx]);
-				}
-			}
+			// 1 7 8 or 3 2 1 or 3 2 2 curNum = [i]
 		}
 		
 		return cnt;
