e3c29ebcb1944f02d5f82814479176f30141711a
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-10-30 00:43:16 +0800

    134. Gas Station

--- AllInOne/src/PreviousQuestions/Temp.java
+++ AllInOne/src/PreviousQuestions/Temp.java
@@ -2,7 +2,7 @@ package PreviousQuestions;
 
 /**
  * 
- * @author jingjiejiang Oct 29, 2018
+ * @author jingjiejiang Oct 30, 2018
  *
  */
 public class Temp {
@@ -15,7 +15,23 @@ public class Temp {
 	}
 	
 	public int canCompleteCircuit(int[] gas, int[] cost) {
-        return ;
+		// int sum = 0, pos = 0, totalSum = 0;
+		// for (i --> len)
+		//   sum += gas[i] - cost[i];
+		//   totalSum += sum;
+		//   if (sum < 0) {pos = i + 1; sum = 0;}
+		// return totalSum < 0 ? - 1 : pos;
+		long totalSum = 0; 
+		int curSum = 0, pos = 0;
+		for (int idx = 0; idx < cost.length; idx ++) {
+			curSum += gas[idx] - cost[idx];
+			totalSum += curSum;
+			if (curSum < 0) {
+				pos = idx + 1;
+				curSum = 0;
+			}
+		}
+        return totalSum < 0 ? -1 : pos;
     }
 	
 	public static void main(String[] args) {
