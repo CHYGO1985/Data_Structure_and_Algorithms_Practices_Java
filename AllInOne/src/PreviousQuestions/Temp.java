a1458f3852eb23a7d209e89518f6d9795ae43a6a
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-10-30 22:40:48 +0800

    55. Jump Game

--- AllInOne/src/PreviousQuestions/Temp.java
+++ AllInOne/src/PreviousQuestions/Temp.java
@@ -14,24 +14,14 @@ public class Temp {
 		Interval(int s, int e) { start = s; end = e; }
 	}
 	
-	public int canCompleteCircuit(int[] gas, int[] cost) {
-		// int sum = 0, pos = 0, totalSum = 0;
-		// for (i --> len)
-		//   sum += gas[i] - cost[i];
-		//   totalSum += sum;
-		//   if (sum < 0) {pos = i + 1; sum = 0;}
-		// return totalSum < 0 ? - 1 : pos;
-		long totalSum = 0; 
-		int curSum = 0, pos = 0;
-		for (int idx = 0; idx < cost.length; idx ++) {
-			curSum += gas[idx] - cost[idx];
-			totalSum += curSum;
-			if (curSum < 0) {
-				pos = idx + 1;
-				curSum = 0;
-			}
-		}
-        return totalSum < 0 ? -1 : pos;
+	public boolean canJump(int[] nums) {
+        int farthest = 0;
+        for (int idx = 0; idx < nums.length - 1; idx ++) {
+            if (idx <= farthest) {
+                farthest = Math.max(farthest, idx + nums[idx]);
+            }
+        }
+        return farthest < nums.length - 1 ? false : true;     
     }
 	
 	public static void main(String[] args) {
