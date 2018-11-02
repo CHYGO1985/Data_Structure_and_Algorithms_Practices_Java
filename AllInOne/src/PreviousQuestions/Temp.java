e533a5acc042988c819d7ea14b361dbbd3f65f45
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-11-02 19:11:02 +0800

    659. Split Array into Consecutive Subsequences

--- AllInOne/src/PreviousQuestions/Temp.java
+++ AllInOne/src/PreviousQuestions/Temp.java
@@ -1,10 +1,11 @@
 package PreviousQuestions;
 
-import java.awt.CardLayout;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 
- * @author jingjiejiang Oct 31, 2018
+ * @author jingjiejiang Nov 2, 2018
  *
  */
 public class Temp {
@@ -16,34 +17,54 @@ public class Temp {
 		Interval(int s, int e) { start = s; end = e; }
 	}
 	
-	public static String removeKdigits(String num, int k) {
-        // arr = num.len - k
-		// int top = 0;
-		// for (0 --> len)
-		//  while (top > 0 && num[i] < arr[top - 1] && num.len - idx + 1 + (top - 1) > arr.len)
-		//    top --
-		//    k --
-		// if (top < arr.len) arr[top ++] = num[i]
-		// get rid of heading 0 in arr
-		char[] arr = new char[num.length() - k];
-		int top = 0;
-		for (int idx = 0; idx < num.length(); idx ++) {
-			char curChar = num.charAt(idx);
-			while (top > 0 && curChar < arr[top - 1] && num.length() - idx + top > arr.length) {
-				top --;
+	public static boolean isPossible(int[] nums) {
+		// 1 2 3 3 4 5
+        // 1 2 3 4 5
+        // get count map<num, count>
+        // for (0 --> nums) 
+        // 3 cases
+        // if map.get(nums[i]) == 0 continue;
+        // if potentialMap.get(nums[i]) > 0 
+        //   potentialMap.[i] count - 1
+        //   potentialMap.nums[i] + 1 count + 1
+        // if map.get(nums[i] + 1) > 0 && map.get(nums[i] + 2) > 0
+        //   map.nums[i]+1 count - 1;
+        //   map.nums[i]+2 count - 1;
+        //   potentialMap.nums[i] + 2 count + 1
+        // return false
+		Map<Integer, Integer> numMap = new HashMap<>();
+		Map<Integer, Integer> pontentialMap = new HashMap<>();
+		
+		for (int num : nums) {
+			numMap.put(num, numMap.getOrDefault(num, 0) + 1);
+		}
+		
+		for (int num : nums) {
+			if (numMap.get(num) == 0) {
+                continue;
+            }
+			else if (pontentialMap.getOrDefault(num, 0) > 0) {
+				pontentialMap.put(num, pontentialMap.get(num) - 1);
+				pontentialMap.put(num + 1, pontentialMap.getOrDefault(num + 1, 0) + 1);
 			}
-			if (top < arr.length) arr[top ++] = curChar;
+			else if (numMap.getOrDefault(num + 1, 0) > 0 && 
+                     numMap.getOrDefault(num + 2, 0) > 0) {
+				numMap.put(num + 1, numMap.get(num + 1) - 1);
+				numMap.put(num + 2, numMap.get(num + 2) - 1);
+				pontentialMap.put(num + 3, pontentialMap.getOrDefault(num + 3, 0) + 1);
+			}
+			else {
+				return false;
+			}
+            
+            numMap.put(num, numMap.get(num) - 1);
 		}
 		
-		int start = 0;
-		while (start < arr.length && arr[start] == '0') {
-			start ++;
-		}
-		
-		return start == arr.length ? "0" : new String(arr, start, arr.length - start);
-    }
+		return true;
+	}
 	
 	public static void main(String[] args) {
-		int[] arr = new int[]{1,2,3,3};
+		int[] arr = new int[]{1,2,3,3, 4, 5};
+		isPossible(arr);
 	}
 }
