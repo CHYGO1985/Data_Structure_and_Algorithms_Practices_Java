179814fb6a88d9a675b96f25dc383289f6275937
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-11-07 23:58:11 +0800

    330. Patching Array

--- AllInOne/src/PreviousQuestions/Temp.java
+++ AllInOne/src/PreviousQuestions/Temp.java
@@ -15,12 +15,28 @@ public class Temp {
 	}
 	
 	public int minPatches(int[] nums, int n) {
+		int cnt = 0;
+        long sum = 0;
+        for (int num : nums) {
+            while (sum < num - 1 && sum < n) {
+                sum += sum + 1;
+                cnt ++;
+            }
+            
+            sum += num;
+        }
         
+        while (sum < n) {
+            sum += sum + 1;
+            cnt ++;
+        }
+        
+        return cnt;
     }
 	
 	public static void main(String[] args) {
 		int[] arr = new int[]{1,2,3,2,2,1};
 //		String s = "cbacdcbc";
-		candy(arr);
+//		candy(arr);
 	}
 }
