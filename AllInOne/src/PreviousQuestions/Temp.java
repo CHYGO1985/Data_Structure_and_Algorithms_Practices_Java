e06c32f6626409ee9b382385981a0f7082d63adf
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-11-09 23:55:08 +0800

    134. Gas Station

--- AllInOne/src/PreviousQuestions/Temp.java
+++ AllInOne/src/PreviousQuestions/Temp.java
@@ -2,7 +2,7 @@ package PreviousQuestions;
 
 /**
  * 
- * @author jingjiejiang Nov 8, 2018
+ * @author jingjiejiang Nov 9, 2018
  *
  */
 public class Temp {
@@ -14,30 +14,9 @@ public class Temp {
 		Interval(int s, int e) { start = s; end = e; }
 	}
 	
-	public int wiggleMaxLength(int[] nums) {
-	    
-		if (nums.length <= 1) return nums.length;
-		
-		int idx = 1;
-		// skip equal nums 1 1 1 2
-		while (nums[idx] == nums[idx - 1]) {
-            idx ++;
-            // *** commit error here
-            if (idx >= nums.length) return 1;
-        }
-		int preSign = nums[idx] - nums[idx - 1];
-		int cnt = preSign == 0 ? 1 : 2;
-		
-		for (idx += 1; idx < nums.length; idx ++) {
-			int curSign = nums[idx] - nums[idx - 1];
-			if (curSign * preSign < 0) {
-				preSign = curSign;
-				cnt ++;
-			}
-		}
-		
-		return cnt;
-    }
+	public int canCompleteCircuit(int[] gas, int[] cost) {
+		return 0;
+	}
 	
 	public static void main(String[] args) {
 		int[] arr = new int[]{1,2,3,2,2,1};
