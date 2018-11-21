4bfa05ced916483ef448e6d84055c2a1b4e524df
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-11-21 23:43:19 +0800

    temp commit

--- AllInOne/src/PreviousQuestions/Temp.java
+++ AllInOne/src/PreviousQuestions/Temp.java
@@ -1,8 +1,13 @@
 package PreviousQuestions;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * 
- * @author jingjiejiang Nov 20, 2018
+ * @author jingjiejiang Nov 21, 2018
  *
  */
 public class Temp {
@@ -14,8 +19,44 @@ public class Temp {
 		Interval(int s, int e) { start = s; end = e; }
 	}
 	
-	public boolean isPossible(int[] nums) {
+	public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
+        Interval interval = new Interval();
+        interval.start = 0;
+        interval.end = 1;
+        List<Interval> list = new LinkedList<>();
+        list.add(interval);
+        // init a prioritylist, sort the element in the list as 
+        // a.start != b.start ? a.start - b.start : a.end - b.end
+        // for (0 --> schedule.size)
+        //   for (slot : schedule.get(i))
+        //     new = slot
+        //     int j = 0;
+        //     for (0 -> prilist.size)
+        //       cur = prilist.get(j)
+        //       if (new.start < cur.end && new.start >= cur.start)
+        //         merge new into cur
+        //       else if (new.start >= cur.end)
+        //         continue
+        //       else if (new.end <= cur.start)
+        //        add to prilist, break;
+        //      if j >= list.size, then add new to prilist
         
+        List<Interval> prioList = new LinkedList<>();
+        Collections.sort(prioList, new Comparator<Interval>() {
+			@Override
+			public int compare(Interval o1, Interval o2) {
+				return o1.start == o2.start? o1.end - o2.end : o1.start - o2.start;
+			}
+		});
+        
+        for (int i = 0; i < schedule.size(); i ++) {
+        	for (int j = 0; j < schedule.get(i).size(); i ++) {
+        		for (int m = 0; m < prioList.size())
+        	}
+        }
+        
+        
+        return list;
     }
 	
 	public static void main(String[] args) {
