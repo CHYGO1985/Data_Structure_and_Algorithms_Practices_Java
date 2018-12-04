32ab3d2e306706c3ef6d7e3b69b5f3bcf955d3ba
Jingjie_Jiang <jingjiejiang85@gmail.com>
2018-12-05 02:10:50 +0800

    Setting env

--- AllInOne/src/PreviousQuestions/Temp.java
+++ AllInOne/src/PreviousQuestions/Temp.java
@@ -6,14 +6,14 @@ import java.util.List;
 
 /**
  * 
- * @author jingjiejiang Nov 30, 2018
+ * @author jingjiejiang Dec 5, 2018
  *
  */
 public class Temp {
 	
 	public class Interval {
 		int start;
-		int end;
+		int end; 
 		Interval() { start = 0; end = 0; }
 		Interval(int s, int e) { start = s; end = e; }
 	}
@@ -44,6 +44,7 @@ public class Temp {
 		int[] arr = new int[]{1,3,4,5,2};
 		int[] nums1 = new int[] {3, 4, 6, 5};
 		int[] nums2 = new int[] {9, 1, 2, 5, 8, 3};
+		System.out.println(1);
 //		System.out.print(candy(arr));
 //		maxNumber(nums1, nums2, 5);
 //		String s = "cbacdcbc";
