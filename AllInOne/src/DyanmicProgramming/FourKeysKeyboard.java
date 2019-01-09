422547bcb4c48e26b929106227fd128bfa25d549
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-01-09 20:34:25 +0800

    651. 4 Keys Keyboard

--- AllInOne/src/DyanmicProgramming/FourKeysKeyboard.java
+++ AllInOne/src/DyanmicProgramming/FourKeysKeyboard.java
@@ -24,4 +24,21 @@ public class FourKeysKeyboard {
 		
 		return res[N];
     }
+
+	/*
+	public int maxA(int N) {
+        
+		int[] dp = new int[N + 1];
+		dp[0] = 0;
+		
+		for (int idx = 0; idx < dp.length; idx ++) {
+			dp[idx] = idx;
+			for (int checkPoint = 3; checkPoint < idx; checkPoint ++) {
+				dp[idx] = Math.max(dp[idx], dp[idx - checkPoint] * (checkPoint - 1));
+			}
+		}
+        
+		return dp[N];  
+    }
+	*/
 }
