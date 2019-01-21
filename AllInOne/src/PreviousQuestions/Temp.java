833b43e8dc9546608bc73b962a3a5fb060a8af8c
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-01-22 01:05:43 +0800

    44. Wildcard Matching

--- AllInOne/src/PreviousQuestions/Temp.java
+++ AllInOne/src/PreviousQuestions/Temp.java
@@ -1,5 +1,7 @@
 package PreviousQuestions;
 
+import javax.print.attribute.standard.RequestingUserName;
+
 /**
  * 
  * @author jingjiejiang Jan 21, 2019
@@ -14,32 +16,41 @@ public class Temp {
 		Interval(int s, int e) { start = s; end = e; }
 	}
 	
-	public int maxA(int N) {
+	public boolean isMatch(String s, String p) {
 		
-		int[] res = new int[N + 1];
-		res[0] = 0;
-		
-		for (int idx = 1; idx <= N; idx ++) {
-			if (idx <= 6) {
-				res[idx] = idx;
-			} else {
-				res[idx] = Math.max(res[idx - 3] * 2, Math.max(res[idx - 4] * 3, 
-						Math.max(res[idx - 5] * 4, idx)));
-			}
-			
+		if (s.length() == 0) {
+			return (p.length() == 0 || p.equals("*"))? true : false;
 		}
 		
-		return res[N];
+		boolean[][] dp = new boolean[p.length() + 1][s.length() + 1];
+		dp[0][0] = true;
+        for (int row = 1; row < dp.length; row ++) {
+            if (p.charAt(row - 1) == '*') dp[row][0] = dp[row - 1][0];
+        }
+		
+		for (int row = 1; row < dp.length; row ++) {
+			
+			char rowChar = p.charAt(row - 1);
+			for (int col = 1; col < dp[0].length; col ++) {
+				char colChar = s.charAt(col - 1); 
+				if (rowChar == '?' || rowChar == colChar) {
+					dp[row][col] = dp[row - 1][col - 1];
+				} else if (rowChar == '*') {
+					dp[row][col] = dp[row][col - 1] || dp[row - 1][col - 1] 
+							|| dp[row - 1][col];
+				} else { // rowChar != colChar
+					dp[row][col] = false;
+				}
+			}
+		}
+		
+		return dp[dp.length - 1][dp[0].length - 1];
     }
 	
 	public static void main(String[] args) {
-		int[] arr = new int[]{1,3,4,5,2};
-		int[] nums1 = new int[] {3, 4, 6, 5};
-		int[] nums2 = new int[] {9, 1, 2, 5, 8, 3};
-		System.out.println(1);
-//		System.out.print(candy(arr));
-//		maxNumber(nums1, nums2, 5);
-//		String s = "cbacdcbc";
-//		candy(arr);
+//		int[] arr = new int[]{1,3,4,5,2};
+//		int[] nums1 = new int[] {3, 4, 6, 5};
+//		int[] nums2 = new int[] {9, 1, 2, 5, 8, 3};
+//		System.out.println(1);
 	}
 }
