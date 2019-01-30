6e46e5c4ef7595eacbf7f33760dedf97dfdc0620
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-01-31 00:29:33 +0800

    122. Best Time to Buy and Sell Stock II

--- AllInOne/src/BestTimeBuyAndSellStock/BestTimeBuyAndSellStockII.java
+++ AllInOne/src/BestTimeBuyAndSellStock/BestTimeBuyAndSellStockII.java
@@ -3,11 +3,25 @@ package BestTimeBuyAndSellStock;
 /**
  * 
  * @author jingjiejiang created on Sep 3, 2018
+ * 2nd Jan 30, 2-19
  *
  */
 public class BestTimeBuyAndSellStockII {
 	
 	// variable: day, buy/sell
+//	public static int maxProfit(int[] prices) {
+//        int[][] res = new int[2][2];
+//        res[0][0] = 0;
+//        res[0][1] = Integer.MIN_VALUE;
+//        for (int i = 0; i < prices.length; i ++) {
+//        	res[(i + 1) % 2][0] = Math.max(res[i % 2][0], res[i % 2][1] + prices[i]);
+//        	res[(i + 1) % 2][1] = Math.max(res[i % 2][1], res[i % 2][0] - prices[i]);
+//        }
+//        
+//        // only when hold 0 stock at the end will get the max
+//        return res[prices.length % 2][0];
+//    }
+	
 	public static int maxProfit(int[] prices) {
         int[][] res = new int[2][2];
         res[0][0] = 0;
