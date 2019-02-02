787654958c8f35fc3a6fd6eb6c0dbe83e1f95d5b
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-02-02 11:59:03 +0800

    339. Nested List Weight Sum

---
+++ AllInOne/src/Array/NestedListWeightSum.java
@@ -0,0 +1,34 @@
+package Array;
+
+import java.util.List;
+
+import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
+
+interface NestedInteger {
+	public boolean isInteger();
+	public Integer getInteger();
+	public List<NestedInteger> getList();
+}
+
+public class NestedListWeightSum {
+
+	public int depthSum(List<NestedInteger> nestedList) {
+		return calDepthSum(nestedList, 1);
+    }
+    
+    public int calDepthSum(List<NestedInteger> nestedList, int depth) {
+    	
+    	int sum = 0;
+    	
+    	for (int idx = 0; idx < nestedList.size(); idx ++) {
+    		if (nestedList.get(idx).isInteger()) {
+    			sum += nestedList.get(idx).getInteger() * depth;
+    		}
+    		else {
+    			sum += calDepthSum(nestedList.get(idx).getList(), depth + 1);
+    		}
+    	}
+    	
+		return sum;
+    }
+}
