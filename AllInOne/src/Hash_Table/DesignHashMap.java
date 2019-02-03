629d9702e91a329df201be26c60edc0c23eeb874
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-02-03 22:08:23 +0800

    706. Design HashMap use array as a database

--- AllInOne/src/Hash_Table/DesignHashMap.java
+++ AllInOne/src/Hash_Table/DesignHashMap.java
@@ -1,37 +1,34 @@
 package Hash_Table;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Arrays;
 
+/**
+ * 
+ * @author jingjiejiang Feb 3, 2019
+ *
+ */
 public class DesignHashMap {
 	
-	private List<Integer> dataList;
+	private int[] dataArr;
 	
 	public DesignHashMap() {
-        dataList = new LinkedList<>();
-        // init for 10 bucket
-        for (int idx = 0; idx <= 1000000; idx ++) {
-        	dataList.add(-1);
-        }
+		dataArr = new int[1000001];
+		Arrays.fill(dataArr, -1);
     }
     
     /** value will always be non-negative. */
     public void put(int key, int value) {
     	
-    	int insertPos = key == dataList.size() - 1 ? key - 1 : key;
-    	dataList.remove(key);
-    	dataList.add(insertPos, value);
+    	dataArr[key] = value;
     }
     
     /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     public int get(int key) {
-        return dataList.get(key);
+        return dataArr[key];
     }
     
     /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     public void remove(int key) {
-    	int insertPos = key == dataList.size() - 1 ? key - 1 : key;
-    	dataList.remove(key);
-    	dataList.add(insertPos, -1);
+    	dataArr[key] = -1;
     }
 }
