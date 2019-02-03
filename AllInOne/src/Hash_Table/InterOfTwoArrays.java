a8737c3934f20e5116100a39d8c018a66f13523f
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-02-03 22:27:04 +0800

    349. Intersection of Two Arrays

---
+++ AllInOne/src/Hash_Table/InterOfTwoArrays.java
@@ -0,0 +1,34 @@
+package Hash_Table;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 
+ * @author jingjiejiang Feb 3, 2019
+ *
+ */
+public class InterOfTwoArrays {
+	public int[] intersection(int[] nums1, int[] nums2) {
+        
+		Set<Integer> nums1Set = new HashSet<>();
+        Set<Integer> interSet = new HashSet<>();
+        
+        for (int num1 : nums1) {
+        	nums1Set.add(num1);
+        }
+        
+        for (int num2 : nums2) {
+        	if (nums1Set.contains(num2) == true) {
+        		interSet.add(num2);
+        	}
+        }
+        
+        Integer[] resInter = interSet.toArray(new Integer[interSet.size()]);
+        int[] res = new int[resInter.length];
+        for (int idx = 0; idx < res.length; idx ++) {
+        	res[idx] = resInter[idx].intValue();
+        }
+        return res;
+    }
+}
