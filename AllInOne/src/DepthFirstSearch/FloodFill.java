1124a5963166ad026032f2ab14878de284fd0f07
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-02-05 13:56:43 +0800

    733. Flood Fill use dfs method

--- AllInOne/src/DepthFirstSearch/FloodFill.java
+++ AllInOne/src/DepthFirstSearch/FloodFill.java
@@ -11,7 +11,7 @@ import java.util.Queue;
 public class FloodFill {
 	
 	private static int[][] directions = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-	
+	/*
 	public static int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
 		
 		Queue<int[]> nextPoints = new LinkedList<int[]>();
@@ -48,9 +48,25 @@ public class FloodFill {
 		
 		bfs(image, nextPoints, visitMap, newColor);
 	}
+	*/
 	
-	public static void main(String[] args) {
-		int[][] image = new int[][] {{1,1,1},{1,1,0},{1,0,1}};
-		floodFill(image, 1, 1, 2);
-	}
+	public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        if (image[sr][sc] == newColor) return image;
+        fill(image, sr, sc, image[sr][sc], newColor);
+        return image;
+    }
+    
+    private void fill(int[][] image, int sr, int sc, int color, int newColor) {
+        if (sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length || image[sr][sc] != color) return;
+        image[sr][sc] = newColor;
+        
+        for (int[] direction : directions) {
+        	fill(image, sr + direction[0], sc + direction[1], color, newColor);
+        }
+    }
+	
+//	public static void main(String[] args) {
+//		int[][] image = new int[][] {{1,1,1},{1,1,0},{1,0,1}};
+//		floodFill(image, 1, 1, 2);
+//	}
 }
