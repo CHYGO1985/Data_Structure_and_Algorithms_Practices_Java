2cdafdb62f3555563cf53d464e58ef2b8d169b23
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-02-08 16:37:49 +0800

    543. Diameter of Binary Tree

--- AllInOne/src/Tree/DiameterOfBinaryTree.java
+++ AllInOne/src/Tree/DiameterOfBinaryTree.java
@@ -1,3 +1,4 @@
+package Tree;
 
 /**
  * 
@@ -12,6 +13,7 @@
  * @author jingjiejiang
  * @history
  * 1. Oct 11, 2017
+ * 2. Feb 8, 2019
  */
 public class DiameterOfBinaryTree {
 
@@ -47,4 +49,27 @@ public class DiameterOfBinaryTree {
         sum[0] = Math.max(sum[0], left + right + 1);
         return Math.max(left, right) + 1;
     }
+    
+    // same method, but use sum as a property of a class
+    /*
+    private int sum = 0;
+    
+    public int diameterOfBinaryTree(TreeNode root) {
+        
+        findDepth(root);
+        
+        return sum > 0 ? sum - 1 : sum;
+    }
+    
+    public int findDepth(TreeNode root) {
+        
+        if (root == null) return 0;
+        
+        int lDepth = findDepth(root.left);
+        int rDepth = findDepth(root.right);
+        sum = Math.max(sum, 1 + lDepth + rDepth);
+        
+        return 1 + Math.max(lDepth, rDepth);
+    }
+    */
 }
