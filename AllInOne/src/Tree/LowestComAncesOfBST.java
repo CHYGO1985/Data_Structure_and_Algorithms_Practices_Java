15688efbbe09e6def622a674eb47fae6155cfcf7
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-02-09 18:46:30 +0800

    235. Lowest Common Ancestor of a Binary Search Tree

---
+++ AllInOne/src/Tree/LowestComAncesOfBST.java
@@ -0,0 +1,34 @@
+package Tree;
+
+/**
+ * 
+ * @author jingjiejiang Feb 9, 2019
+ *
+ */
+public class LowestComAncesOfBST {
+	public static class TreeNode {
+		
+		public int val;
+		public TreeNode left;
+		public TreeNode right;
+		
+		public TreeNode(int val) {
+			this.val = val;
+		}
+	}
+	
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+		
+		if (root == null) return null;
+		
+		TreeNode left = lowestCommonAncestor(root.left, p, q);
+		TreeNode right = lowestCommonAncestor(root.right, p, q);
+		
+		if (root.val == p.val || root.val == q.val) {
+			return root;
+		} else {
+			return (left != null && right != null) ? root : 
+				(left == null ? right : left);
+		}
+	}
+}
