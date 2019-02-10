8bc340c37d8b068d3f53db21d18af10242f3dbf6
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-02-11 00:27:29 +0800

    inital imple

---
+++ AllInOne/src/Graph/NetworkDelayTime.java
@@ -0,0 +1,12 @@
+package Graph;
+
+/**
+ * 
+ * @author jingjiejiang Feb 10, 2019
+ *
+ */
+public class NetworkDelayTime {
+	public int networkDelayTime(int[][] times, int N, int K) {
+        
+    }
+}
