a23ee5d6fca05317a7df34a3e2d1274e792338d7
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-02-10 16:21:55 +0800

    191. Number of 1 Bits

--- AllInOne/src/Bit_Manipuation/NumberOf1Bits.java
+++ AllInOne/src/Bit_Manipuation/NumberOf1Bits.java
@@ -1,10 +1,16 @@
+package Bit_Manipuation;
 
+/**
+ * 
+ * @author jingjiejiang Feb 10, 2019
+ *
+ */
 public class NumberOf1Bits {
+	/*
     public static int hammingWeight(int n) {
         
         // straight forward: & 1 from bit 1 to 32
         // *** result: 2ms, beat around 16%
-    	/*
         int oneBits = 0;
         
         for (int i = 0; i < 32; i ++) {
@@ -20,7 +26,6 @@ public class NumberOf1Bits {
         }
     
         return oneBits;
-        */
          // you need to treat n as an unsigned value
          // http://bookshadow.com/weblog/2015/03/10/leetcode-number-1-bits/
          // *** result: 1ms, beat 89.42%, the fatest solution.
@@ -32,14 +37,40 @@ public class NumberOf1Bits {
         }
         return ans;
     }
-    
+    */
+	// 1111 1111 1111 1111 1111 1111 1111 1101
+	// 1000 0000 0000 0000 0000 0000 0000 0000
+	// 111  1111 1111 1111 1111 1111 1111 1111
+	// 1111 1111 1111 1111 1111 1111 1111 1111
+	public static int hammingWeight(int n) {
+        
+//		int comNum = 1 << 31;
+//		int count = 32, sum = 0;
+//		
+//		while (count -- >= 0) {
+//			sum += (comNum & n) != 0? 1 : 0;
+//			n = n << 1;
+//		}
+//		
+//		return sum;
+		
+		// Best
+		int ans = 0;
+        while (n != 0) {
+            ans += n & 1;
+            n >>>= 1;
+            System.out.println("count: " + ans + " n: " +Integer.toBinaryString(n));
+        }
+        return ans;
+    }
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println(hammingWeight(1101));
 		
-		System.out.println(hammingWeight(11));
-		System.out.println(Integer.toBinaryString(1<<31));
+		System.out.println(Integer.parseInt("01111111111111111111111111111111", 2));
+		System.out.println(Integer.toBinaryString(Integer.MAX_VALUE));
 		System.out.println(Integer.toBinaryString(-2147483648));
 		
 	}
-
 }
