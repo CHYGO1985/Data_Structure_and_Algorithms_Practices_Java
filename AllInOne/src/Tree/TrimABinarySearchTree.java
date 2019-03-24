fd3f537f6c0459483bced57ffb5159c4af9166bc
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-03-24 22:00:56 +0800

    669. Trim a Binary Search Tree

--- AllInOne/src/Tree/TrimABinarySearchTree.java
+++ AllInOne/src/Tree/TrimABinarySearchTree.java
@@ -48,19 +48,18 @@ public class TrimABinarySearchTree {
     }
 	*/
 	
-	public TreeNode TrimBST(TreeNode root, int L, int R) {
+	public TreeNode trimBST(TreeNode root, int L, int R) {
         
-		if (root == null) return null;
+        if (root == null) return null;
         
         if (root.val < L) {
-        	return TrimBST(root.right, L, R);
+        	trimBST(root.right, L, R);
         } else if (root.val > R) {
-        	return TrimBST(root.left, L, R);
+        	trimBST(root.left, L, R);
         }
         
-        root.left = TrimBST(root.left, L, R);
-        root.right = TrimBST(root.right, L, R);
-        
+        root.left = trimBST(root.left, L, R);
+        root.right = trimBST(root.right, L, R);
         return root;
     }
 }
