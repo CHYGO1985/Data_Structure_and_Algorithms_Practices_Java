ad502c22446b331f80e8501f5e6bdddc33e389bf
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-03-27 23:53:47 +0800

    784. Letter Case Permutation

--- AllInOne/src/DepthFirstSearch/LetterCasePermutation.java
+++ AllInOne/src/DepthFirstSearch/LetterCasePermutation.java
@@ -1,33 +1,36 @@
 package DepthFirstSearch;
 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
  * 
- * @author jingjiejiang Mar 26, 2019
+ * @author jingjiejiang Mar 27, 2019
  *
  */
 public class LetterCasePermutation {
 	public List<String> letterCasePermutation(String S) {
+        char[] arr = S.toCharArray();
+        List<String> list = new LinkedList<>();
+        dfs(list, 0, arr);
         
+        return list;
     }
-	
-	private void dfs(String S, List<String> list, int start, StringBuilder builder) {
-		
-		if (start >= S.length()) {
-			list.add(builder.toString());
-			return ;
-		}
-		
-		for (int idx = start; idx < S.length(); idx ++) {
-			
-			char cur = S.charAt(idx);
-			if (Character.isLetter(cur)) {
-				cur = Character.toUpperCase(cur);
-			}
-			builder.append(cur);
-			dfs(S, list, start + 1, builder);
-			
-		}
-	}
+    
+    private void dfs (List<String> list, int index, char[] arr) {
+        if (index >= arr.length) {
+            list.add(String.valueOf(arr));
+            return ;
+        }
+        
+        // if it is a digit number, pass to next step
+        if (Character.isDigit(arr[index])) {
+            dfs(list, index + 1, arr);
+        }
+        else {
+            dfs(list, index + 1 , arr);
+            arr[index] = Character.isLowerCase(arr[index]) ? (char)(arr[index] - 32) : (char)(arr[index] + 32);
+            dfs(list, index + 1, arr);  
+        }
+    } 
 }
