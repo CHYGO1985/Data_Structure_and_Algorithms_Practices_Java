f96fcb492eef42ede3742378053a8c8bef88a3da
JJ Jiang <jingjiejiang85@gmail.com>
2020-08-30 13:36:28 +0800

    300. Longest Increasing Subsequence dp

--- AllInOne/src/DyanmicProgramming/LongestIncreasingSubsequence.java
+++ AllInOne/src/DyanmicProgramming/LongestIncreasingSubsequence.java
@@ -6,20 +6,44 @@ import java.util.Arrays;
  * @hitory Aug 29, 2020
  */
 class Solution {
+
+  // method1: binary search method
+  // public int lengthOfLIS(int[] nums) {
+  //      // method 1: dp
+  //      int[] dp = new int[nums.length];
+  //      int len = 0;
+
+  //      for (int num : nums) {
+  //        int i = Arrays.binarySearch(dp, 0, len, x);
+  //        if (i < 0)
+  //         i = - (i + 1);
+  //         dp[i] = x;
+
+  //         if (i == len) len ++;
+  //      }
+
+  //      return len;
+  // }
+
+  // method 2: dp
   public int lengthOfLIS(int[] nums) {
-       // method 1: dp
-       int[] dp = new int[nums.length];
-       int len = 0;
+    
+    if (nums == null || nums.length == 0) return 0;
 
-       for (int num : nums) {
-         int i = Arrays.binarySearch(dp, 0, len, x);
-         if (i < 0)
-          i = - (i + 1);
-          dp[i] = x;
+    int[] dp = new int[nums.length]; 
+    Arrays.fill(dp, 1);
+    int max = 1;
 
-          if (i == len) len ++;
-       }
+    for (int outIdx = 0; outIdx < nums.length; outIdx ++) {
+      for (int inIdx = 0; inIdx < outIdx; inIdx ++) {
 
-       return len;
+        if (nums[inIdx] < nums[outIdx]) {
+          dp[outIdx] = Math.max(dp[outIdx], 1 + dp[inIdx]);
+          max = Math.max(dp[outIdx], max); 
+        }        
+      }
+    }
+
+    return max;
   }
 }
