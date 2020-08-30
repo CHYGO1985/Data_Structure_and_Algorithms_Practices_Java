174b946388db1ba15d079e1c871c76916e41afdb
JJ Jiang <jingjiejiang85@gmail.com>
2020-08-30 21:39:52 +0800

    44. Wildcard Matching (revise version)

--- AllInOne/src/DyanmicProgramming/WildcardMatching.java
+++ AllInOne/src/DyanmicProgramming/WildcardMatching.java
@@ -3,34 +3,39 @@ package DyanmicProgramming;
 /**
  * 
  * @author jingjiejiang Jan 22, 2019
+ * @history Aug 30, 2020
  *
  */
 public class WildcardMatching {
 
 	public boolean isMatch(String s, String p) {
-		
-		boolean[][] dp = new boolean[p.length() + 1][s.length() + 1];
-		dp[0][0] = true;
-        for (int row = 1; row < dp.length; row ++) {
-            if (p.charAt(row - 1) == '*') dp[row][0] = dp[row - 1][0];
+        
+        if (s == null) return false;
+        
+        boolean[][] dp = new boolean[p.length() + 1][s.length() + 1];
+        dp[0][0] = true;
+        
+        for (int row = 1; row <= p.length(); row ++) {
+            dp[row][0] = p.charAt(row - 1) == '*'? dp[row - 1][0] : false; 
         }
-		
-		for (int row = 1; row < dp.length; row ++) {
-			
-			char rowChar = p.charAt(row - 1);
-			for (int col = 1; col < dp[0].length; col ++) {
-				char colChar = s.charAt(col - 1); 
-				if (rowChar == '?' || rowChar == colChar) {
-					dp[row][col] = dp[row - 1][col - 1];
-				} else if (rowChar == '*') {
-					dp[row][col] = dp[row][col - 1] || dp[row - 1][col - 1] 
-							|| dp[row - 1][col];
-				} else { // rowChar != colChar
-					dp[row][col] = false;
-				}
-			}
-		}
-		
-		return dp[dp.length - 1][dp[0].length - 1];
+        
+        for (int row = 1; row <= p.length(); row ++) {
+            
+            char pChar = p.charAt(row - 1);
+            for (int col = 1; col <= s.length(); col ++) {
+                
+                char sChar = s.charAt(col - 1);
+                
+                if (sChar == pChar || pChar == '?') {
+                    dp[row][col] = dp[row - 1][col - 1];
+                } else if (pChar == '*') {
+                    dp[row][col] = dp[row - 1][col - 1] || dp[row][col - 1] || dp[row - 1][col];
+                } else {
+                    dp[row][col] = false;
+                }
+            }
+        }
+        
+        return dp[p.length()][s.length()];
     }
 }
