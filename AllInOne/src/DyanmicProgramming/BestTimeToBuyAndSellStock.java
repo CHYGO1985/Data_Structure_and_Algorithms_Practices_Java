f72cc9bcc6c85db579545b1a09cbaa251bc75323
JJ Jiang <jingjiejiang85@gmail.com>
2020-09-14 08:53:00 +0800

    121. Best Time to Buy and Sell Stock -- optimize

--- AllInOne/src/DyanmicProgramming/BestTimeToBuyAndSellStock.java
+++ AllInOne/src/DyanmicProgramming/BestTimeToBuyAndSellStock.java
@@ -6,21 +6,43 @@
  * @history Sep 14, 2020
  */
 class Solution {
+
+    // DP: without optimize
+    // public int maxProfit(int[] prices) {
+                    
+    //     if (prices == null || prices.length == 0) return 0;
+        
+    //     // 0: sell, 1: buy
+    //     int[][] profits = new int[prices.length][2];
+    //     profits[0][0] = 0;
+    //     profits[0][1] = - prices[0];
+        
+    //     for (int idx = 1; idx < profits.length; idx ++) {
+            
+    //         profits[idx][0] = Math.max(profits[idx - 1][0], profits[idx - 1][1] + prices[idx]);
+    //         profits[idx][1] = Math.max(profits[idx - 1][1], - prices[idx]);
+    //     }
+        
+    //     return profits[prices.length - 1][0];
+    // }
+
+
+    // optimize to 1 demensional array
     public int maxProfit(int[] prices) {
                     
         if (prices == null || prices.length == 0) return 0;
         
         // 0: sell, 1: buy
-        int[][] profits = new int[prices.length][2];
-        profits[0][0] = 0;
-        profits[0][1] = - prices[0];
+        int[] profits = new int[2];
+        profits[0] = 0;
+        profits[1] = - prices[0];
         
-        for (int idx = 1; idx < profits.length; idx ++) {
+        for (int idx = 1; idx < prices.length; idx ++) {
             
-            profits[idx][0] = Math.max(profits[idx - 1][0], profits[idx - 1][1] + prices[idx]);
-            profits[idx][1] = Math.max(profits[idx - 1][1], - prices[idx]);
+            profits[0] = Math.max(profits[0], profits[1] + prices[idx]);
+            profits[1] = Math.max(profits[1], - prices[idx]);
         }
         
-        return profits[prices.length - 1][0];
+        return profits[0];
     }
 }
