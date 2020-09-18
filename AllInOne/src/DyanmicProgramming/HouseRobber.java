885b95c67e8613cce648b8a04ab56fb070eeffd0
JJ Jiang <jingjiejiang85@gmail.com>
2020-09-18 09:43:21 +0800

    198. House Robber

--- AllInOne/src/DyanmicProgramming/HouseRobber.java
+++ AllInOne/src/DyanmicProgramming/HouseRobber.java
@@ -2,19 +2,37 @@ package DyanmicProgramming;
 
 /**
  * 
+ * 198. House Robber
+ * 
  * @author jingjiejiang Feb 10, 2019
- *
+ * @history 
  */
 public class HouseRobber {
-	public int rob(int[] nums) {
+	// public int rob(int[] nums) {
 		
-        int[] profits = new int[nums.length + 2]; 
-        profits[0] = profits[1] = 0;
+    //     int[] profits = new int[nums.length + 2]; 
+    //     profits[0] = profits[1] = 0;
         
-        for (int idx = 0; idx < nums.length; idx ++) {
-        	profits[idx + 2] = Math.max(nums[idx] + profits[idx - 2], profits[idx + 1]);
+    //     for (int idx = 0; idx < nums.length; idx ++) {
+    //     	profits[idx + 2] = Math.max(nums[idx] + profits[idx - 2], profits[idx + 1]);
+    //     }
+        
+    //     return profits[profits.length - 1];
+    // }
+
+    public int rob(int[] nums) {
+		
+        if (nums == null || nums.length == 0) return 0;
+
+        int numSize = nums.length;
+        int[] dp = new int[numSize + 2];
+        Arrays.fill(dp, 0);
+
+        for (int idx = numSize - 1; idx >= 0; idx --) {
+
+            dp[idx] = Math.max(dp[idx + 1], nums[idx] + dp[idx + 2]);
         }
         
-        return profits[profits.length - 1];
+        return dp[0];
     }
 }
