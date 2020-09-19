83722375d1fec839c8cedd02035db786e38238b9
JJ Jiang <jingjiejiang85@gmail.com>
2020-09-19 12:50:08 +0800

    337. House Robber III

--- AllInOne/src/DyanmicProgramming/HouseRobberIII.java
+++ AllInOne/src/DyanmicProgramming/HouseRobberIII.java
@@ -25,23 +25,46 @@ import java.util.HashMap;
  */
 class Solution {
 
-    private Map<TreeNode, Integer> nodeResMap = new HashMap<>();
+    // Method 1: https://github.com/labuladong/fucking-algorithm/blob/master/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92%E7%B3%BB%E5%88%97/%E6%8A%A2%E6%88%BF%E5%AD%90.md
+    // private Map<TreeNode, Integer> nodeResMap = new HashMap<>();
 
+    // public int rob(TreeNode root) {
+        
+    //     if (root == null) return 0;
+
+    //     if (nodeResMap.containsKey(root)) return nodeResMap.get(root);
+
+    //     int robRoot = root.val
+    //         + (root.left == null ? 0 : rob(root.left.left) + rob(root.left.right))
+    //         + (root.right == null ? 0 : rob(root.right.left) + rob(root.right.right));
+    //     int notRobRoot = rob(root.left) + rob(root.right);
+
+    //     int res = Math.max(robRoot, notRobRoot);
+
+    //     nodeResMap.put(root, res);
+
+    //     return res;
+    // }
+
+    // Method 2: https://github.com/labuladong/fucking-algorithm/blob/master/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92%E7%B3%BB%E5%88%97/%E6%8A%A2%E6%88%BF%E5%AD%90.md
     public int rob(TreeNode root) {
         
-        if (root == null) return 0;
+        int[] res = dp(root);
 
-        if (nodeResMap.containsKey(root)) return nodeResMap.get(root);
+        return Math.max(res[0], res[1]);
+    }
 
-        int robRoot = root.val
-            + (root.left == null ? 0 : rob(root.left.left) + rob(root.left.right))
-            + (root.right == null ? 0 : rob(root.right.left) + rob(root.right.right));
-        int notRobRoot = rob(root.left) + rob(root.right);
+    // 0: not rob root, 1: rob root
+    private int[] dp(TreeNode root) {
 
-        int res = Math.max(robRoot, notRobRoot);
+        if (root == null) return new int[]{0, 0};
 
-        nodeResMap.put(root, res);
+        int[] left = dp(root.left);
+        int[] right = dp(root.right);
 
-        return res;
+        int robRoot = root.val + left[0] + right[0];
+        int notRobRoot = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+
+        return new int[]{notRobRoot, robRoot};
     }
 }
