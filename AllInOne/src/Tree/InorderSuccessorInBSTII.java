37ffe8a7169961e8417bc734217cd8a334fb3a96
CHYGO1985 <jingjiejiang85@gmail.com>
2020-09-22 23:53:47 +0800

    510. Inorder Successor in BST II

---
+++ AllInOne/src/Tree/InorderSuccessorInBSTII.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ * 510. Inorder Successor in BST II
+ * 
+ * @author jingjiejiang
+ * @history Sep 22, 2020 
+ */
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node parent;
+};
+*/
+
+class Solution {
+  public Node inorderSuccessor(Node node) {
+      
+  }
+}
