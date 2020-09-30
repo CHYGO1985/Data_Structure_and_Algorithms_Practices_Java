c2ac77ce47bdfd64f8420ef1197c03c7bcf41c48
JJ Jiang <jingjiejiang85@gmail.com>
2020-09-30 17:04:28 +0800

    239. Sliding Window Maximum

--- AllInOne/src/DyanmicProgramming/SlidingWindowMaximum.java
+++ AllInOne/src/DyanmicProgramming/SlidingWindowMaximum.java
@@ -14,5 +14,26 @@ class Solution {
     if (k == 1) return nums;
 
     int[] left = new int[len];
+    left[0] = nums[0];
+
+    int[] right = new int[len];
+    right[len - 1] = nums[len - 1];
+
+    for (int idx = 1; idx < len; idx ++) {
+
+      if (idx % k == 0) left[idx] = nums[idx];
+      else left[idx] = Math.max(left[idx - 1], nums[idx]);
+
+      if ((len - 1 - idx + 1) % k == 0) right[len - 1 - idx] = nums[len - 1 - idx];
+      else right[len - 1 - idx] = Math.max(right[len - 1 - idx + 1], nums[len - 1 - idx]);
+    }
+
+    int[] output = new int[len - k + 1];
+    
+    for (int idx = 0; idx < output.length; idx ++) {
+      output[idx] = Math.max(left[idx + k - 1], right[idx]);
+    }
+
+    return output;
   }
 }
