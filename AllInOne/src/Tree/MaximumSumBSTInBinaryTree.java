32a1a75ee299de52638b6358dfe9471c0f64f48c
JJ Jiang <jingjiejiang85@gmail.com>
2020-10-03 18:14:29 +0800

    1373. Maximum Sum BST in Binary Tree

---
+++ AllInOne/src/Tree/MaximumSumBSTInBinaryTree.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ * 1373. Maximum Sum BST in Binary Tree
+ * 
+ * @CHYGO1985
+ * @history Oct 3, 2020
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    private int max = 0;
+
+    public int maxSumBST(TreeNode root) {
+        
+        if (root == null) return 0;
+
+        sum(root);
+
+        return max;
+    }
+
+    private Integer[] sum(TreeNode root) {
+
+        // 0: val 1: min 2: max
+        // val = null(invalid BST);
+        if (root == null) return new Integer[]{0, Integer.MIN_VALUE, Integer.MAX_VALUE};
+
+        Integer[] leftSum = sum(root.left);
+        Integer[] rightSum = sum(root.right);
+
+        if (leftSum == null || rightSum == null || root.val <= leftSum[2] || root.val >= rightSum[1]) {
+            return null;
+        }
+
+        int sum = leftSum[0] + rightSum[0] + root.val;
+        max = Math.max(max, sum);
+        int min = Math.min(leftSum[0], rightSum[0]);
+        int max = Math.max(leftSum[0], rightSum[0]);
+
+        return new Integer[]{sum, min, max};
+    }
+}
