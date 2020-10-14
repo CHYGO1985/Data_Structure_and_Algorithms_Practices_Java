1451a286ce8c523401293d502efb60ec0031a24f
JJ Jiang <jingjiejiang85@gmail.com>
2020-10-15 07:48:24 +0800

    746. Min Cost Climbing Stairs

--- AllInOne/src/DyanmicProgramming/MinCostClimbingStairs.java
+++ AllInOne/src/DyanmicProgramming/MinCostClimbingStairs.java
@@ -1,22 +1,42 @@
 package DyanmicProgramming;
 
+import sun.jvm.hotspot.debugger.cdbg.IndexableFieldIdentifier;
+
 /**
  * 
  * @author jingjiejiang Feb 8, 2019
+ * @history Oct 15, 2020
  *
  */
 public class MinCostClimbingStairs {
 
+	// public int minCostClimbingStairs(int[] cost) {
+        
+	// 	int[] costs = new int[cost.length + 1];
+	// 	costs[0] = 0;
+	// 	costs[1] = cost[0];
+		
+	// 	for (int idx = 1; idx < cost.length; idx ++) {
+	// 		costs[idx + 1] = Math.min(cost[idx] + costs[idx], cost[idx] + costs[idx - 1]);
+	// 	}
+		
+	// 	return costs[cost.length];
+	// }
+
+	// optimized DP
 	public int minCostClimbingStairs(int[] cost) {
         
-		int[] costs = new int[cost.length + 1];
-		costs[0] = 0;
-		costs[1] = cost[0];
-		
-		for (int idx = 1; idx < cost.length; idx ++) {
-			costs[idx + 1] = Math.min(cost[idx] + costs[idx], cost[idx] + costs[idx - 1]);
+        int first = cost[0];
+		int second = cost[1];
+		int third = 0;
+
+		for (int idx = 2; idx < cost.length; IndexableFieldIdentifier ++) {
+			
+			third = Math.min(first, second) + cost[idx];
+			first = second;
+			second = third;
 		}
-		
-		return costs[cost.length];
+
+		return Math.min(first, second);
     }
 }
