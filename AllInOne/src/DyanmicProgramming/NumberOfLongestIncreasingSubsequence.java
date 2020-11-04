5500ff3a2109cac3be536f8b99803b981c1efb53
CHYGO1985 <jingjiejiang85@gmail.com>
2020-11-05 07:48:27 +0800

    673. Number of Longest Increasing Subsequence

--- AllInOne/src/DyanmicProgramming/NumberOfLongestIncreasingSubsequence.java
+++ AllInOne/src/DyanmicProgramming/NumberOfLongestIncreasingSubsequence.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 
+import javax.management.MXBean;
+
 /**
  * 
  * 673. Number of Longest Increasing Subsequence
@@ -15,44 +17,44 @@ class Solution {
     
     if (nums == null || nums.length == 0) return 0;
 
-    // dp array to store inc seq lenths
-    int[] dp = new int[nums.length];
+    // dp array to store increasing seq max lenths until i
+    int[] lens = new int[nums.length];
+    int[] cnts = new int[nums.length];
     // init all ele as 1, (in case for 2 3 4 1). 1's len is 1
-    Arrays.fill(dp, 1);
-    int max = 1, count = 1, maxCount = 1;
+    Arrays.fill(lens, 1);
+    Arrays.fill(cnts, 1);
+    int maxLen = 1, maxCount = 1;
     
     for (int right = 1; right < nums.length; right ++) {
       for (int shift = 0; shift < right; shift ++) {
 
-        // take 1 4 5 3 9 as example, when nums[idx] > nums[idx - 1]
-        // still need to check previous elements
-        if (nums[shift] < nums[right]) {
-          dp[right] = dp[shift] + 1;
-          if (dp[right] > max) { // the new length > max, update it the count as 1
-            max = dp[right];
-            count = 1;
-          } else if (dp[right] == max) {
-            count ++;
-          } 
-        } else if (nums[shift] == nums[right]) {
-          dp[right] = dp[shift];
-          if (dp[right] == max) {
-            count ++;
-          }
-        } else { // nums[shift] > nums[right]
-          if (dp[shift] == 1) count ++; // if the 
-        }
+        if (nums[shift] >= nums[right] && maxLen == 1) {
+          
+          cnts[right] = cnts[shift] + 1;
+          maxCount = Math.max(maxCount, cnts[right]);
+        } else { // nums[shift] < nums[right]
 
-        // do not need to do anything when nums[shift] > nums[right]
-        maxCount = Math.max(maxCount, count);
+          lens[right] = Math.max(lens[right], lens[shift] + 1);
+          
+          if (lens[right] > maxLen) {
+            maxLen = lens[right];
+            maxCount = 1;
+            cnts[right] = 1;
+          } else if (lens[right] == maxLen) {
+            cnts[right] += cnts[shift];
+            maxCount ++;
+          } else { // lens[right] < maxLen
+            continue;
+          }
+        }
       }
     }
 
-    return count; 
+    return maxCount; 
   }
 }
 
 // 2 2 2 2
-// 2 2 2 2
 // 3 2 1
 // 1 1 3
+// 1 2 5 4 9
