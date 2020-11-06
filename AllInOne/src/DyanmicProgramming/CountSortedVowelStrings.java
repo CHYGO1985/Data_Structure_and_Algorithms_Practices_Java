715a01e4ba27ca8ad120a34057377fde2d3c020d
JJ Jiang <jingjiejiang85@gmail.com>
2020-11-06 15:24:51 +0800

    1641. Count Sorted Vowel Strings

---
+++ AllInOne/src/DyanmicProgramming/CountSortedVowelStrings.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ * 1641. Count Sorted Vowel Strings
+ * 
+ * @CHYGO1985
+ * @history Nov 6, 2020
+ * 
+ */
+class Solution {
+    public int countVowelStrings(int n) {
+        
+        if (n < 1) return 0;
+        
+        int rowLen = 5 + 1, colLen = n;
+        // row: 5 + 1
+        int[][] dp = new int[rowLen][colLen];
+
+        // init dp[5][x] as 0
+        for (int col = 0; col < colLen; col ++) {
+            dp[5][col] = 0;
+        }
+
+        // init dp[4][x] as 1
+        for (int col = 0; col < colLen; col ++) {
+            dp[4][col] = 1;
+        }
+
+        // states transfer formular
+        // dp[row][col] = 5 - row (when col = n - 1)
+        // dp[row][col] = dp[row][col + 1] + dp[row + 1][col + 1] + dp[row + 2][col + 1] ... until dp[row + x][col + 1]
+        // (row + x) < 5
+        for (int row = 3; row >= 0; row --) {
+            for (int col = colLen - 1; col >= 0; col --) {
+
+                if (col == colLen - 1) {
+                    dp[row][col] = 5 - row;
+                } else {
+                    for (int curRow = row; curRow < rowLen - 1; curRow ++) {
+                        dp[row][col] += dp[curRow][col + 1];
+                    }
+                }
+            }
+        }
+
+        return dp[0][0]; 
+    }
+}
