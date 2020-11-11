354ed38905d5bbe4617fc697145b17e3153b49cd
JJ Jiang <jingjiejiang85@gmail.com>
2020-11-11 14:38:06 +0800

    1048. Longest String Chain

---
+++ AllInOne/src/DyanmicProgramming/LongestStringChain.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Map;
+
+/**
+ * 
+ * 1048. Longest String Chain
+ * 
+ * @CHYGO1985
+ * @history Nov 11, 2020
+ * 
+ */
+class Solution {
+    public int longestStrChain(String[] words) {
+        
+        if (words == null || words.length == 0) return 0;
+
+        int res = 0;
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+        // Map<String, Integer> word and its longest chain length
+        Map<String, Integer> wordChainLen = new HashMap<>();
+
+        for (String word : words) {
+
+            int curLongest = 0;
+            // skip letters in word one by one, and then check if there exist previous word
+            for (int idx = 0; idx < word.length(); idx ++) {
+
+                String prev = word.substring(0, idx) + word.substring(idx + 1);
+                curLongest = Math.max(curLongest, wordChainLen.getOrDefault(prev, 0) + 1);
+            }
+
+            wordChainLen.put(word, curLongest);
+            res = Math.max(res, curLongest);
+        }
+
+        return res;
+    }
+}
