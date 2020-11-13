c135750c49375c57128d3a1ee2d8d0433f5f2b13
JJ Jiang <jingjiejiang85@gmail.com>
2020-11-13 18:49:15 +0800

    116. Populating Next Right Pointers in Each Node

--- AllInOne/src/Tree/PopulatingNextRightPointersInEachNode.java
+++ AllInOne/src/Tree/PopulatingNextRightPointersInEachNode.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * 
  * 116. Populating Next Right Pointers in Each Node
@@ -25,22 +28,55 @@ class Node {
 };
 */
 class Solution {
+
+    // method 1: recursive method
+    // public Node connect(Node root) {
+        
+    //     if (root == null) return root;
+        
+    //     cross(root.left, root.right);
+    //     connect(root.left);
+    //     connect(root.right);
+
+    //     return root;
+    // }
+
+    // private void cross(Node left, Node right) {
+
+    //     if (left == null) return;
+
+    //     left.next = right;
+    //     cross(left.right, right.left);
+    // }
+
+    // method 2: level traverse with list
     public Node connect(Node root) {
         
         if (root == null) return root;
         
-        cross(root.left, root.right);
-        connect(root.left);
-        connect(root.right);
+        buildPointers(root, new LinkedList<>(), 0);
 
         return root;
     }
 
-    private void cross(Node left, Node right) {
+    private void buildPointers(Node root, List<List<Node>> nodesList, int level) {
+    
+        if (root == null) return ;
 
-        if (left == null) return;
+        if (nodesList.size() <= level) {
+            nodesList.add(new LinkedList<>());
+        }
 
-        left.next = right;
-        cross(left.right, right.left);
+        List curList = nodesList.get(level);
+        if (curList.size() == 0) {
+            curList.add(root);
+        } else {
+            Node topNode = (Node)curList.get(curList.size() - 1);
+            topNode.next = root;
+            curList.add(root);
+        }
+
+        buildPointers(root.left, nodesList, level + 1);
+        buildPointers(root.right, nodesList, level + 1);
     }
 }
