485e08e4e0f067dd26960c245ef71ac0c4e8d1b6
JJ Jiang <jingjiejiang85@gmail.com>
2020-11-19 16:16:56 +0800

    394. Decode String

---
+++ AllInOne/src/Stack/DecodeString.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * 394. Decode String
+ * 
+ * @CHYGO1985
+ * @history Nov 19, 2020
+ * 
+ * idea: one stack for num, one stack for letters
+ */
+class Solution {
+    public String decodeString(String s) {
+        
+    }
+}
