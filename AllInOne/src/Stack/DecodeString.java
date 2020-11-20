0fe8f90b6fa1d1aa17a857fb0a84737da8cbae7e
CHYGO1985 <jingjiejiang85@gmail.com>
2020-11-20 09:52:11 +0800

    394. Decode String

--- AllInOne/src/Stack/DecodeString.java
+++ AllInOne/src/Stack/DecodeString.java
@@ -10,5 +10,41 @@
 class Solution {
     public String decodeString(String s) {
         
+      Stack<Integer> numStack = new Stack<>();
+      Stack<String> charStack = new Stack<>();
+
+      StringBuilder builder = new StringBuilder();
+      int idx = 0;
+      while (idx < s.length()) {
+
+        char character = s.charAt(idx);
+          
+        if (Character.isDigit(character)) {
+
+          int num = 0;
+          while (Character.isDigit(s.charAt(idx))) {
+            num = num * 10 + (s.charAt(idx) - '0');
+            idx ++;
+          }
+          numStack.push(num);
+        } else if (character == '[') {
+          charStack.push(builder.toString());
+          builder.setLength(0);
+          idx ++;
+        } else if (character == ']') {
+          StringBuilder temp = new StringBuilder(charStack.pop());
+          int repeatTimes = numStack.pop();
+          while (repeatTimes -- > 0) {
+            temp.append(builder.toString());
+          }
+          builder = new StringBuilder(temp);
+          idx ++;
+        } else {
+          builder.append(character);
+          idx ++;
+        }
+      }
+
+      return builder.toString();
     }
 }
