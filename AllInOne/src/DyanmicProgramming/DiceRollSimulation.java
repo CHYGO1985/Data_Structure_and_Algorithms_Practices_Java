7acaa26a632d4f79026b9e16da0023d6b4e726d5
JJ Jiang <jingjiejiang85@gmail.com>
2020-11-30 16:10:32 +0800

    1223. Dice Roll Simulation

---
+++ AllInOne/src/DyanmicProgramming/DiceRollSimulation.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * 1223. Dice Roll Simulation
+ * 
+ * @CHYGO1985
+ * @history Nov 30, 2020
+ *
+ */
+class Solution {
+    public int dieSimulator(int n, int[] rollMax) {
+        
+        
+    }
+}
