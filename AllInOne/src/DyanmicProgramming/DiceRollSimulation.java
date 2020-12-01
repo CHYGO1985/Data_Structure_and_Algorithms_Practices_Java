11c129f2320a88a3a1f6eba3c7b67815d7b8215b
CHYGO1985 <jingjiejiang85@gmail.com>
2020-12-01 23:59:27 +0800

    1

--- AllInOne/src/DyanmicProgramming/DiceRollSimulation.java
+++ AllInOne/src/DyanmicProgramming/DiceRollSimulation.java
@@ -9,6 +9,8 @@
 class Solution {
     public int dieSimulator(int n, int[] rollMax) {
         
-        
+        long divisor = (long) Math.pow(10, 9) + 7;
+        // n is the num of rolls, and 7 is 6 nums(idx:0 -5) + sum(idx:6)
+        long[][] dp = new long[n][7];
     }
 }
