156669cd2043d48f82deff93970549965239696e
CHYGO1985 <jingjiejiang85@gmail.com>
2020-12-14 22:26:15 +0800

    1423. Maximum Points You Can Obtain from Cards

---
+++ AllInOne/src/DyanmicProgramming/MaxPointsYouCanObtainFromCards.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * 1423. Maximum Points You Can Obtain from Cards
+ * 
+ * @author jingjiejiang
+ * @hiistory Dec 14, 2020
+ * 
+ * ref: comment 3 https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/discuss/598111/Java-dp-solution(explanation-with-picture)
+ * 
+ */
+class Solution {
+  public int maxScore(int[] cardPoints, int k) {
+      
+  }
+}
