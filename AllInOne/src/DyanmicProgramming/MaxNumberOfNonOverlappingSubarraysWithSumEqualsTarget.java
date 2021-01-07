572cb06848b9b0d8c6f6b0c270326a043f001d65
JJ Jiang <jingjiejiang85@gmail.com>
2021-01-07 17:22:49 +0800

    1546. Maximum Number of Non-Overlapping Subarrays With Sum Equals Target

--- AllInOne/src/DyanmicProgramming/MaxNumberOfNonOverlappingSubarraysWithSumEqualsTarget.java
+++ AllInOne/src/DyanmicProgramming/MaxNumberOfNonOverlappingSubarraysWithSumEqualsTarget.java
@@ -1,40 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 
  * 1546. Maximum Number of Non-Overlapping Subarrays With Sum Equals Target
  * 
  * @author jingjiejiang
- * @history Dec 29, 2020
+ * @history Jan 7, 2020
  * 
+ * ref: (Greedy) https://leetcode.com/problems/maximum-number-of-non-overlapping-subarrays-with-sum-equals-target/discuss/780882/Java-14-lines-Greedy-PrefixSum-with-line-by-line-explanation-easy-to-understand
  */
 class Solution {
     public int maxNonOverlapping(int[] nums, int target) {
         
         assert nums != null && nums.length >= 1 && target >= 0; 
 
-        int[] preSums = new int[nums.length];
-        preSums[0] = nums[0];
-
-        int tarPos = 0, cnt = 0;
-
-        for (int idx = 1; idx < nums.length; idx ++) {
-
-            preSums[idx] += preSums[idx - 1] + nums[idx];
-            if (preSums[idx] == target) {
-                tarPos = idx;
-                cnt ++;
-            }
-        }
+        Map<Integer, Integer> numIdxMap = new HashMap<>();
+        int endIdx = -1, preSum = 0, cnt = 0;
+        numIdxMap.put(preSum, endIdx);
         
-        while (tarPos < nums.length) {
-
-            tarPos ++;
-            for (int row = tarPos; row < nums.length; row ++) {
-                preSums[row] -= preSums[row - 1];
-                if (preSums[row] == target) {
-                    tarPos = row;
-                    cnt ++;
-                }
+        for (int idx = 0; idx < nums.length; idx ++) {
+            
+            preSum += nums[idx];
+            int remain = preSum - target;
+            // we need == endIdx here, as we may get remain = 0, and we will get -1, which is the start point
+            if (numIdxMap.containsKey(remain) && numIdxMap.get(remain) >= endIdx) {
+                cnt ++;
+                endIdx = idx;
             }
+
+            numIdxMap.put(preSum, idx);
         }
 
         return cnt;
