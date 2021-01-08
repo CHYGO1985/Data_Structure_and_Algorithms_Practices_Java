3357d23a66eb344edc0221cdf9cd1b4ad05dbd4c
CHYGO1985 <jingjiejiang85@gmail.com>
2021-01-08 23:08:04 +0800

    1621. Num of Sets of K Non-Overlapping Line Seg

---
+++ AllInOne/src/DyanmicProgramming/NumberOfSetsOfKNonOverlappingLineSegments.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * 1621. Number of Sets of K Non-Overlapping Line Segments
+ * 
+ * @author jingjiejiang
+ * @history Jan 8, 2021
+ * 
+ */
+class Solution {
+  public int numberOfSets(int n, int k) {
+      
+  }
+}
