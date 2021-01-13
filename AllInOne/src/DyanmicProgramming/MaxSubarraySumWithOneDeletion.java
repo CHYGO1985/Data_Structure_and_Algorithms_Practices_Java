6d8e8afd8ee8d1024f2dc2d4c8efe8dc0f635feb
JJ Jiang <jingjiejiang85@gmail.com>
2021-01-13 20:12:32 +0800

    1186. Maximum Subarray Sum with One Deletion

---
+++ AllInOne/src/DyanmicProgramming/MaxSubarraySumWithOneDeletion.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * 1186. Maximum Subarray Sum with One Deletion
+ * 
+ * @author jingjiejiang
+ * @hisotry Jan 13, 2021
+ * 
+ * ref: https://leetcode.com/problems/maximum-subarray-sum-with-one-deletion/discuss/377486/Java-DP-O(n)-Beat-100-detail-explanation
+ * 
+ */
+class Solution {
+    public int maximumSum(int[] arr) {
+
+
+        return 1;
+    }
+}
