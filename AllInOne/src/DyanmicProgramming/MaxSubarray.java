0c3e06fefe83b70176193cc5f17d971ac5bbb7d8
JJ Jiang <jingjiejiang85@gmail.com>
2021-02-06 20:07:29 +0800

    53. Maximum Subarray

---
+++ AllInOne/src/DyanmicProgramming/MaxSubarray.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ * 53. Maximum Subarray
+ *
+ * @author jingjiejiang
+ * @history Feb 6, 2021
+ * 
+ */
+class Solution {
+    // DP: kadane algorithm
+    public int maxSubArray1(int[] nums) {
+        
+        int max_so_far = Integer.MIN_VALUE, max_ending_here = 0;
+
+        for (int idx = 0; idx < nums.length; idx ++) {
+            
+            max_ending_here = max_ending_here + nums[idx];
+            if (max_so_far < max_ending_here) {
+                max_so_far = max_ending_here;
+            }
+
+            if (max_ending_here < 0) {
+                max_ending_here = 0;
+            }
+        }
+
+        return max_so_far;
+    }
+
+    // Greedy
+    public int maxSubArray2(int[] nums) {
+        
+        int curSum = nums[0], maxSum = nums[0];
+
+        for (int idx = 1; idx < nums.length; idx ++) {
+            curSum = Math.max(nums[idx], curSum + nums[idx]);
+            maxSum = Math.max(maxSum, curSum);
+        }
+
+        return maxSum;
+    }        
+}
