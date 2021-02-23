c0dc74a09885d9222a3a92cf506ab98e8ef6008c
JJ Jiang <jingjiejiang85@gmail.com>
2021-02-23 21:20:03 +0800

    240. Search a 2D Matrix II

---
+++ AllInOne/src/BinarySearch/SearchA2DMatrixII.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * 240. Search a 2D Matrix II
+ * 
+ * @author jingjiejiang
+ * @history Feb 23, 2021
+ * 
+ */
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        if (matrix == null || matrix.length == 0) return false;
+
+        return false;
+    }
+}
