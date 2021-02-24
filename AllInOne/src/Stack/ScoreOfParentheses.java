310e6eb91b52ce7cdaeef14c69aa57a95a4c07ab
JJ Jiang <jingjiejiang85@gmail.com>
2021-02-24 21:11:54 +0800

    856. Score of Parentheses

---
+++ AllInOne/src/Stack/ScoreOfParentheses.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * 856. Score of Parentheses
+ * 
+ * @author jingjiejiang
+ * @history Feb 24, 2021
+ * 
+ */
+class Solution {
+    public int scoreOfParentheses(String S) {
+        
+    }
+}
