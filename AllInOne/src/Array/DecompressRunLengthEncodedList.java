56db684378e4f80d9e4d57c0f591fc0863dac8dd
CHYGO1985 <jingjiejiang85@gmail.com>
2021-02-27 21:57:43 +0800

    1313. Decompress Run-Length Encoded List

---
+++ AllInOne/src/Array/DecompressRunLengthEncodedList.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+/**
+ * 
+ * 1313. Decompress Run-Length Encoded List
+ * 
+ * @author jingjiejiang
+ * @history Feb 27, 2021
+ * 
+ */
+class Solution {
+  public int[] decompressRLElist(int[] nums) {
+      
+    assert nums != null && nums.length >= 2 && nums.length % 2 == 0;
+
+    int resArrSize = 0;
+    for (int idx = 0; idx < nums.length; idx += 2) {
+      resArrSize += nums[idx];
+    }
+
+    int[] res = new int[resArrSize];
+
+    int startIdx = 0;
+    for (int idx = 0; idx < nums.length; idx += 2) {
+
+      Arrays.fill(res, startIdx, startIdx + nums[idx], nums[idx + 1]);
+      startIdx += nums[idx];
+    }
+
+    return res;
+  }
+}
