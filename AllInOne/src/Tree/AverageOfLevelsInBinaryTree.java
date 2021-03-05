f30895d56fcb1aa238eb12417de87ab38e516094
JJ Jiang <jingjiejiang85@gmail.com>
2021-03-05 21:32:28 +0800

    637. Average of Levels in Binary Tree

---
+++ AllInOne/src/Tree/AverageOfLevelsInBinaryTree.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+ * 
+ * 637. Average of Levels in Binary Tree
+ * 
+ * @author jingjiejiang
+ * @history Mar 5, 2021
+ * 
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        
+    }
+}
