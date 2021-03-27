cb57c8a25182d9f86c9b8b5f4a22c20bc4d68641
CHYGO1985 <jingjiejiang85@gmail.com>
2021-03-27 23:55:39 +0800

    doomsday_fuel

--- AllInOne/src/google_foobar/Solution.java
+++ AllInOne/src/google_foobar/Solution.java
@@ -33,7 +33,6 @@ public class Main {
   public static void main(String[] args) {
       // int a = solution(new int[]{13, 5, 6, 2, 5}, new int[]{5, 2, 5, 13});
 
-
       int a = solution(new int[]{14, 27, 1, 4, 2, 50, 3, 1}, new int[]{2, 4, -4, 3, 1, 1, 14, 27, 50});
 
       System.out.println(a);
