c5891f08ff1dc8384eb5233638b184cc9315f558
JJ Jiang <jingjiejiang85@gmail.com>
2021-04-02 19:33:03 +0800

    29. Divide Two Integers

--- AllInOne/src/Math/DivideTwoIntegers.java
+++ AllInOne/src/Math/DivideTwoIntegers.java
@@ -41,4 +41,51 @@ public class DivideTwoIntegers {
 
         return substraction;
     }
+
+    private static int HALF_INT_MIN = -1073741824;
+    // Repeated Exponential Searches
+    public int divide2(int dividend, int divisor) {
+
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+
+        int negatives = 2;
+
+        if (dividend > 0) {
+            negatives --;
+            dividend = -dividend;
+        }
+
+        if (divisor > 0) {
+            negatives --;
+            divisor = -divisor; 
+        }
+
+        int quotient = 0;
+
+        while (divisor >= dividend) {
+
+            // in case (int)
+            int powerOfTwo = -1;
+            int value = divisor;
+
+            while (value >= HALF_INT_MIN && value + value >= dividend) {
+                value += value;
+                powerOfTwo += powerOfTwo;
+            }
+
+            quotient += powerOfTwo;
+            dividend -= value;
+        }
+
+
+        /* If there was originally one negative sign, then
+         * the quotient remains negative. Otherwise, switch
+         * it to positive. */
+        if (negatives != 1) {
+            return -quotient;
+        }
+        return quotient;
+    }
 }
