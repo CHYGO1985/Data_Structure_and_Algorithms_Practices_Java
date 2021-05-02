fb4c64f674aae328224284b8208f07d8155a7346
JJ Jiang <jingjiejiang85@gmail.com>
2021-05-02 14:55:06 +0800

    42. Trapping Rain Water

--- AllInOne/src/TwoPointers/TrappingRainWater.java
+++ AllInOne/src/TwoPointers/TrappingRainWater.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 /**
  * 
  * 42. Trapping Rain Water
@@ -7,7 +9,7 @@
  * 
  */
 public class TrappingRainWater {
-    public int trap(int[] height) {
+    public int trap1(int[] height) {
 
         assert height != null;
 
@@ -34,4 +36,23 @@ public class TrappingRainWater {
 
         return sum;
     }
+
+    public int trap(int[] height) {
+        if (height == null) return 0;
+         Stack<Integer> s = new Stack<Integer>();
+         int i = 0, maxWater = 0, maxBotWater = 0;
+         while (i < height.length){
+             if (s.isEmpty() || height[i]<=height[s.peek()]){
+                 s.push(i++);
+             }
+             else {
+                 int bot = s.pop();
+                 maxBotWater = s.isEmpty()? // empty means no il
+                 0:(Math.min(height[s.peek()], height[i]) - height[bot])*(i-s.peek()-1);
+                 maxWater += maxBotWater;
+             }
+         }
+         return maxWater;
+     }
+ }
 }
