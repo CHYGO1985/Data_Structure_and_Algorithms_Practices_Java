2579d542aecd9fd9c0e4562dd479b9b53fe35b4d
JJ Jiang <jingjiejiang85@gmail.com>
2021-05-04 22:41:42 +0800

    19. Remove Nth Node From End of List

---
+++ AllInOne/src/TwoPointers/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ * 19. Remove Nth Node From End of List
+ * 
+ * @author jingjiejiang
+ * @history May 4, 2021
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        
+    
+    }
+}
