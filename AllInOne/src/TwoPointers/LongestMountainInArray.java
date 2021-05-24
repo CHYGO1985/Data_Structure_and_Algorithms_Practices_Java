9f727a90d085280b46180b1cde03fefb7d869316
JJ Jiang <jingjiejiang85@gmail.com>
2021-05-24 14:44:31 +0800

    845. Longest Mountain in Array

--- AllInOne/src/TwoPointers/LongestMountainInArray.java
+++ AllInOne/src/TwoPointers/LongestMountainInArray.java
@@ -1,9 +1,12 @@
+package TwoPointers;
 /**
  * 
  * 845. Longest Mountain in Array
  * 
  * @CHYGO1985
  * @author jingjiejiang
+ * 
+ * @history May 24, 2021
  * 
  */
 class Solution {
@@ -16,8 +19,11 @@ class Solution {
         for (int idx = 1; idx < A.length; idx ++) {
             
             if ( (down > 0 && A[idx - 1] < A[idx]) || A[idx - 1] == A[idx]) up = down = 0;
+            
             if (A[idx - 1] < A[idx]) up ++;
-            if (A[idx - 1] > A[idx]) down ++;
+            else if (A[idx - 1] > A[idx]) down ++;
+            
+            // cannot use Math.max here, for array like [2,2,2]
             if (up > 0 && down > 0 && up + down + 1 > res) res = up + down + 1;
         }
         
