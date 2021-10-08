7641e5760adeee7c9bc34fb9a7ad67e483e8e093
CHYGO1985 <jingjiejiang85@gmail.com>
2021-10-08 23:55:53 +0800

    1152. Analyze User Website Visit Pattern

---
+++ AllInOne/src/HashTable/AnalyzeUserWebsiteVisitPattern.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * 1152. Analyze User Website Visit Pattern
+ * 
+ * @author jingjiejiang
+ * @history Oct 8, 2021
+ * 
+ */
+public class AnalyzeUserWebsiteVisitPattern {
+  public List<String> mostVisitedPattern(String[] username, int[] timestamp, String[] website) {
+        
+  }
+}
