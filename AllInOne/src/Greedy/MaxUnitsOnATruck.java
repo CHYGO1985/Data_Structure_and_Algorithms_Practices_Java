4e12b93551b871cc4abcf8f4566eecc1e6e6c2d3
CHYGO1985 <jingjiejiang85@gmail.com>
2021-10-19 01:45:10 +0800

    1710. Maximum Units on a Truck

---
+++ AllInOne/src/Greedy/MaxUnitsOnATruck.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * 1710. Maximum Units on a Truck
+ *
+ * @author jingjiejiang
+ * @history Oct 19, 2021
+ * 
+ * ref: greedy (sort, then get from max to min)
+ *
+ */
+class Solution {
+  public int maximumUnits(int[][] boxTypes, int truckSize) {
+      
+  }
+}
