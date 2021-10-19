70b376b4b5759cc3c02858ffc5f7d0b08aedf9fd
CHYGO1985 <jingjiejiang85@gmail.com>
2021-10-19 15:35:11 +0800

    1710. Maximum Units on a Truck

--- AllInOne/src/Greedy/MaxUnitsOnATruck.java
+++ AllInOne/src/Greedy/MaxUnitsOnATruck.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * 
  * 1710. Maximum Units on a Truck
@@ -11,5 +13,26 @@
 class Solution {
   public int maximumUnits(int[][] boxTypes, int truckSize) {
       
+    assert boxTypes.length >= 1 && boxTypes.length <= 1000
+            && truckSize >= 1;
+
+
+    // sort in reverse order
+    Arrays.sort(boxTypes, (b1, b2) -> b2[1] - b1[1]);
+
+    int maxUnit = 0;
+
+    for (int idx = 0; idx < boxTypes.length; idx ++) {
+      if (truckSize >= boxTypes[idx][0]) {
+        maxUnit += boxTypes[idx][0] * boxTypes[idx][1];
+        truckSize -= boxTypes[idx][0];
+      } else {
+
+        maxUnit += truckSize * boxTypes[idx][1];
+        break;
+      }
+    }
+
+    return maxUnit;
   }
 }
