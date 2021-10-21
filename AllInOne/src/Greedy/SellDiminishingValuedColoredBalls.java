277e58500fde4c9a24ca153964107c122e1bf3f2
CHYGO1985 <jingjiejiang85@gmail.com>
2021-10-21 17:02:02 +0800

    1648. Sell Diminishing-Valued Colored Balls

---
+++ AllInOne/src/Greedy/SellDiminishingValuedColoredBalls.java
@@ -0,0 +1,40 @@
+import java.math.BigInteger;
+import java.util.PriorityQueue;
+
+/**
+ * 
+ * 1648. Sell Diminishing-Valued Colored Balls
+ * 
+ * @author jingjiejiang
+ * @history Oct 21, 2021
+ * 
+ */
+public class SellDiminishingValuedColoredBalls {
+  // time limit exceeded solution
+  public int maxProfit(int[] inventory, int orders) {
+        
+    assert inventory.length >= 1 && inventory.length <= 1e5 && orders >= 1;
+
+        BigDecimal sum = BigDecimal.valueOf(0);
+        BigDecimal divisor = BigDecimal.valueOf(1e9 + 7); 
+
+        PriorityQueue<Integer> inventQueue = new PriorityQueue<>(
+            (a, b) -> b - a);
+        
+        for (int item : inventory) {
+            inventQueue.offer(item);
+        }
+
+        while (orders > 0) {
+
+          int curInvent = inventQueue.poll();
+          // add current inventory
+          sum = sum.add(BigDecimal.valueOf(curInvent));
+          // add the new inventory num to the queue
+          inventQueue.offer(curInvent - 1);
+          orders --;
+        }
+
+        return sum.remainder(divisor).intValue();
+  }
+}
