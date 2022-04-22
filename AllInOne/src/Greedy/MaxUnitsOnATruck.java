b31f5c3229ad51c531d60d87fd09f9b212fb4187
CHYGO1985 <jingjiejiang85@gmail.com>
2022-04-22 22:37:05 +0800

    1710. Maximum Units on a Truck

--- AllInOne/src/Greedy/MaxUnitsOnATruck.java
+++ AllInOne/src/Greedy/MaxUnitsOnATruck.java
@@ -13,24 +13,42 @@ import java.util.Arrays;
 class Solution {
   public int maximumUnits(int[][] boxTypes, int truckSize) {
       
-    assert boxTypes.length >= 1 && boxTypes.length <= 1000
-            && truckSize >= 1;
+    // assert boxTypes.length >= 1 && boxTypes.length <= 1000
+    //         && truckSize >= 1;
+
+    // // sort in reverse order
+    // Arrays.sort(boxTypes, (b1, b2) -> b2[1] - b1[1]);
+
+    // int maxUnit = 0;
+
+    // for (int idx = 0; idx < boxTypes.length; idx ++) {
+    //   if (truckSize >= boxTypes[idx][0]) {
+    //     maxUnit += boxTypes[idx][0] * boxTypes[idx][1];
+    //     truckSize -= boxTypes[idx][0];
+    //   } else {
+
+    //     maxUnit += truckSize * boxTypes[idx][1];
+    //     break;
+    //   }
+    // }
+
+    // return maxUnit;
 
 
-    // sort in reverse order
+    assert boxTypes.length >= 1 && boxTypes.length <= 1000 && truckSize >= 1;
+
     Arrays.sort(boxTypes, (b1, b2) -> b2[1] - b1[1]);
 
     int maxUnit = 0;
 
-    for (int idx = 0; idx < boxTypes.length; idx ++) {
-      if (truckSize >= boxTypes[idx][0]) {
-        maxUnit += boxTypes[idx][0] * boxTypes[idx][1];
-        truckSize -= boxTypes[idx][0];
-      } else {
-
-        maxUnit += truckSize * boxTypes[idx][1];
+    for (int row = 0; row < boxTypes.length; row ++) {
+      if (boxTypes[row][0] >= truckSize) {
+        maxUnit += truckSize * boxTypes[row][1];
         break;
-      }
+      } else {
+        truckSize -= boxTypes[row][0];
+        maxUnit += boxTypes[row][0] * boxTypes[row][1]; 
+      } 
     }
 
     return maxUnit;
