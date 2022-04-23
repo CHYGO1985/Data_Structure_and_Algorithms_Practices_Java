efe70a242af28b950d3f867b69068f4b45a9a787
CHYGO1985 <jingjiejiang85@gmail.com>
2022-04-23 21:06:24 +0800

    13. Roman to Integer

---
+++ AllInOne/src/HashTable/RomanToInteger.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ * 13. Roman to Integer
+ * 
+ * @author jingjiejiang
+ * @history Apr 23, 2022
+ * 
+ */
+public class RomanToInteger {
+  public int romanToInt(String s) {
+       
+    assert s != null && s.length() >= 1 && s.length() <= 15;
+
+    Map<Character, Integer> romanIntMap = new HashMap<>() {{
+      put('I', 1);
+      put('V', 5);
+      put('X', 10);
+      put('L', 50);
+      put('C', 100);
+      put('D', 500);
+      put('M', 1000);
+    }};
+
+    int sum = 0;
+
+    for (char letter : s.toCharArray()) {
+      sum += romanIntMap.get(letter);
+    }
+
+    return sum;
+  }
+}
