7381b27d017757c41a48d592a8fcd9eea014c3e7
CHYGO1985 <jingjiejiang85@gmail.com>
2022-04-24 10:13:21 +0800

    53. Maximum Subarray

--- AllInOne/src/DyanmicProgramming/MaximumSubarray.java
+++ AllInOne/src/DyanmicProgramming/MaximumSubarray.java
@@ -22,4 +22,23 @@ class Solution {
         
         return maxSum;
     }
+
+    // method 2:
+    public int maxSubArray2(int[] nums) {
+      int max_so_far = Integer.MIN_VALUE, max_ending_here = 0;
+
+      for (int idx = 0; idx < nums.length; idx ++) {
+          
+          max_ending_here = max_ending_here + nums[idx];
+          if (max_so_far < max_ending_here) {
+              max_so_far = max_ending_here;
+          }
+
+          if (max_ending_here < 0) {
+              max_ending_here = 0;
+          }
+      }
+
+      return max_so_far;
+  }
 }
