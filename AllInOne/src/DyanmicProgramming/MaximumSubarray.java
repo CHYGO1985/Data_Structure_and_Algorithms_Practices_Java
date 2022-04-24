8b4a3a7ccc06969292300b4c465f7beec5df9822
CHYGO1985 <jingjiejiang85@gmail.com>
2022-04-24 10:01:02 +0800

    53. Maximum Subarray

---
+++ AllInOne/src/DyanmicProgramming/MaximumSubarray.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ * 53. Maximum Subarray
+ *
+ * @author jingjie jiang
+ */
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        assert nums != null && nums.length >= 1 && nums.length <= Math.pow(10, 5);
+        
+        int maxSum = nums[0], preSum = nums[0];
+        
+        for (int idx = 1; idx < nums.length; idx ++) {
+            
+            int curSum = preSum + nums[idx];
+            preSum = Math.max(curSum, nums[idx]);
+            maxSum = Math.max(preSum, maxSum);
+        }
+        
+        return maxSum;
+    }
+}
