29baaf540098dae833d7ad833948795aa47d7888
CHYGO1985 <jingjiejiang85@gmail.com>
2022-10-07 20:37:16 +0800

    8. String to Integer (atoi)

--- AllInOne/src/String/StringToIntegerAtoi.java
+++ AllInOne/src/String/StringToIntegerAtoi.java
@@ -1,3 +1,5 @@
+package src.String;
+
 import java.math.BigInteger;
 
 /**
@@ -11,28 +13,34 @@ import java.math.BigInteger;
 class Solution {
     public int myAtoi(String s) {
         
-        int idx = 0;
-        int sign = 1;
-        int result = 0;
-        if (s.length() == 0) return 0;
-
-        //Discard whitespaces in the beginning
-        while (idx < s.length() && s.charAt(idx) == ' ')
-            idx++;
-
-        // Check if optional sign if it exists
-        if (idx < s.length() && (s.charAt(idx) == '+' || s.charAt(idx) == '-'))
-            sign = (s.charAt(idx++) == '-') ? -1 : 1;
-
-        // Build the result and check for overflow/underflow condition
-        while (idx < s.length() && s.charAt(idx) >= '0' && s.charAt(idx) <= '9') {
-            if (result > Integer.MAX_VALUE / 10 ||
-                    (result == Integer.MAX_VALUE / 10 && s.charAt(idx) - '0' > Integer.MAX_VALUE % 10)) {
-                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-            }
-            result = result * 10 + (s.charAt(idx++) - '0');
-        }
+      assert s != null && s.length() >= 0;
         
-        return result * sign;
+      int idx = 0, sign = 1, result = 0;
+      
+      if (s.length() == 0) return result;
+      
+      //Discard whitespaces in the beginning
+      while (idx < s.length() && s.charAt(idx) == ' ') 
+          idx ++;
+      
+      // Check if optional sign if it exists
+      while (idx < s.length() && (s.charAt(idx) == '+' || s.charAt(idx) == '-')) {
+          sign = (s.charAt(idx ++) == '-') ? - 1 : 1;
+          // for case "+-12"
+          if (idx < s.length() && (s.charAt(idx) == '+' || s.charAt(idx) == '-')) return result;
+      }
+          
+      
+      // Build the result and check for overflow/underflow condition
+      while (idx < s.length() && s.charAt(idx) >= '0' && s.charAt(idx) <= '9') {
+          if (result > Integer.MAX_VALUE / 10 ||
+                  (result == Integer.MAX_VALUE / 10 && s.charAt(idx) - '0' > Integer.MAX_VALUE % 10)) {
+              return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+          }
+          
+          result = result * 10 + (s.charAt(idx ++) - '0');
+      }
+      
+      return result * sign;
     }
 }
