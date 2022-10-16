5f3b3da9a112e1531ebce53475658413cf55bf68
CHYGO1985 <jingjiejiang85@gmail.com>
2022-10-17 07:56:05 +0800

    26. Remove Duplicates from Sorted Array

---
+++ AllInOne/src/TwoPointers/RemoveDupFromSortedArr.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ * 26. Remove Duplicates from Sorted Array
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+  if (!nums) {
+    return -1;
+  }   
+  
+  let insertIdx = 1;
+
+  for (let idx = 1; idx < nums.length; idx ++) {
+    if (nums[idx - 1] != nums[idx]) {
+      nums[insertIdx] = nums[idx];
+      insertIdx ++;
+    }
+  }
+
+  return insertIdx;
+};
