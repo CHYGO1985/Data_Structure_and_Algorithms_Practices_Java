7e27c9ba321b02d58c69b462cb80281eb26eb5ff
CHYGO1985 <jingjiejiang85@gmail.com>
2022-12-18 12:38:20 +0800

    73. Set Matrix Zeroes

--- AllInOne/src/Array/SetMatrixZeroes.java
+++ AllInOne/src/Array/SetMatrixZeroes.java
@@ -1,3 +1,5 @@
+package src.Array;
+
 /**
  * 
  * 73. Set Matrix Zeroes
@@ -18,93 +20,42 @@ public class SetMatrixZeroes {
     
     public void setZeroes(int[][] matrix) {
         
-        if(matrix==null) return;
-        final int M=matrix.length, N=matrix[0].length;
-        boolean topZero=false, leftZero=false;
-        
-        for(int i=0; i<M; i++){
-            for(int j=0; j<N; j++){
-                if(matrix[i][j]==0){
-                    if(i==0) topZero = true;
-                    if(j==0) leftZero = true;
-                    
-                    // set the first ele of this row as 0
-                    matrix[i][0] = 0;
-                    // set the first ele of the col as 0
-                    matrix[0][j] = 0;
-                }
-            }
-        }
-        
-        for(int i=1; i<M; i++){
-            if(matrix[i][0]==0){
-                for(int q=1; q<N; q++) 
-                    matrix[i][q] = 0;
-            }
-        }
-        
-        for(int j=1; j<N; j++){
-            if(matrix[0][j]==0){
-                for(int p=1; p<M; p++)  
-                    matrix[p][j] = 0;
-            }
-        }
-        
-        if(topZero){
-            for(int q=0; q<N; q++) matrix[0][q] = 0;
-        }
-        if(leftZero){
-            for(int p=0; p<M; p++) matrix[p][0] = 0;
-        }
-        /*
-        if (0 == matrix.length) {
-            return ;
-        }
-        
-        long rowChecker = Long.MAX_VALUE;
-        long colChecker = Long.MAX_VALUE;
-        
-        for (int i = 0; i < matrix.length; i ++) {
-            
-            boolean isZeroRow = false;
-            for (int j = 0; j < matrix[0].length; j ++) {
-                
-                if (0 == matrix[i][j]) {
-                    isZeroRow = true;
-                    
-                    if ( ((colChecker ^ Long.MAX_VALUE) & (1 << j)) >> j != 1) {
-                        colChecker ^= 1 << j;
-                    }
-                }
-            }
-            
-            if (true == isZeroRow) {
-                rowChecker ^= 1 << i;
-            }
-        }
-        
-        // check rows
-        for (int row = 0; row < matrix.length; row ++) {
-            
-            if ( ((rowChecker ^ Long.MAX_VALUE) & (1 << row)) >> row == 1) {
-                
-                for (int col = 0; col < matrix[0].length; col ++) {
-                    matrix[row][col] = 0;
-                }
-            }
-        }
-        
-        // check columns
-        for (int col = 0; col < matrix[0].length; col ++) {
-            
-            if ( ((colChecker ^ Long.MAX_VALUE) & (1 << col)) >> col == 1) {
-                
-                for (int row = 0; row < matrix.length; row ++) {
-                    matrix[row][col] = 0;
-                }
-            }
-        }
-        */
+      assert matrix.length >= 1 && matrix[0].length <= 200;
+
+      boolean isFstColZero = false;
+
+      for (int rowIdx = 0; rowIdx < matrix.length; rowIdx ++) {
+          if (matrix[rowIdx][0] == 0) {
+              isFstColZero = true;
+          }
+
+          for (int colIdx = 1; colIdx < matrix[0].length; colIdx ++) {
+              if (matrix[rowIdx][colIdx] == 0) {
+                  matrix[0][colIdx] = 0;
+                  matrix[rowIdx][0] = 0;
+              }
+          }
+      }
+
+      for (int rowIdx = 1; rowIdx < matrix.length; rowIdx ++) {
+          for (int colIdx = 1; colIdx < matrix[0].length; colIdx ++) {
+              if (matrix[rowIdx][0] == 0 || matrix[0][colIdx] == 0) {
+                  matrix[rowIdx][colIdx] = 0;
+              }
+          }
+      }
+
+      if (matrix[0][0] == 0) {
+          for (int colIdx = 0; colIdx < matrix[0].length; colIdx ++) {
+              matrix[0][colIdx] = 0;
+          }
+      }
+
+      if (isFstColZero) {
+          for (int rowIdx = 0; rowIdx < matrix.length; rowIdx ++) {
+              matrix[rowIdx][0] = 0;
+          }
+      }
     }
     
 	public static void main(String[] args) {
