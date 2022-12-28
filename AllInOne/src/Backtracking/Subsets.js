cf4d0b26797681c646971b314519cd13f605ae05
CHYGO1985 <jingjiejiang85@gmail.com>
2022-12-28 18:10:09 +0800

    78. Subsets

---
+++ AllInOne/src/Backtracking/Subsets.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+  
+  if (!nums || nums.length < 1) return undefined;
+
+  const res = [];
+  const getSubSet = (nums, cur, begin) => {
+    res.push([...cur]);
+
+    for (let idx = begin; idx < nums.length; idx ++) {
+      cur.push(nums[idx]);
+      getSubSet(nums, cur, idx + 1);
+      cur.pop();
+    }
+  };
+
+  getSubSet(nums, [], 0);
+  return res;
+};
