b60dea416a8d97563bc747106ef7b8141a4560e8
CHYGO1985 <jingjiejiang85@gmail.com>
2024-07-12 12:16:00 +0800

    2721. Execute Asynchronous Functions in Parallel

---
+++ AllInOne/src/30DaysJS/ExecuteAsynchronousFunctionsinParallel.js
@@ -0,0 +1,38 @@
+/**
+ * 2721. Execute Asynchronous Functions in Parallel
+ */
+
+/**
+ * @param {Array<Function>} functions
+ * @return {Promise<any>}
+ */
+var promiseAll = function(functions) {
+  return new Promise((resolve, reject) => {
+      if (functions.length === 0) {
+          resolve([])
+          return
+      }
+
+      const res = new Array(functions.length).fill(null)
+
+      let resolvedCount = 0
+
+      functions.forEach(async (func, idx) => {
+          try {
+              const curRes = await func()
+              res[idx] = curRes
+              resolvedCount ++
+              if (resolvedCount === functions.length) {
+                  resolve(res)
+              }
+          } catch (err) {
+              reject(err)
+          }
+      })
+  }) 
+};
+
+/**
+* const promise = promiseAll([() => new Promise(res => res(42))])
+* promise.then(console.log); // [42]
+*/
