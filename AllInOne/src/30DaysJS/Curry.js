13737f260249629bd9172906828d099dd79f4aa3
CHYGO1985 <jingjiejiang85@gmail.com>
2024-07-23 19:15:55 +0800

    2632.Curry

---
+++ AllInOne/src/30DaysJS/Curry.js
@@ -0,0 +1,31 @@
+/**
+ * 2632.Curry
+ */
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var curry = function (fn) {
+  return function curried(...args) {
+    //   if(args.length >= fn.length) {
+    //     return fn(...args);
+    //   }
+
+    //   return (...nextArgs) => {
+    //     console.log(`args: ${args}`)
+    //     console.log(`nextArgs: ${nextArgs}`)
+    //     return curried(...args, ...nextArgs);
+    //   }
+    if (args.length >= fn.length) {
+      return fn.apply(this, args);
+    }
+
+    return curried.bind(this, ...args);
+  };
+};
+
+/**
+ * function sum(a, b) { return a + b; }
+ * const csum = curry(sum);
+ * csum(1)(2) // 3
+ */
