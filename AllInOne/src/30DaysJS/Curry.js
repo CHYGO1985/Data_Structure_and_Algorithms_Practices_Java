d3a88a430777b0033d873d61ba3a5bdcdc860175
CHYGO1985 <jingjiejiang85@gmail.com>
2024-08-04 10:27:29 +0800

    curry

--- AllInOne/src/30DaysJS/Curry.js
+++ AllInOne/src/30DaysJS/Curry.js
@@ -1,12 +1,10 @@
 /**
- * 2632.Curry
- */
-/**
  * @param {Function} fn
  * @return {Function}
  */
 var curry = function (fn) {
   return function curried(...args) {
+    // method 1
     //   if(args.length >= fn.length) {
     //     return fn(...args);
     //   }
@@ -16,6 +14,8 @@ var curry = function (fn) {
     //     console.log(`nextArgs: ${nextArgs}`)
     //     return curried(...args, ...nextArgs);
     //   }
+
+    // method 2
     if (args.length >= fn.length) {
       return fn.apply(this, args);
     }
