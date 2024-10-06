c71e229c0963ef58748333d52bf24e39e3190c61
CHYGO1985 <jingjiejiang85@gmail.com>
2024-10-06 20:58:33 +0200

    55. Jump Game

---
+++ AllInOne/src/Array/JumpGame.ts
@@ -0,0 +1,14 @@
+/**
+ * 55. Jump Game
+ */
+function canJump(nums: number[]): boolean {
+  if (!nums) return false;
+
+  let far = 0;
+
+  for (let idx = 0; idx < nums.length && idx <= far; idx++) {
+    far = Math.max(far, idx + nums[idx]);
+  }
+
+  return far >= nums.length - 1 ? true : false;
+}
